9e7431404a6d8e24de70a6a536ca89f362a54f28
Henry Tao <henry.tao@shopify.com>
2019-11-29 15:51:58 -0500

    Fix List race condition

--- android/resourceextensions/src/main/java/com/shopify/resourceextensions/List.kt
+++ android/resourceextensions/src/main/java/com/shopify/resourceextensions/List.kt
@@ -3,7 +3,7 @@ package com.shopify.resourceextensions
 class List(value: kotlin.collections.List<Node>) : Node(value) {
 
     override fun evaluate(context: Context): Any? {
-        val elements = value as kotlin.collections.List<Node>
+        val elements = (value as kotlin.collections.List<Node>)?.takeIf { it.isNotEmpty() } ?: return null
         ((elements.first() as? Identifier)?.evaluate(context) as? Factory)?.let { factory ->
             val args = elements.subList(1, elements.size)
             return factory.build(context, args)
