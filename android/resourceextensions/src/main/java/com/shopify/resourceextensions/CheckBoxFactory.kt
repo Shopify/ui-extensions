97096458ba21a1e11dc0d33f1365bd7a22e5f56e
Henry Tao <henry.tao@shopify.com>
2019-12-02 12:50:13 -0500

    Update checkbox and textfield factory

--- android/resourceextensions/src/main/java/com/shopify/resourceextensions/CheckBoxFactory.kt
+++ android/resourceextensions/src/main/java/com/shopify/resourceextensions/CheckBoxFactory.kt
@@ -3,9 +3,22 @@ package com.shopify.resourceextensions
 class CheckBoxFactory : Factory {
 
     override fun build(context: Context, args: kotlin.collections.List<Node>): Any {
-        return CheckBox()
+        return CheckBox(context, args)
     }
 
 }
 
-class CheckBox
+class CheckBox (context: Context, args: kotlin.collections.List<Node>) {
+
+    var checked: Boolean = false
+
+    init {
+        args.forEach { node ->
+            (node.evaluate(context) as? Properties)?.let { properties ->
+                // TODO
+            }
+        }
+    }
+
+}
+
--- android/resourceextensions/src/main/java/com/shopify/resourceextensions/TextFieldFactory.kt
+++ android/resourceextensions/src/main/java/com/shopify/resourceextensions/TextFieldFactory.kt
@@ -3,9 +3,22 @@ package com.shopify.resourceextensions
 class TextFieldFactory : Factory {
 
     override fun build(context: Context, args: kotlin.collections.List<Node>): Any {
-        return TextField()
+        return TextField(context, args)
     }
 
 }
 
-class TextField
+class TextField(context: Context, args: kotlin.collections.List<Node>) {
+
+    var label: String? = null
+
+    init {
+        args.forEach { node ->
+            (node.evaluate(context) as? Properties)?.let { properties ->
+                label = properties["label"] as String
+            }
+        }
+    }
+
+}
+
