c1da68d74c4630472e1daefc7e4a75a53757170d
Fred Ulrich <fred.ulrich@shopify.com>
2019-12-16 14:51:48 -0500

    feat(components): add structured component definition for Button

--- packages/app-extensions-polaris-components/src/Button/builder-definition.ts
+++ packages/app-extensions-polaris-components/src/Button/builder-definition.ts
@@ -1,7 +1,14 @@
 import {AppExtensionMinor} from '@shopify/polaris-icons';
+import * as Types from '../constants';
 
 export default {
   componentType: 'Button',
   icon: AppExtensionMinor,
-  editableProps: {},
+  editableProps: {
+    label: Types.STRING,
+    onClick: Types.FUNCTION,
+    primary: Types.BOOLEAN,
+    secondary: Types.BOOLEAN,
+    textAlign: ['left', 'right', 'center'],
+  },
 };
---
+++ packages/app-extensions-polaris-components/src/constants.ts
@@ -0,0 +1,4 @@
+export const STRING = 'string';
+export const FUNCTION = 'function';
+export const BOOLEAN = 'boolean';
+export const NUMBER = 'number';
