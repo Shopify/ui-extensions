28c931e5b923350f97f8e425cb6f55b4e37d598a
Konstantin Tennhard <konstantin@tennhard.net>
2020-01-23 12:47:48 -0500

    refactor(renderer): extract identifier resolution

--- packages/app-extensions-renderer/src/ast/evaluate.ts
+++ packages/app-extensions-renderer/src/ast/evaluate.ts
@@ -7,19 +7,49 @@ export default function<T extends {[name: string]: any}>(
   ast: AST,
   context?: T,
 ): EvaluationResult<T> {
-  return traverse(ast, (type, value) => {
-    switch (type) {
-      case 'identifier':
-        if (context === undefined)
-          throw `Cannot evaluate identifier due to missing runtime context`;
-        if (context[value] === undefined) throw `AST contains an unknown identifier: ${value}`;
-        return context[value];
-      case 'literal':
-        return value;
-      case 'list':
-        const [first, ...rest] = value;
-        if (typeof first === 'function') return first.call(undefined, ...rest);
-        return [first, ...rest];
-    }
-  });
+  return environment({}, context || {}, ({resolve}) =>
+    traverse(ast, (type, value) => {
+      switch (type) {
+        case 'identifier':
+          return resolve(value);
+        case 'literal':
+          return value;
+        case 'list':
+          const [first, ...rest] = value;
+          if (typeof first === 'function') return first.call(undefined, ...rest);
+          return [first, ...rest];
+      }
+    }),
+  );
+}
+
+interface Scope {
+  [identifier: string]: any;
+}
+
+interface Environment {
+  resolve(identifier: string): any;
+  withinNewScope<T>(program: (environment: Environment) => T): T;
+}
+
+function environment<T>(
+  keywords: Scope,
+  globals: Scope,
+  program: (environment: Environment) => T,
+): T {
+  function resolve(env: Array<Scope>, identifier: string): any {
+    const ctx = env[0];
+    if (ctx === undefined) throw `Cannot evaluate identifier due to missing runtime context`;
+    if (ctx[identifier] === undefined) throw `AST contains an unknown identifier: ${identifier}`;
+    return ctx[identifier];
+  }
+
+  function withinEnvironment<T>(env: Array<Scope>, program: (environment: Environment) => T): T {
+    return program({
+      resolve: identifier => resolve(env, identifier),
+      withinNewScope: program => withinEnvironment([{}, ...env], program),
+    });
+  }
+
+  return withinEnvironment([globals], program);
 }
--- packages/app-extensions-renderer/src/ast/test/evaluate.test.ts
+++ packages/app-extensions-renderer/src/ast/test/evaluate.test.ts
@@ -51,12 +51,4 @@ describe('evaluate', () => {
 
     expect(() => evaluate(expression, {})).toThrow(`AST contains an unknown identifier: Invalid`);
   });
-
-  it('throws an error if identifier resultion is attempted without a runtime context', () => {
-    const expression = List([Identifier('Invalid')]);
-
-    expect(() => evaluate(expression)).toThrow(
-      `Cannot evaluate identifier due to missing runtime context`,
-    );
-  });
 });
