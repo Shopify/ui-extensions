8d43654982d770de88b04ae9ef45a908a4e7d382
David Muzi <davidmuzi@gmail.com>
2020-02-24 14:27:15 -0500

    Update to Text props

--- components/Card.ts
+++ components/Card.ts
@@ -1,3 +1,10 @@
+/**
+ * Cards are used to group similar concepts and tasks together to make Shopify easier for merchants to scan, read, and get things done.
+ * @property title Title content for the card
+ * @property primaryFooterAction Primary action for the card footer
+ * @property secondaryFooterActions Secondary actions for the card footer
+ * @property children Inner content of the card
+ */
 interface Card {
     title?: string;
     primaryFooterAction?: Action;
--- components/Text.ts
+++ components/Text.ts
@@ -1,20 +1,31 @@
-enum Variation {
-    Subdued, // grey
-    Strong,  // bold
-    Positive, // green
-    Negative, // red
-    Code // monospace
-}
-
 enum TextSize {
-    Small,
-    Medium,
-    Large,
-    ExtraLarge
+    Small, Medium, TitleSmall, TitleMedium, TitleLarge, TitleExtraLarge
 }
 
+enum Alignment {
+    Left, Right, Center, Justify
+}
+
+enum Style {
+    Normal, Italic, Monospace, Bold
+}
+
+enum Color {
+    Primary, Secondary, Warning, Error, Success
+}
+
+/**
+ * Text style enhances text with additional visual meaning. For example, using subdued text to de-emphasize it from its surrounding text.
+ * @property text Text value to display
+ * @property size Size of the text. Defaults to Medium
+ * @property style The style of the font 
+ * @property color The color of the text. Defaults to Primary
+ * @property alignment Text alignment. Defaults to left aligned
+ */
 interface Text {
     text: string;
     size?: TextSize;
-    variation?: Variation
+    style?: Style;
+    color?: Color;
+    alignment?: Alignment;
 }
