8fce4b779cc2be5ac786f6097d0d40277a82ab81
David Muzi <davidmuzi@gmail.com>
2020-02-25 09:54:04 -0500

    use child for text

--- components/Stack.ts
+++ components/Stack.ts
@@ -2,7 +2,7 @@
  * Use to lay out a horizontal row of components or to achieve no-fuss vertical centering. A stack is made of flexible items that wrap each of the stack’s children. Options provide control of the wrapping, spacing, and relative size of the items in the stack.
  * @property vertical Stack the elements vertically. Defaults to Horizontal
  * @property distribution Horizontal alignment of elements. Defaults to Leading
- * @property spacing Spacing between elements. Defaults to Default
+ * @property spacing Spacing between elements
  * @property children Elements to display inside the stack
  */
 interface Stack {
@@ -13,17 +13,9 @@ interface Stack {
 }
 
 enum Distribution {
-    Leading,
-    Trailing,
-    Center,
-    Fill
+    Leading, Trailing, Center, Fill
 }
 
 enum Spacing {
-    ExtraTight,
-    Tight,
-    Default,
-    Loose,
-    ExtraLoose,
-    None
+    None, ExtraTight, Tight, Loose, ExtraLoose, 
 }
--- components/Text.ts
+++ components/Text.ts
@@ -1,3 +1,19 @@
+/**
+ * Text style enhances text with additional visual meaning. For example, using subdued text to de-emphasize it from its surrounding text.
+ * @property text Text value to display
+ * @property size Size of the text. Defaults to Medium
+ * @property style The style of the font 
+ * @property color The color of the text. Defaults to Primary
+ * @property alignment Text alignment. Defaults to left aligned
+ */
+interface Text {
+    children: React.ReactNode;
+    size?: TextSize;
+    style?: Style;
+    color?: Color;
+    alignment?: Alignment;
+}
+
 enum TextSize {
     Small, Medium, TitleSmall, TitleMedium, TitleLarge, TitleExtraLarge
 }
@@ -12,20 +28,4 @@ enum Style {
 
 enum Color {
     Primary, Secondary, Warning, Error, Success
-}
-
-/**
- * Text style enhances text with additional visual meaning. For example, using subdued text to de-emphasize it from its surrounding text.
- * @property text Text value to display
- * @property size Size of the text. Defaults to Medium
- * @property style The style of the font 
- * @property color The color of the text. Defaults to Primary
- * @property alignment Text alignment. Defaults to left aligned
- */
-interface Text {
-    text: string;
-    size?: TextSize;
-    style?: Style;
-    color?: Color;
-    alignment?: Alignment;
 }
