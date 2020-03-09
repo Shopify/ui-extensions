b4868dab8c75cd2d769eaa59674e93358d5015fb
Henry Tao <henry.tao@shopify.com>
2020-03-09 14:21:37 -0400

    chore: resolve comments

--- packages/app-extensions-playground/src/components/AppExtension/workers/mobile.ts
+++
@@ -1,5 +0,0 @@
-import {render} from './worker';
-
-export default (extensionPoint: any, data: any, components: any, channel: any) => {
-  return render(extensionPoint, data, components, channel);
-};
--- packages/app-extensions-playground/webpack.mobile.config.js
+++ packages/app-extensions-playground/webpack.mobile.config.js
@@ -1,10 +1,8 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
 const defaultConfig = require('./webpack.config');
 
 module.exports = {
   ...defaultConfig,
-  entry: './src/components/AppExtension/workers/mobile.ts',
+  entry: './src/components/AppExtension/workers/worker.ts',
   output: {
     globalObject: 'self',
     filename: 'mobile.js',
