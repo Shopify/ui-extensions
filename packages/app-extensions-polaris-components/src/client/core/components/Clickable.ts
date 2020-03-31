3f24a27e9e7170d06aaa820b9b019bba0ee8525b
Henry Tao <henry.tao@shopify.com>
2020-03-31 15:13:54 -0400

    chore: add tests for Clickable component

--- packages/app-extensions-polaris-components/src/client/core/components/Clickable.ts
+++ packages/app-extensions-polaris-components/src/client/core/components/Clickable.ts
@@ -2,6 +2,7 @@ import {createRemoteComponent} from '@shopify/remote-ui-core';
 
 export interface ClickableProps {
   onClick: () => void;
+  children?: React.ReactNode;
 }
 
 export const Clickable = createRemoteComponent<'Clickable', ClickableProps>('Clickable');
