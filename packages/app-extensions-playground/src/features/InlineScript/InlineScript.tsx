e8cbde52dda60c3715e29a7361da8897376d8f25
Trish Ta <trish.ta@shopify.com>
2020-04-23 14:16:41 -0400

    chore: create new page for inline script

--- packages/app-extensions-playground/src/features/InlineScript/InlineScript.tsx
+++ packages/app-extensions-playground/src/features/InlineScript/InlineScript.tsx
@@ -3,14 +3,23 @@ import {Page} from '@shopify/polaris';
 import {usePerformanceMark} from '@shopify/react-performance';
 import {ExtensionPoint} from '@shopify/app-extensions-renderer';
 import {host} from '@shopify/app-extensions-polaris-components';
-import {AppExtension} from '../components';
+import {AppExtension} from '../../components';
+import {createPlainWorkerFactory} from '@shopify/web-worker';
+
+const reactThirdPartyWorker = createPlainWorkerFactory(() =>
+  import(/* webpackChunkName: '3p-inline-script' */ '../../third-party/inline-script'),
+);
 
 export function InlineScript() {
   const [inlineScript, setInlineScript] = useState<string | undefined>();
 
   useEffect(() => {
     async function getDefaultScript() {
-      const results = await fetch('../../../inline-script.txt').then(response => {
+      if (!reactThirdPartyWorker.url) {
+        return;
+      }
+      const {url} = reactThirdPartyWorker;
+      const results = await fetch(url.href).then(response => {
         return response.text();
       });
       setInlineScript(results);
