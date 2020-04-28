19c656c4640ee6cc7af34768571a0c4c03182a1d
Henry Tao <henry.tao@shopify.com>
2020-04-28 17:07:11 -0400

    chore: clean up code

--- packages/app-extensions-polaris-components/src/host/components/Spinner/Spinner.tsx
+++ packages/app-extensions-polaris-components/src/host/components/Spinner/Spinner.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Spinner as PolarisSpinner} from '@shopify/polaris';
 
-import {SpinnerProps} from '../../../client/core';
-
-export default function Spinner(_: SpinnerProps) {
+export default function Spinner() {
   return <PolarisSpinner />;
 }
