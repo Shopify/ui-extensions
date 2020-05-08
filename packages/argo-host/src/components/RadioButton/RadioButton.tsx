75abb81695c3ebac016aedce1f15ce90425b45fb
Hassan Naeem <hnae6443@gmail.com>
2020-05-08 10:49:23 -0400

    feat: radiobutton cleanup

--- packages/argo-host/src/components/RadioButton/RadioButton.tsx
+++ packages/argo-host/src/components/RadioButton/RadioButton.tsx
@@ -3,7 +3,7 @@ import {RadioButtonProps} from '@shopify/argo/components';
 import {RadioButton as PolarisRadioButton} from '@shopify/polaris';
 
 export default function RadioButton({label = '', onChange, ...props}: RadioButtonProps) {
-  const polarisOnChange = useCallback((checked, newValue) => onChange(newValue), [onChange]);
+  const polarisOnChange = useCallback((_checked, newValue) => onChange(newValue), [onChange]);
 
   return <PolarisRadioButton {...props} label={label} onChange={polarisOnChange} />;
 }
