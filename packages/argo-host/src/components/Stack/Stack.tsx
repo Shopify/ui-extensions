de9fca35d42655e6e3783ea8a43b18fdd221c3db
Henry Tao <henry.tao@shopify.com>
2020-05-12 16:23:03 -0400

    fix: simplify stack component to remove forcing key prop

--- packages/argo-host/src/components/Stack/Stack.tsx
+++ packages/argo-host/src/components/Stack/Stack.tsx
@@ -1,9 +1,6 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {StackProps} from '@shopify/argo/components';
 import {Stack as PolarisStack} from '@shopify/polaris';
-
-import {elementChildren, getElementProps, isElementOfType} from '../../utilities/components';
-import StackItem from '../StackItem';
 
 export default function Stack({
   children,
@@ -13,20 +10,6 @@ export default function Stack({
   vertical,
   wrap,
 }: StackProps) {
-  const itemMarkup = useMemo(
-    () =>
-      elementChildren(children).map((child, index) => {
-        if (isElementOfType(child, StackItem)) {
-          return (
-            <PolarisStack.Item key={index} {...getElementProps(child)}>
-              {child}
-            </PolarisStack.Item>
-          );
-        }
-        return child;
-      }),
-    [children],
-  );
   return (
     <PolarisStack
       alignment={alignment}
@@ -35,7 +18,7 @@ export default function Stack({
       vertical={vertical}
       wrap={wrap}
     >
-      {itemMarkup}
+      {children}
     </PolarisStack>
   );
 }
--- packages/argo-host/src/utilities/components.tsx
+++ packages/argo-host/src/utilities/components.tsx
@@ -2,28 +2,6 @@ import React, {useMemo} from 'react';
 
 import {Action} from '@shopify/argo/types';
 
-// Checks whether `element` is a React element of type `Component` (or one of
-// the passed components, if `Component` is an array of React components).
-export function isElementOfType<P>(
-  element: React.ReactNode | null | undefined,
-  Component: React.ComponentType<P> | React.ComponentType<P>[],
-): boolean {
-  if (element == null || !React.isValidElement(element) || typeof element.type === 'string') {
-    return false;
-  }
-
-  const {type} = ((element.props || {}) as any).component || {};
-  const Components = Array.isArray(Component) ? Component : [Component];
-
-  return Components.some(AComponent => type === AComponent.name);
-}
-
-// Returns props of element specificially for Remote UI component. It might not work with regular React element.
-export function getElementProps(element: React.ReactNode | null | undefined) {
-  const {props} = (((element || {}) as any).props || {}).component || {};
-  return props || {};
-}
-
 // Returns all children that are valid elements as an array. Can optionally be
 // filtered by passing `predicate`.
 export function elementChildren<T extends React.ReactElement<{}>>(
