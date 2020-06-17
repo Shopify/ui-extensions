2d2c285549d248e308fd294f50df770be913eb8c
Henry Tao <henry.tao@shopify.com>
2020-06-17 14:02:56 -0400

    chore: fix resource-list example in playground

--- packages/argo-playground/src/third-party/components-list/resource-list.tsx
+++ packages/argo-playground/src/third-party/components-list/resource-list.tsx
@@ -3,6 +3,7 @@ import {Card, ResourceList, ResourceItem, Stack, Checkbox, StackItem, Text} from
 
 export function ResourceListExample() {
   const dataList = [1, 2, 3, 4, 5, 12, 13, 145];
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [listItems, setListItems] = useState(dataList);
 
   const [resourceListQuery, setResourceListQuery] = useState('');
@@ -28,10 +29,17 @@ export function ResourceListExample() {
           <ResourceItem
             key={index}
             id={`${index}`}
-            onClick={() => console.log('ResourceList item click:', item)}
+            onClick={() => {
+              console.log('ResourceList item toggle:', item);
+              if (selectedItems.includes(item)) {
+                setSelectedItems(selectedItems.filter((o) => o !== item));
+              } else {
+                setSelectedItems(selectedItems.concat(item));
+              }
+            }}
           >
             <Stack alignment="center">
-              <Checkbox />
+              <Checkbox checked={selectedItems.includes(item)} />
               <StackItem fill>Every {item} week or 15 days * 20-25% off</StackItem>
               <Text>{item} product</Text>
             </Stack>
