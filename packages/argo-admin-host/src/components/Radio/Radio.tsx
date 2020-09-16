2b196fab0854877ac18548992d988a15e25b013a
Alan Thai <alan.thai@shopify.com>
2020-09-16 13:42:43 -0400

    feat: radio id and checked is optional

--- packages/argo-admin-host/src/components/Radio/Radio.tsx
+++ packages/argo-admin-host/src/components/Radio/Radio.tsx
@@ -1,9 +1,20 @@
-import React, {useCallback} from 'react';
+import React, {useRef, useCallback} from 'react';
 import {RadioProps} from '@shopify/argo-admin';
 import {RadioButton as PolarisRadio} from '@shopify/polaris';
 
-export default function RadioButton({label = '', onChange, value, ...props}: RadioProps) {
-  const polarisOnChange = useCallback(() => onChange(value), [onChange, value]);
+let nextUniqueId = 0;
 
-  return <PolarisRadio {...props} label={label} onChange={polarisOnChange} />;
+export default function Radio({label = '', value, onChange, id, ...props}: RadioProps) {
+  const polarisOnChange = useCallback(() => onChange(value), [onChange, value]);
+  const {current: uniqueId} = useRef(`argo-radio-${nextUniqueId++}`);
+
+  return (
+    <PolarisRadio
+      {...props}
+      id={id || uniqueId}
+      label={label}
+      value={value}
+      onChange={polarisOnChange}
+    />
+  );
 }
