a6947ee3c092f90ccee6bd313638e388d86dead3
Alan Thai <alan.thai@shopify.com>
2020-10-08 11:43:05 -0400

    fix: button with icon is centered when displayed without text (#1010)

--- packages/argo-admin/src/components/Button.ts
+++ packages/argo-admin/src/components/Button.ts
@@ -2,12 +2,20 @@ import {createRemoteComponent} from '@remote-ui/core';
 
 import {IconProps} from './Icon';
 
-export interface ButtonProps {
-  title: string;
+type RequiredTitleOrIcon =
+  | {
+      title: string;
+      icon?: IconProps;
+    }
+  | {
+      title?: string;
+      icon: IconProps;
+    };
+
+export type ButtonProps = RequiredTitleOrIcon & {
   primary?: boolean;
-  icon?: IconProps;
   onPress?(): void;
   disabled?: boolean;
-}
+};
 
 export const Button = createRemoteComponent<'Button', ButtonProps>('Button');
