95468972560205519ccf51f1f8be1dbe34bafc5d
Alan Thai <alan.thai@shopify.com>
2020-10-09 10:48:53 -0400

    fix: text-field onchange error (#1015)
    onchange and onclearbuttonpress were out of sync

--- packages/argo-admin-host/src/utilities/useQueuedState.ts
+++ packages/argo-admin-host/src/utilities/useQueuedState.ts
@@ -7,24 +7,20 @@ export function useQueuedState(
   onChange: (v: FieldValue) => void | Promise<FieldValue>,
 ): [string, (s: string) => void] {
   const [localValue, setLocalValue] = useState<FieldValue>('');
-  const [queue, setQueue] = useState<FieldValue[]>([]);
+  const [queue, setQueue] = useState<FieldValue | undefined>();
   const [processing, setProcessing] = useState(false);
 
   useEffect(() => {
     setProcessing(false);
-    if (localValue !== value && !queue.length) setLocalValue(value);
-    const reducedArray = [...queue];
-    reducedArray.shift();
-    setQueue(reducedArray);
-    if (reducedArray[0]) onChange(reducedArray[0]);
+    if (localValue !== value && !queue) setLocalValue(value);
+    if (queue != null) onChange(queue);
+    setQueue(undefined);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value]);
 
   const queuedOnChange = (newValue: FieldValue) => {
     setLocalValue(newValue);
-    const addedArray = [...queue];
-    addedArray.push(newValue);
-    setQueue(addedArray);
+    setQueue(newValue);
     if (!processing) {
       setProcessing(true);
       onChange(newValue);
