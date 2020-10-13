b2ccb1aca46e0deadbdc388eef0be4adda60daa1
Alan Thai <alan.thai@shopify.com>
2020-10-13 17:07:06 -0400

    fix: queued state (#1027)

--- packages/argo-admin-host/src/components/TextField/TextField.tsx
+++ packages/argo-admin-host/src/components/TextField/TextField.tsx
@@ -30,7 +30,19 @@ export default function TextField({
     onBlur();
   }, [onBlur, onChange, appliedValue]);
   const polarisOnFocus = useCallback(() => onFocus(), [onFocus]);
-  const polarisOnClearButtonClick = useCallback(() => onClearButtonPress?.(), [onClearButtonPress]);
+  const polarisOnClearButtonClick = useCallback(() => {
+    /**
+     This is a race condition happening when the client sends more than one update to the host in short amount of time. The host (remote-ui) only notifies the latest update to the UI. The `useQueueState` has `useEffect` which doesn't run in this case because latest value is same as previous value. This is most likely happened because of the `useQueueState` hook for TextField only. A temporary workaround is to handle only this specific use case where:
+      - TextField doesn't have onInput
+      - Client value is empty
+      - Host value is not empty
+      - Clear button is tapped (touch on trackpad, not click)
+    */
+    if (!onInput && !value.length && appliedValue.length) {
+      polarisOnChange('');
+    }
+    onClearButtonPress?.();
+  }, [onClearButtonPress, onInput, appliedValue, value, polarisOnChange]);
 
   return (
     <PolarisTextField
