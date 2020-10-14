991e362541e0179609c3c97759a9e58cbce4f1db
Alan Thai <alan.thai@shopify.com>
2020-10-14 16:54:57 -0400

    chore: upgrade remote-ui/react 2.0.0 (#1021)

--- packages/argo-admin-react/src/tests/api.test.tsx
+++ packages/argo-admin-react/src/tests/api.test.tsx
@@ -1,45 +1,40 @@
 import React from 'react';
-import {ExtensionPoint} from '@shopify/argo-admin';
-import {render as remoteRender} from '@remote-ui/react';
 
 import {render} from '../api';
 import {ExtensionApiContext} from '../extension-api/utils';
 
 jest.mock('@remote-ui/react', () => ({
-  ...require.requireActual('@remote-ui/react'),
+  ...jest.requireActual('@remote-ui/react'),
   render: jest.fn(),
 }));
 
 jest.mock('react', () => ({
-  ...require.requireActual('react'),
+  ...jest.requireActual('react'),
   createElement: jest.fn(),
 }));
 
 jest.mock('@shopify/argo-admin', () => ({
-  ...require.requireActual('@shopify/argo-admin'),
+  ...jest.requireActual('@shopify/argo-admin'),
   extend: jest.fn(),
 }));
 
-const extend = require.requireMock('@shopify/argo-admin').extend;
-const reactCreateElement = require.requireMock('react').createElement;
+const reactCreateElement = jest.requireMock('react').createElement;
+const remoteRender = jest.requireMock('@remote-ui/react').render;
 
 describe('extend()', () => {
   it('calls remote render with generated element', () => {
     reactCreateElement.mockReset();
 
     const element = <div />;
-    extend(
-      'Playground',
-      render(() => element),
-    );
-    const root = {};
+
+    const root = {mount: jest.fn()};
     const api = {locale: 'en'};
 
     const createdElement = <p />;
 
     reactCreateElement.mockImplementation(() => createdElement);
 
-    const extendCallback = extend.mock.calls[0][1];
+    const extendCallback = render(() => element);
     extendCallback(root, api);
 
     expect(reactCreateElement).toHaveBeenCalledWith(
@@ -47,6 +42,11 @@ describe('extend()', () => {
       {value: api},
       element,
     );
-    expect(remoteRender).toHaveBeenCalledWith(createdElement, root);
+    expect(remoteRender).toHaveBeenCalledWith(createdElement, root, expect.any(Function));
+
+    const renderCallback = remoteRender.mock.calls[0][2];
+    renderCallback();
+
+    expect(root.mount).toHaveBeenCalled();
   });
 });
