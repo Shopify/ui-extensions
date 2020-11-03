00f754aea7503fe7c5297f9d18d1cbbeda9cb960
Robin Drexler <robin.drexler@shopify.com>
2020-11-03 13:28:21 -0500

    include node_modules when transpiling

--- packages/argo-run/src/webpack-config.ts
+++ packages/argo-run/src/webpack-config.ts
@@ -84,7 +84,7 @@ export function createWebpackConfiguration({
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.(jsx?|esnext)$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
@@ -99,7 +99,8 @@ export function createWebpackConfiguration({
           },
         },
         {
-          test: /\.esnext$/,
+          test: /\.(js|esnext)$/,
+          include: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
