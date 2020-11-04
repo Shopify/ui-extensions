fd666f1ad721bdaeac89fd0e18670c1446c4876a
Henry Tao <henry.tao@shopify.com>
2020-11-04 16:26:50 -0500

    chore: fix test

--- packages/argo-admin-host/src/worker/test/sandbox.test.ts
+++ packages/argo-admin-host/src/worker/test/sandbox.test.ts
@@ -5,14 +5,17 @@ describe('sandbox', () => {
     const target = {
       foo: jest.fn(),
       bar: jest.fn(),
+      xyz: jest.fn(),
     };
     const denylist = {
       foo: true,
+      xyz: false,
     };
     applySandbox(target, denylist);
 
     expect(target.foo).toBeUndefined();
     expect(target.bar).toBe(target.bar);
+    expect(target.xyz).toBe(target.xyz);
   });
 
   it('applies builtIns denylist to target object', () => {
