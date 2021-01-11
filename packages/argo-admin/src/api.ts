e6bf420333e17a039104cea84a99dcbea75aaab8
Elana Kopelevich <elana.kopelevich@shopify.com>
2021-01-11 14:46:04 -0700

    chore: remove unused types with naming conflict (StandardApi)

--- packages/argo-admin/src/api.ts
+++ packages/argo-admin/src/api.ts
@@ -16,19 +16,6 @@ export interface ShopifyGlobal {
   readonly shopify: ShopifyApi;
 }
 
-// For external consumption
-export type StandardApi<T extends ExtensionPoint> = Omit<
-  ExtensionApi[T],
-  'locale' | 'layoutApi' | 'sessionToken'
->;
-
-export interface RenderExtensionComponentProps<T extends ExtensionPoint> {
-  extensionPoint: T;
-  script?: string | URL;
-  api?: StandardApi<T>;
-  components?: {[key: string]: any};
-}
-
 export function extend<T extends ExtensionPoint>(
   extensionPoint: T,
   callback: ExtensionPointCallback[T],
