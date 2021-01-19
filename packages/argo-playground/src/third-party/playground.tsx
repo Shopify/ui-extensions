6375f6219d8c428d40273a32cf7c8cae50094103
Henry Tao <henry.tao@shopify.com>
2021-01-19 08:19:59 -0500

    chore: update playground

--- packages/argo-playground/src/third-party/playground.tsx
+++ packages/argo-playground/src/third-party/playground.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import {extend, render, Card} from '@shopify/argo-admin-react';
+import {extend, render, Text} from '@shopify/argo-admin-react';
 
 function App() {
-  return (
-    <Card title="Hello world" sectioned>
-      From 3p code
-    </Card>
-  );
+  return <Text size="titleMedium">From 3p code</Text>;
 }
 
 extend(
   'Playground',
+  render(() => <App />),
+);
+extend(
+  'Admin::Product::SubscriptionPlan::Add',
+  render(() => <App />),
+);
+extend(
+  'Admin::Product::SubscriptionPlan::Create',
+  render(() => <App />),
+);
+extend(
+  'Admin::Product::SubscriptionPlan::Edit',
+  render(() => <App />),
+);
+extend(
+  'Admin::Product::SubscriptionPlan::Remove',
   render(() => <App />),
 );
