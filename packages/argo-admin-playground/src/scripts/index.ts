351159dc35408d55b5500d4e9a23c5d016acc2b9
Henry Tao <henry.tao@shopify.com>
2021-03-15 15:41:49 -0400

    chore: fix script name

--- packages/argo-admin-playground/src/scripts/index.ts
+++ packages/argo-admin-playground/src/scripts/index.ts
@@ -1,5 +1,6 @@
 interface Script {
   label: string;
+  name: string;
   path: string;
   excludeFromManifest?: boolean;
 }
@@ -7,26 +8,32 @@ interface Script {
 const scripts: Script[] = [
   {
     label: 'Playground',
+    name: '3p-playground.worker',
     path: './scripts/playground.tsx',
   },
   {
     label: 'Components List',
+    name: '3p-components-list.worker',
     path: './scripts/components-list/index.tsx',
   },
   {
     label: 'Inline Script',
+    name: '3p-inline-script.worker',
     path: './scripts/inline-script.tsx',
   },
   {
     label: 'Use Form',
+    name: '3p-use-form.worker',
     path: './scripts/use-form.tsx',
   },
   {
     label: 'Vanilla',
+    name: '3p-vanilla.worker',
     path: './scripts/vanilla.ts',
   },
   {
     label: 'SubscriptionContent',
+    name: '3p-subscription-content.worker',
     path: './scripts/subscription-content.tsx',
     excludeFromManifest: true,
   },
