9298f31427ae56166edc9db8690ccc7ee7c9458b
Joey Freund <joey.freund@shopify.com>
2021-03-25 17:12:29 -0400

    chore: if apiKey is specified multiple times, use the last value

--- packages/argo-admin-cli/src/cli.ts
+++ packages/argo-admin-cli/src/cli.ts
@@ -11,16 +11,20 @@ import {server} from './server';
       'server',
       'run local server',
       (_yargs) =>
-        _yargs.options({
-          type: {type: 'string', demandOption: true},
-          entry: {type: 'string', demandOption: true},
-          port: {type: 'number', demandOption: true},
-          env: {type: 'string'},
-          apiKey: {type: 'string'},
-          name: {type: 'string'},
-          resourceUrl: {type: 'string'},
-          shop: {type: 'string'},
-        }),
+        _yargs
+          .parserConfiguration({
+            'duplicate-arguments-array': false,
+          })
+          .options({
+            type: {type: 'string', demandOption: true},
+            entry: {type: 'string', demandOption: true},
+            port: {type: 'number', demandOption: true},
+            env: {type: 'string'},
+            apiKey: {type: 'string'},
+            name: {type: 'string'},
+            resourceUrl: {type: 'string'},
+            shop: {type: 'string'},
+          }),
       server,
     )
     .command(
