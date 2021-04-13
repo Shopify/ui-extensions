5377183ee54b648e9295787c79778f74a3fe4c94
Elana Kopelevich <elana.kopelevich@shopify.com>
2021-04-13 17:39:18 -0600

    feat: add VisuallyHidden

---
+++ .github/pull_request_template.md
@@ -0,0 +1,16 @@
+### Background
+
+(Provide a link to any relevant issues AND provide a TLDR of the issue in this pull request)
+
+### Solution
+
+(Describe your solution, why this approach was chosen, and what the alternatives/impacts may be)
+
+### 🎩
+
+- ...
+
+### Checklist
+
+- [ ] It is safe to simply rollback this change
+- [ ] I have :tophat:'d these changes
--- packages/argo-admin-playground/src/scripts/components-list/index.tsx
+++ packages/argo-admin-playground/src/scripts/components-list/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import {extend, render} from '@shopify/argo-admin-react';
 
-import {CheckboxExample} from './checkbox';
 import {BadgeExample} from './badge';
 import {BannerExample} from './banner';
 import {ButtonExample} from './button';
-import {ExtensionApiExample} from './extension-api';
-import {RadioExample} from './radio';
 import {CardExample} from './card';
-import {PressableExample} from './pressable';
-import {ModalExample} from './modal';
-import {TextFieldExample} from './text-field';
-import {TextExample} from './text';
-import {StackExample} from './stack';
-import {ResourceListExample} from './resource-list';
-import {SelectExample} from './select';
+import {CheckboxExample} from './checkbox';
+import {ExtensionApiExample} from './extension-api';
 import {IconExample} from './icon';
 import {LinkExample} from './link';
+import {ModalExample} from './modal';
+import {PressableExample} from './pressable';
+import {RadioExample} from './radio';
+import {ResourceListExample} from './resource-list';
+import {SelectExample} from './select';
 import {SpinnerExample} from './spinner';
+import {StackExample} from './stack';
+import {TextFieldExample} from './text-field';
+import {TextExample} from './text';
+import {VisuallyHiddenExample} from './visuallyHidden';
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
       <StackExample />
       <TextExample />
       <TextFieldExample />
+      <VisuallyHiddenExample />
       <ExtensionApiExample />
     </>
   );
