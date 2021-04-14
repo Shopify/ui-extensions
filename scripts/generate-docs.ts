0b8cac707ef6e15c076eb6a2b96609c0da23abb6
Iain Campbell <iain.campbell@shopify.com>
2021-04-14 17:11:30 -0400

    Genericize renderExamples

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -4,7 +4,7 @@ const paths = {
   root: '../checkout-web/packages/argo-checkout',
   devDocsRoot: '../shopify-dev/content/tools/argo-checkout',
   packages: {
-    JavaScript: '../checkout-web/packages/argo-checkout',
+    JS: '../checkout-web/packages/argo-checkout',
     React: '../checkout-web/packages/argo-checkout-react',
   },
   shopifyDevUrl: '/tools/argo-checkout',
