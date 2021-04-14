31e35dd2a6ef6bebff8075e5d3627f3676c43444
Iain Campbell <iain.campbell@shopify.com>
2021-04-14 17:35:05 -0400

    genericize getAdditionalContentFor, renaming

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,8 +1,8 @@
 import {renderForShopifyDev} from './typedoc/typedoc';
 
 const paths = {
-  root: '../checkout-web/packages/argo-checkout',
-  devDocsRoot: '../shopify-dev/content/tools/argo-checkout',
+  inputRoot: '../checkout-web/packages/argo-checkout',
+  outputRoot: '../shopify-dev/content/tools/argo-checkout',
   packages: {
     JS: '../checkout-web/packages/argo-checkout',
     React: '../checkout-web/packages/argo-checkout-react',
