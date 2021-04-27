b55fcf432bdab3c380ac3ea0a15c13e351f763b9
Trish Ta <trish.ta@shopify.com>
2021-04-26 20:30:47 -0400

    [Admin]: Use same build and test settings as Checkout
    (argo-admin-react): put back api.test.tsx

--- packages/argo-webpack-hot-client/sewing-kit.config.ts
+++ packages/argo-webpack-hot-client/sewing-kit.config.ts
@@ -1,8 +1,8 @@
 import {createPackage, Runtime} from '@sewing-kit/config';
-import {argoCheckoutPackage} from '../../config/sewing-kit';
+import {argoPackage} from '../../config/sewing-kit';
 
 export default createPackage((pkg) => {
   pkg.entry({root: './src/index', runtime: Runtime.Node});
   pkg.entry({name: 'worker', root: './src/worker', runtime: Runtime.WebWorker});
-  pkg.use(argoCheckoutPackage());
+  pkg.use(argoPackage());
 });
