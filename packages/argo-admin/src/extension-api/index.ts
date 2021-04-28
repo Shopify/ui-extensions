d44c962923765c91fa7df58b7f45746c2c912827
Trish Ta <trish.ta@shopify.com>
2021-04-28 16:44:07 -0400

    Update extensions-api/index.ts so that it works with tsdocs

--- packages/argo-admin/src/extension-api/index.ts
+++ packages/argo-admin/src/extension-api/index.ts
@@ -1,6 +1,19 @@
-export * from './layout';
-export * from './locale';
-export * from './container';
-export * from './data';
-export * from './sessionToken';
-export * from './toast';
+export type {LayoutApi, Layout} from './layout';
+export {isLayoutApi} from './layout';
+
+export type {LocaleApi} from './locale';
+export {isLocaleApi} from './locale';
+
+export type {ContainerApi, ExtensionContainer} from './container';
+
+export {isContainerApi} from './container';
+
+export type {DataApi, ExtensionData} from './data';
+
+export {isDataApi} from './data';
+
+export type {SessionTokenApi} from './sessionToken';
+export {isSessionTokenApi} from './sessionToken';
+
+export type {ToastApi} from './toast';
+export {isToastApi} from './toast';
