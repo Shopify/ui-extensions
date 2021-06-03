8570ef1463751ee775d63e2ea7eb118a9afa99be
Iain Campbell <iain.campbell@shopify.com>
2021-06-03 15:19:42 -0400

    Use 'export type'

--- packages/argo-admin-react/src/index.ts
+++ packages/argo-admin-react/src/index.ts
@@ -2,5 +2,7 @@ export * from '@shopify/argo-admin/extension-points';
 export * from '@shopify/argo-admin/extension-api';
 
 export * from './api';
+export type {ShopifyApi, RenderCallback} from './api';
+
 export * from './components';
 export * from './extension-api';
