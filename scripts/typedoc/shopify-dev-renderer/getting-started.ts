9cb24b6e9dc84fd708b8ed3dbf675626a2428f6c
Tim Layton <tmlayton@users.noreply.github.com>
2021-06-09 14:08:53 -0700

    this problem should be fixed in shopify-dev

--- scripts/typedoc/shopify-dev-renderer/getting-started.ts
+++ scripts/typedoc/shopify-dev-renderer/getting-started.ts
@@ -50,11 +50,9 @@ export function gettingStarted(paths: Paths) {
     ` [\`Checkout::Feature::Render\`](${paths.shopifyDevUrl}/extension-points/api)`,
   );
   // Add a link to the PP extension point API
-  // Using an anchor tag because shopify-dev seems to be stripping
-  // markdown links which start with "/api"
   markdown = markdown.replace(
-    '[`Checkout::PostPurchase::ShouldRender` and `Checkout::PostPurchase::Render`](../src/extension-points/api/post-purchase)',
-    `<a href="${paths.shopifyDevUrl}/extension-points/api">\`Checkout::PostPurchase::ShouldRender\` and \`Checkout::PostPurchase::Render\`</a>`,
+    '../src/extension-points/api/post-purchase',
+    `${paths.shopifyDevUrl}/extension-points/api`,
   );
   markdown = markdown.replace(
     '#components',
