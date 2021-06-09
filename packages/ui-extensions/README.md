e660ecfbe656861ea4379b3b2d7312fe29556318
Elana Kopelevich <elana.kopelevich@shopify.com>
2021-06-09 13:51:10 -0600

    Add shared ui-extensions package, BaseBannerProps, and use in Admin Banner

---
+++ packages/ui-extensions/README.md
@@ -0,0 +1,3 @@
+# `@shopify/ui-extensions`
+
+This package contains shared types and utilities for all of the other ui-extensions packages in this repo.
---
+++ packages/ui-extensions/src/types/Banner.ts
@@ -0,0 +1,12 @@
+export type Status = 'critical' | 'info' | 'success' | 'warning';
+
+export interface BaseBannerProps {
+  /** Callback fired when banner is dismissed. */
+  onDismiss?: () => void;
+
+  /** Visual treatment of the banner based on message purpose. */
+  status?: Status;
+
+  /** Title of the banner. */
+  title?: string;
+}
