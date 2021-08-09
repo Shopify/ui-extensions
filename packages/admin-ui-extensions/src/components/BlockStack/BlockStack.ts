10b89274422b4430d2a67cc042300e7a180fdbc8
Olavo Santos <olavo.santos@shopify.com>
2021-08-09 15:52:13 -0500

    fix: fixed typo on BlockStack doc in admin-ui-extensions

--- packages/admin-ui-extensions/src/components/BlockStack/BlockStack.ts
+++ packages/admin-ui-extensions/src/components/BlockStack/BlockStack.ts
@@ -7,7 +7,7 @@ export type {BaseBlockStackProps as BlockStackProps} from '@shopify/ui-extension
  * Use to achieve no-fuss vertical centering.
  *
  * A stack is made of flexible items that wrap each of the stack’s children. Options provide control of the alignment and spacing of the items in the stack.
- * Use `StackItem` to group multiple elements inside a `Stack` together.
+ * Use `StackItem` to group multiple elements inside a `BlockStack` together.
  */
 export const BlockStack = createRemoteComponent<
   'BlockStack',
