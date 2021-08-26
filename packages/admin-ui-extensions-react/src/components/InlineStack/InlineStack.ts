d7a971eb99797f65540202e7ce49a2c8501fa742
Olavo Santos <olavo.santos@shopify.com>
2021-08-26 10:08:14 -0500

    feat: added the InlineStack component to admin-ui-extensions-react

---
+++ packages/admin-ui-extensions-react/src/components/InlineStack/InlineStack.ts
@@ -0,0 +1,4 @@
+import {InlineStack as BaseInlineStack} from '@shopify/admin-ui-extensions';
+import {createRemoteReactComponent} from '@remote-ui/react';
+
+export const InlineStack = createRemoteReactComponent(BaseInlineStack);
