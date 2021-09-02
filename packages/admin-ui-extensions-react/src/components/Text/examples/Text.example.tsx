f282e12cc77007cff1e13b54322139b80eb07c30
Elana Kopelevich <elana.kopelevich@shopify.com>
2021-09-02 11:17:41 -0600

    feat: update Text component API

--- packages/admin-ui-extensions-react/src/components/Text/examples/Text.example.tsx
+++ packages/admin-ui-extensions-react/src/components/Text/examples/Text.example.tsx
@@ -4,7 +4,13 @@ import {extend, render, Text} from '@shopify/admin-ui-extensions-react';
 function App() {
   return (
     <>
-      <Text size="small" style="italic" color="success" alignment="center">
+      <Text size="small"
+        emphasized
+        subdued
+        role='address'
+        id='some_id'
+        appearance='accent'
+      >
         Formatted text is awesome!
       </Text>
       This simple text is a bit less awesome
