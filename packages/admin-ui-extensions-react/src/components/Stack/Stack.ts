38eb4d4631f825ffe03ea1315b3f0498e4735324
Olavo Santos <olavo.santos@shopify.com>
2021-09-08 10:30:39 -0500

    chore: removed admin-ui-extensions' Stack component

--- packages/admin-ui-extensions-react/src/components/Stack/Stack.ts
+++
@@ -1,4 +0,0 @@
-import {Stack as BaseStack} from '@shopify/admin-ui-extensions';
-import {createRemoteReactComponent} from '@remote-ui/react';
-
-export const Stack = createRemoteReactComponent(BaseStack);
--- packages/admin-ui-extensions-react/src/components/Stack/index.ts
+++
@@ -1,5 +0,0 @@
-import type {PropsWithChildren} from 'react';
-import type {StackProps as BaseStackProps} from '@shopify/admin-ui-extensions';
-
-export type StackProps = PropsWithChildren<BaseStackProps>;
-export {Stack} from './Stack';
