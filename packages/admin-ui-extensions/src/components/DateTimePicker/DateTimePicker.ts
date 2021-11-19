4381c1c5f3968ab168c2af4700fed07f0416c8eb
Elana Kopelevich <elana.kopelevich@shopify.com>
2021-11-19 10:48:49 -0700

    Add DateTimePicker API

--- packages/admin-ui-extensions/src/components/DateTimePicker/DateTimePicker.ts
+++ packages/admin-ui-extensions/src/components/DateTimePicker/DateTimePicker.ts
@@ -1,12 +1,18 @@
 import {createRemoteComponent} from '@remote-ui/core';
 
 export interface DateTimePickerProps {
-  /** Option to display calendar for Date or clock for time */
-  type: 'date' | 'time';
+  dateLabel: string;
+  timeLabel: string;
+  value: string;
+  onChange(newValue: string): void;
+  error?: string;
+  disableDatesBefore?: string;
+  disableDatesAfter?: string;
+  disabled?: boolean;
 }
 
 /**
- * DateTimePicker is text fields with a popover calender or clock that allow users to select dates and/or times
+ * DateTimePicker is a set of text fields with popover a calender and a time selector that allow users to select dates and times.
  */
 export const DateTimePicker = createRemoteComponent<
   'DateTimePicker',
