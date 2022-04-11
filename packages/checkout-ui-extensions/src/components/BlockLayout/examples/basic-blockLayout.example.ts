20675452d4dd4411a2e249c2f4b1befafd67a514
James Vidler <james.vidler@shopify.com>
2022-04-11 18:13:28 -0400

    additional renamed of `Feature` to `Dynamic`

--- packages/checkout-ui-extensions/src/components/BlockLayout/examples/basic-blockLayout.example.ts
+++ packages/checkout-ui-extensions/src/components/BlockLayout/examples/basic-blockLayout.example.ts
@@ -1,6 +1,6 @@
 import {extend, BlockLayout, View} from '@shopify/checkout-ui-extensions';
 
-extend('Checkout::Feature::Render', (root) => {
+extend('Checkout::Dynamic::Render', (root) => {
   const blockLayout = root.createComponent(
     BlockLayout,
     {
