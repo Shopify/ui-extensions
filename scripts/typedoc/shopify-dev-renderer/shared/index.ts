450c02a91bffe5adaff8e8a172819144de136ed7
James Vidler <james.vidler@shopify.com>
2022-05-04 16:39:13 -0400

    implemented TSIndexSignature

--- scripts/typedoc/shopify-dev-renderer/shared/index.ts
+++ scripts/typedoc/shopify-dev-renderer/shared/index.ts
@@ -225,6 +225,7 @@ function propType(
         // console.warn(
         //   `Can’t resolve export type \`${value.name}\` in ${dir}. Maybe it’s not exported from the component index or imported from a remote package.`,
         // );
+        
         if(value.name === 'Unknown' && value?.params[0]?.name) {
           //HACK: to get the TSMethodSignature name to show instead of simply "Unknown"
           return `${value.params[0].name}${params}`;
@@ -316,6 +317,20 @@ function propType(
         dir,
         additionalPropsTables,
       )}`;
+
+    case 'IndexSignatureType':
+      
+      return `[${paramsType(
+        value.parameters,
+        exports,
+        dir,
+        additionalPropsTables,
+      )}]: ${propType(
+        value.props,
+        exports,
+        dir,
+        additionalPropsTables,
+      )}`
 
     default:
       if (value.kind === 'UndocumentedType' && value.name === 'T') {
