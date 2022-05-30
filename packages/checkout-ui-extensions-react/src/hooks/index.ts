987e34e0db6870600d8d4189ae0e7727fbd302ca
James Vidler <james.vidler@shopify.com>
2022-05-30 15:31:31 -0400

    lint fixes

--- packages/checkout-ui-extensions-react/src/hooks/index.ts
+++ packages/checkout-ui-extensions-react/src/hooks/index.ts
@@ -1,22 +1,22 @@
-export {useConfiguration} from './configuration'; ✅
-export {useExtensionApi} from './api'; ✅
-export {useLocale} from './locale'; ✅
-export {useExtensionLocale} from './extension-locale'; ✅
-export {useBuyerJourney, useBuyerJourneyIntercept} from './buyer-journey'; ✅
+export {useConfiguration} from './configuration';
+export {useExtensionApi} from './api';
+export {useLocale} from './locale';
+export {useExtensionLocale} from './extension-locale';
+export {useBuyerJourney, useBuyerJourneyIntercept} from './buyer-journey';
 export {useMetafield} from './metafield';
-export {useMetafields,  useApplyMetafieldsChange ✅} from './metafields';
+export {useMetafields, useApplyMetafieldsChange} from './metafields';
 export {useNote} from './note';
-export {useCustomAttributes} from './custom-attributes'; ✅
+export {useCustomAttributes} from './custom-attributes';
 export {useShippingAddress} from './shipping-address';
-export {useBillingAddress} from './billing-address'; ✅
+export {useBillingAddress} from './billing-address';
 export {usePrimaryAddress} from './primary-address';
 export {useRunningTotal} from './running-total';
-export {useLineItems, useApplyLineItemsChange} from './line-items'; ✅
+export {useLineItems, useApplyLineItemsChange} from './line-items';
 export {useTargetLineItem} from './target-line-item';
-export {useApplySignedChange} from './signed'; ✅
-export {useAppMetafields} from './app-metafields'; ✅
+export {useApplySignedChange} from './signed';
+export {useAppMetafields} from './app-metafields';
 export {useShop} from './shop';
 export {useStorage} from './storage';
-export {useExtensionData} from './extension-data'; ✅
-export {useCustomerAccount} from './customer-account'; ✅
+export {useExtensionData} from './extension-data';
+export {useCustomerAccount} from './customer-account';
 export {useTranslate} from './translate';
