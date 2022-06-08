5454d15fbdbf55338ceb026b05523d834c4860f2
Ryan Keller <ryan.keller@shopify.com>
2022-06-08 10:29:56 -0500

    fix: use babel-loader to load ts instead, paves the way for including remote-ui worker plugin

--- packages/ui-extensions-cypress/cypress/component/Example.cy.tsx
+++ packages/ui-extensions-cypress/cypress/component/Example.cy.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 
+interface IFoobar {}
+
 describe('Example.cy.ts', () => {
   it('playground', () => {
     cy.mount(<div>FOOBAR</div>);
-    cy.window('shopify' as any);
+    cy.window('shopify');
     cy.window().then((win) => {
       win.eval(`
+      console.warn('HELLO', window.shopify)
       shopify.extend('Checkout::Dynamic::Render', (root) => {
+        console.warn('ROOT', root);
         root.appendChild(root.createText('Hello world!'));
         root.mount();
       });
+      `);
+    });
+    cy.window().then((win) => {
+      win.eval(`
+      window.run()
       `);
     });
   });
--- packages/ui-extensions-cypress/webpack.config.ts
+++ packages/ui-extensions-cypress/webpack.config.ts
@@ -1,34 +1,55 @@
 module.exports = {
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
   // resolve: {
   //   extensions: ['.ts', '.tsx'],
   // },
   // node: {fs: 'empty', child_process: 'empty', readline: 'empty'},
   module: {
     rules: [
+      // other rules...
+      // This is the rule for our application JavaScript/ TypeScript
       {
-        test: /\.ts$/,
-        exclude: [/node_modules/],
+        // test: /\.[tj]sx?$/,
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
         use: [
+          // However you configure Babel, just make sure to include the plugin.
           {
-            loader: 'ts-loader',
+            loader: 'babel-loader',
             options: {
-              transpileOnly: true,
+              configFile: false,
+              presets: ['@babel/env', '@babel/react', '@babel/typescript'],
+              // plugins: [require.resolve('@remote-ui/web-workers/babel')],
             },
           },
         ],
       },
-      {
-        test: /\.tsx$/,
-        exclude: [/node_modules/],
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
-          },
-        ],
-      },
+      // {
+      //   test: /\.ts$/,
+      //   exclude: [/node_modules/],
+      //   use: [
+      //     {
+      //       loader: 'ts-loader',
+      //       options: {
+      //         transpileOnly: true,
+      //       },
+      //     },
+      //   ],
+      // },
+      // {
+      //   test: /\.tsx$/,
+      //   exclude: [/node_modules/],
+      //   use: [
+      //     {
+      //       loader: 'ts-loader',
+      //       options: {
+      //         transpileOnly: true,
+      //       },
+      //     },
+      //   ],
+      // },
     ],
   },
 };
