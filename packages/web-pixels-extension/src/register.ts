dffe32dbd1882b500107cd8276a8f983a972b0ba
Antoine Grant <antoine.grant@shopify.com>
2022-07-11 14:57:01 -0700

    @shopify/web-pixels-extension Update Web Pixels API types

--- packages/web-pixels-extension/src/register.ts
+++ packages/web-pixels-extension/src/register.ts
@@ -1,9 +1,9 @@
 import {ShopifyGlobal, EXTENSION_POINT} from './globals';
-import {RuntimeAPI} from './types/runtime-api';
+import {WebPixelAPI} from './types/runtime-api';
 
 declare const shopify: ShopifyGlobal;
 
-type Register = (extend: (api: RuntimeAPI) => void) => void;
+type Register = (extend: (api: WebPixelAPI) => void) => void;
 
 export const register: Register = (extend) =>
   shopify.extend(EXTENSION_POINT, extend);
--- packages/web-pixels-extension/src/types/runtime-api.ts
+++ packages/web-pixels-extension/src/types/runtime-api.ts
@@ -1,48 +1,83 @@
 import type {StandardEventsV0} from './standard-events-V0';
 import type {StandardEventsV1} from './standard-events-V1';
 
-type StandardEvents = StandardEventsV0 | StandardEventsV1;
+// Standard Events
+type StandardEvent = StandardEventsV0 | StandardEventsV1;
 const SCHEMA_VERSIONS = ['v0', 'v1'] as const;
-type SchemaVersion = typeof SCHEMA_VERSIONS[number];
+export type SchemaVersion = typeof SCHEMA_VERSIONS[number];
 
-export interface RuntimeAPI {
-  configuration: Configuration;
-  analytics: AnalyticsAPI;
-  browser: BrowserAPI;
+// Event Bus
+export type EventBusChannel = string;
+
+export type EventBusPublishPayload = Record<string, unknown>;
+export type EventBusPublishOptions = Record<string, unknown>;
+export type EventBusPublish<T> = (
+  eventName: EventBusChannel,
+  payload: T,
+  options?: EventBusPublishOptions,
+) => boolean;
+
+export type EventBusSubscribeCallback<T> = (event: T) => void;
+export type EventBusSubscribe<T> = (
+  eventName: EventBusChannel,
+  callback: EventBusSubscribeCallback<T>,
+  options?: EventBusSubscribeOptions,
+) => () => boolean;
+
+interface EventBus {
+  subscribe: (
+    channel: string,
+    callback: WebPixelCallback,
+    options?: SubscribeOptions,
+  ) => () => boolean;
 }
 
-type Configuration = Record<string, any>;
+export type EventBusSubscribeOptions = Record<string, unknown>;
 
-interface AnalyticsAPI {
-  readonly subscribe: EventBus['subscribe'];
+interface SubscribeOptions extends EventBusSubscribeOptions {
+  schemaVersion?: SchemaVersion;
 }
 
-type EventCallback = (
-  event: StandardEvents,
-  metadata?: EventCallbackMetadata,
+// Web Pixel Manager
+export type WebPixelInputPayload = Record<string, any>;
+export interface WebPixelPublishPayload extends EventBusPublishPayload {
+  channel: EventBusChannel;
+  inputPayload: WebPixelInputPayload;
+  metadata: EventCallbackMetadata;
+}
+
+export type WebPixelCallback = (
+  event: StandardEvent,
+  metadata: EventCallbackMetadata,
 ) => void;
 
 interface EventCallbackMetadata {
-  location: {
+  location?: {
     href: string;
     host: string;
     hostname: string;
     origin: string;
   };
-  referrer: string;
+  referrer?: string;
 }
 
-interface EventBus {
+export interface WebPixelSubscribeOptions extends EventBusSubscribeOptions {
+  schemaVersion?: SchemaVersion;
+}
+export interface WebPixelEventBus {
+  publish: EventBusPublish<WebPixelPublishPayload>;
   subscribe: (
     channel: string,
-    callback: EventCallback,
-    options?: SubscribeOptions,
+    callback: WebPixelCallback,
+    options?: WebPixelSubscribeOptions,
   ) => () => boolean;
 }
 
-type EventBusSubscribeOptions = Record<string, unknown>;
-interface SubscribeOptions extends EventBusSubscribeOptions {
-  schemaVersion?: SchemaVersion;
+// Publish Runtime API
+type Configuration = Record<string, any>;
+
+interface AnalyticsAPI {
+  readonly subscribe: EventBus['subscribe'];
 }
 
 interface BrowserAPI {
@@ -57,4 +92,11 @@ interface BrowserAPI {
     url: string | URL,
     data?: Record<string, unknown> | null,
   ) => Promise<boolean>;
+}
+
+export interface WebPixelAPI {
+  configuration: Configuration;
+  analytics: AnalyticsAPI;
+  browser: BrowserAPI;
+  readonly _pixelInfo: Record<string, unknown>;
 }
