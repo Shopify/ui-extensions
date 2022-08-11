62f5f416f98390f8c6d0a1a4332cd853dd149066
Robin Drexler <robin.drexler@shopify.com>
2022-08-11 18:01:02 +0000

    re-export extension types in react package

--- packages/customer-account-ui-extensions-react/src/index.ts
+++ packages/customer-account-ui-extensions-react/src/index.ts
@@ -1,3 +1,12 @@
 export {extend, Style} from '@shopify/customer-account-ui-extensions';
+export type {
+  ApiForRenderExtension,
+  ArgumentsForExtension,
+  ExtensionPoint,
+  ExtensionPoints,
+  RenderExtension,
+  RenderExtensionPoint,
+  ReturnTypeForExtension,
+} from '@shopify/customer-account-ui-extensions';
 export * from './components';
 export {render} from './render';
