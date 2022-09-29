739d6eadcf3a9dd3ff9a5a349b8ab80ecf0c202d
JS Goupil <jeansebastien.goupil@shopify.com>
2022-09-29 15:34:37 -0400

    Changed to just include root mounth

--- packages/retail-ui-extensions-react/src/render.tsx
+++ packages/retail-ui-extensions-react/src/render.tsx
@@ -1,5 +1,4 @@
 import type {ReactElement} from 'react';
-import {createElement} from 'react';
 import {render as remoteRender} from '@remote-ui/react';
 import {extend} from '@shopify/retail-ui-extensions';
 import type {
@@ -36,15 +35,24 @@ export function render<ExtensionPoint extends RenderExtensionPoint>(
   return extend<'Retail::SmartGrid::Tile'>(
     extensionPoint as any,
     (root, api) => {
-      return new Promise(() => {
-        const element = render(api as ApiForRenderExtension<ExtensionPoint>);
-        remoteRender(
-          createElement(ExtensionApiContext.Provider, {value: api}, element),
-          root,
-          () => {
-            root.mount();
-          },
-        );
+      return new Promise((resolve, reject) => {
+        try {
+          remoteRender(
+            <ExtensionApiContext.Provider value={api}>
+              {render(api as ApiForRenderExtension<ExtensionPoint>)}
+            </ExtensionApiContext.Provider>,
+            root,
+            () => {
+              root.mount();
+              resolve();
+            },
+          );
+        } catch (error) {
+          // Workaround for https://github.com/Shopify/ui-extensions/issues/325
+          // eslint-disable-next-line no-console
+          console.error(error);
+          reject(error);
+        }
       });
     },
   );
