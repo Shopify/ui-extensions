cdc2a968086710d7ae4d23e036bbc5d6ceb4c44a
JS Goupil <jeansebastien.goupil@shopify.com>
2022-09-29 12:36:38 -0400

    Deleted dead file

--- packages/retail-ui-extensions-react/src/context.ts
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react';
-import {
-  ApiForRenderExtension,
-  RenderExtensionPoint,
-} from '@shopify/retail-ui-extensions';
-
-export const ExtensionApiContext = createContext<ApiForRenderExtension<
-  RenderExtensionPoint
-> | null>(null);
