cba3399fd74f5238a186a381e67604bb158c5cca
JS Goupil <jeansebastien.goupil@shopify.com>
2022-10-24 14:24:04 -0400

    Made disabled optional

--- packages/retail-ui-extensions/src/components/Selectable/Selectable.ts
+++ packages/retail-ui-extensions/src/components/Selectable/Selectable.ts
@@ -2,7 +2,7 @@ import {createRemoteComponent} from '@remote-ui/core';
 
 export interface SelectableProps {
   onPress: () => void;
-  disabled: boolean;
+  disabled?: boolean;
 }
 
 export const Selectable = createRemoteComponent<'Selectable', SelectableProps>(
