5e6c9a1c73b07ad57852904d5224b7e2b742865f
Eugene Pancheshnyi <eugene.pancheshnyi@shopify.com>
2022-10-26 11:30:57 -0400

    Added params to the API

--- packages/retail-ui-extensions/src/extension-api/navigation-api/navigation-api.ts
+++ packages/retail-ui-extensions/src/extension-api/navigation-api/navigation-api.ts
@@ -4,14 +4,18 @@ export interface NavigationApiContent {
    */
   currentScreen?: string;
 
+  /** The parameters the screen was displayed with */
+  params?: any;
+
   /** Dismisses the modal highest on the stack */
   dismiss(): void;
 
   /** Pushes a screen onto the stack.
    * @param screenName the title of the screen you want to push onto the stack. This property is then returned in this API as `currentScreen`
    * which can then be used to determine which component should be mounted.
+   * @param params the parameters you want to pass for the new screen.
    */
-  push(screenName: string): void;
+  push(screenName: string, params: any): void;
 }
 
 /**
