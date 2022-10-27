126b2bfdd726c26ceae9988ec4e3bc81f5741515
Chris Sauve <chris.sauve@shopify.com>
2022-10-27 10:24:33 -0400

    Fix ui-extensions package build issues (#582)

--- packages/ui-extensions/sewing-kit.config.ts
+++ packages/ui-extensions/sewing-kit.config.ts
@@ -2,8 +2,8 @@ import {createPackage} from '@sewing-kit/config';
 import {uiExtensionsPackage} from '../../config/sewing-kit';
 
 export default createPackage((pkg) => {
-  pkg.entry({root: './src/index.ts'});
-  pkg.entry({name: 'admin', root: './src/surfaces/admin/index.ts'});
-  pkg.entry({name: 'checkout', root: './src/surfaces/checkout/index.ts'});
+  pkg.entry({root: './src'});
+  pkg.entry({name: 'admin', root: './src/surfaces/admin'});
+  pkg.entry({name: 'checkout', root: './src/surfaces/checkout'});
   pkg.use(uiExtensionsPackage());
 });
