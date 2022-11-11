620319503fb64f64cabd1edea08f503e44ce05d8
Alan Hill <alan.hill@shopify.com>
2022-11-11 16:09:29 -0500

    rename + add orderid

--- packages/admin-ui-extensions/src/extension-points/identifiers/order_details_ui.ts
+++ packages/admin-ui-extensions/src/extension-points/identifiers/order_details_ui.ts
@@ -3,7 +3,7 @@ import {RemoteRoot} from '@remote-ui/core';
 import {AppOverlaySchema} from '../../containers';
 import {RenderableExtensionCallback, StandardApi, ToastApi} from '../types';
 
-export type OrderDetailsExtensionPoint ='Admin::OrderDetails::Sidebar::RenderAfterNotes'
+export type OrderDetailsExtensionPoint ='Admin::OrderDetails::Sidebar::RenderAfterCustomerInformation'
 
 export type OrderDetailsExtensionStandardApi<
   T extends OrderDetailsExtensionPoint
@@ -14,22 +14,23 @@ export interface OrderDetailsExtensionContainerApi {
   done(): void
 }
 
-export type OrderDetailsExtensionApiRenderAfterNotes = OrderDetailsExtensionStandardApi<
-  'Admin::OrderDetails::Sidebar::RenderAfterNotes'
+export type OrderDetailsExtensionApiRenderAfterCustomerInformation = OrderDetailsExtensionStandardApi<
+  'Admin::OrderDetails::Sidebar::RenderAfterCustomerInformation'
 > & {
   container: OrderDetailsExtensionContainerApi;
   data: {
     metafields: Array<{ [key: string]: string }>;
+    orderId: String
   };
 };
 
 export interface OrderDetailsExtensionApi {
-  'Admin::OrderDetails::Sidebar::RenderAfterNotes': OrderDetailsExtensionApiRenderAfterNotes;
+  'Admin::OrderDetails::Sidebar::RenderAfterCustomerInformation': OrderDetailsExtensionApiRenderAfterCustomerInformation;
 }
 
 export interface OrderDetailsExtensionPointCallback {
-  'Admin::OrderDetails::Sidebar::RenderAfterNotes': RenderableExtensionCallback<
-    OrderDetailsExtensionApi['Admin::OrderDetails::Sidebar::RenderAfterNotes'],
+  'Admin::OrderDetails::Sidebar::RenderAfterCustomerInformation': RenderableExtensionCallback<
+    OrderDetailsExtensionApi['Admin::OrderDetails::Sidebar::RenderAfterCustomerInformation'],
     RemoteRoot<AppOverlaySchema>
   >
 }
