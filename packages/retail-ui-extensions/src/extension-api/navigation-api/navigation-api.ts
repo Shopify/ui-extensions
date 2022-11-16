33ba63b4c42a10d6e2fba726d2b6789ae45cb17c
Eugene Pancheshnyi <eugene.pancheshnyi@shopify.com>
2022-11-16 15:00:07 -0500

    Navigation API modal support

--- packages/retail-ui-extensions/src/extension-api/navigation-api/navigation-api.ts
+++ packages/retail-ui-extensions/src/extension-api/navigation-api/navigation-api.ts
@@ -16,6 +16,13 @@ export interface NavigationApiContent {
    * @param params the parameters you want to pass for the new screen.
    */
   push(screenName: string, params?: any): void;
+
+  /** Opens a full screen with the `Retail::SmartGrid::Modal` extension point.
+   * @param screenName you can specify a screen name, which will be returned in the `NavigationAPI` as `currentScreen`.
+   * This value can be used to evaluate which component should be mounted. The `push(screenName: string)` method in the NavigationAPI can be used then to push
+   * additional screens on top of the stack.
+   */
+  presentModal(screenName?: string): void;
 }
 
 /**
--- packages/retail-ui-extensions/src/extension-api/smartgrid-api/smartgrid-api.ts
+++ packages/retail-ui-extensions/src/extension-api/smartgrid-api/smartgrid-api.ts
@@ -1,7 +1,7 @@
 export interface SmartGridApiContent {
   /** Opens a full screen with the `Retail::SmartGrid::Modal` extension point.
-   * @param screenName you can specific a screen name, which will be returned in the `NavigationAPI` as `currentScreen`.
-   * This value can you used to evaluate which component should be mounted. The `push(screenName: string)` method in the NavigationAPI can be used then to push
+   * @param screenName you can specify a screen name, which will be returned in the `NavigationAPI` as `currentScreen`.
+   * This value can be used to evaluate which component should be mounted. The `push(screenName: string)` method in the NavigationAPI can be used then to push
    * additional screens on top of the stack.
    */
   presentModal(screenName?: string): void;
