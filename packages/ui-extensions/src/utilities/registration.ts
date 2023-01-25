ed23a329db354c9338a3345473c50832e16f97b5
Chris Sauve <chris.sauve@shopify.com>
2023-01-25 11:43:26 -0500

    Add updated extension registration API

--- packages/ui-extensions/src/utilities/registration.ts
+++ packages/ui-extensions/src/utilities/registration.ts
@@ -1,7 +1,26 @@
+import {createRemoteRoot} from '@remote-ui/core';
+import type {
+  RenderExtensionConnection,
+  RenderExtension,
+  RenderExtensionWithRemoteRoot,
+} from '../extension';
+
 export interface ExtensionRegistrationFunction<ExtensionPoints> {
   <Target extends keyof ExtensionPoints>(
     target: Target,
     implementation: ExtensionPoints[Target],
+  ): ExtensionPoints[Target];
+}
+
+export interface ExtensionRegistrationFunctionWithRoot<ExtensionPoints> {
+  <Target extends keyof ExtensionPoints>(
+    target: Target,
+    implementation: ExtensionPoints[Target] extends RenderExtension<
+      infer Api,
+      infer Components
+    >
+      ? RenderExtensionWithRemoteRoot<Api, Components>
+      : ExtensionPoints[Target],
   ): ExtensionPoints[Target];
 }
 
@@ -14,13 +33,34 @@ export interface ExtensionRegistrationFunction<ExtensionPoints> {
  */
 export function createExtensionRegistrationFunction<
   ExtensionPoints,
->(): ExtensionRegistrationFunction<ExtensionPoints> {
-  const extensionWrapper: ExtensionRegistrationFunction<ExtensionPoints> = (
-    target,
-    implementation,
-  ) => {
-    (globalThis as any).shopify?.extend(target, implementation);
-    return implementation;
+>(): ExtensionRegistrationFunctionWithRoot<ExtensionPoints> {
+  const extensionWrapper: ExtensionRegistrationFunctionWithRoot<
+    ExtensionPoints
+  > = (target, implementation) => {
+    function extension(...args: any[]) {
+      // Rendering extensions have two arguments. Non-rendering extensions don’t have
+      // a `RemoteChannel` that needs to be normalized, so we can just pass the arguments
+      // through.
+      if (args.length === 1) {
+        return (implementation as any)(...args);
+      }
+
+      const [{channel, components}, api] = args as [
+        RenderExtensionConnection,
+        any,
+      ];
+
+      const root = createRemoteRoot(channel, {
+        components,
+        strict: true,
+      });
+
+      return (implementation as any)(root, api);
+    }
+
+    (globalThis as any).shopify?.extend(target, extension);
+
+    return extension as any;
   };
 
   return extensionWrapper;
