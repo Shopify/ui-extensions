768f60d7fae26694d0a7f2f9524ea0d8f018180d
Kevan Davis <kevan.davis@shopify.com>
2023-01-27 18:58:20 +0000

    adding back the variant extension point instead of making product do both

--- packages/admin-ui-extensions/src/extension-points/identifiers/product_configuration.ts
+++ packages/admin-ui-extensions/src/extension-points/identifiers/product_configuration.ts
@@ -4,18 +4,22 @@ import {AllComponentsSchema} from '../../containers';
 import {RenderableExtensionCallback, StandardApi, ToastApi} from '../types';
 
 export type ProductConfigurationExtensionPoint =
-  'Admin::Product::Configuration::Render';
+  | 'Admin::Product::Configuration::Render'
+  | 'Admin::Variant::Configuration::Render';
 
 export interface ProductConfigurationExtensionApi {
   'Admin::Product::Configuration::Render': StandardApi<ProductConfigurationExtensionPoint> &
     ToastApi & {
       data: {
-        productId: string;
-        variantId?: string;
-        // TO-DO #1: type these
-        // TO-DO #2: we don't need to pass the whole product, just the product/variant components, maybe options, variants, etc, and metafields.
+        // TO-DO (future): full typing for Product
         product: any;
-        variant?: any;
+      };
+    };
+  'Admin::Variant::Configuration::Render': StandardApi<ProductConfigurationExtensionPoint> &
+    ToastApi & {
+      data: {
+        // TO-DO (future): full typing for Variant
+        variant: any;
       };
     };
 }
@@ -23,6 +27,10 @@ export interface ProductConfigurationExtensionApi {
 export interface ProductConfigurationExtensionPointCallback {
   'Admin::Product::Configuration::Render': RenderableExtensionCallback<
     ProductConfigurationExtensionApi['Admin::Product::Configuration::Render'],
+    RemoteRoot<AllComponentsSchema>
+  >;
+  'Admin::Variant::Configuration::Render': RenderableExtensionCallback<
+    ProductConfigurationExtensionApi['Admin::Variant::Configuration::Render'],
     RemoteRoot<AllComponentsSchema>
   >;
 }
