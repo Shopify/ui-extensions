49cf79c34293bbb1ca4823a4232a4800d9e3019d
Roberto Floris <roberto.floris@shopify.com>
2023-04-12 07:36:18 +0000

    chore: remove section from customer account ui extensions

--- packages/customer-account-ui-extensions-react/src/components/Section/index.ts
+++
@@ -1,2 +0,0 @@
-export {Section} from './Section';
-export type {SectionProps} from './Section';
--- packages/customer-account-ui-extensions/src/components/index.ts
+++ packages/customer-account-ui-extensions/src/components/index.ts
@@ -2,6 +2,5 @@ export * from './Card';
 export * from './PageBlock';
 export * from './PaymentIcon';
 export * from './PolicyModal';
-export * from './Section';
 export * from './Thumbnail';
 export * from './shared-components';
