fc59013657b88b655c1e14208187cf5a3280e3bf
Lianne Sit <lianne.sit@shopify.com>
2023-05-24 12:24:46 +0000

    Remove unused Card props

--- packages/customer-account-ui-extensions/src/components/Card/Card.ts
+++ packages/customer-account-ui-extensions/src/components/Card/Card.ts
@@ -7,14 +7,6 @@ export interface CardProps {
    */
   background?: Background;
   /**
-   * Enables or disables a divider after each section. **Enabled by default**
-   */
-  includeDividers?: boolean;
-  /**
-   * The children that will be rendered inside the Card
-   */
-  fillHeight?: boolean;
-  /**
    * The padding within the Card. Default is "loose"
    */
   padding?: SpacingProps;
