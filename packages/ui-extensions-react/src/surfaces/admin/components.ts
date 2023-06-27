d464ca2b5cad391484a304e3a58526ecc1155dfe
Olavo Santos <olavo.a.santos@gmail.com>
2023-06-27 09:25:35 -0500

    chore: export new remote React component on admin

--- packages/ui-extensions-react/src/surfaces/admin/components.ts
+++ packages/ui-extensions-react/src/surfaces/admin/components.ts
@@ -1,16 +1,32 @@
 export {CustomerSegmentationTemplate} from './components/CustomerSegmentationTemplate/CustomerSegmentationTemplate';
 export type {CustomerSegmentationTemplateProps} from './components/CustomerSegmentationTemplate/CustomerSegmentationTemplate';
+export {Divider} from './components/Divider/Divider';
+export type {DividerProps} from './components/Divider/Divider';
+export {EmailField} from './components/EmailField/EmailField';
+export type {EmailFieldProps} from './components/EmailField/EmailField';
 export {HorizontalStack} from './components/HorizontalStack/HorizontalStack';
 export type {HorizontalStackProps} from './components/HorizontalStack/HorizontalStack';
+export {Icon} from './components/Icon/Icon';
+export type {IconProps} from './components/Icon/Icon';
+export {Image} from './components/Image/Image';
+export type {ImageProps} from './components/Image/Image';
 export {Link} from './components/Link/Link';
 export type {LinkProps} from './components/Link/Link';
+export {NumberField} from './components/NumberField/NumberField';
+export type {NumberFieldProps} from './components/NumberField/NumberField';
+export {PasswordField} from './components/PasswordField/PasswordField';
+export type {PasswordFieldProps} from './components/PasswordField/PasswordField';
 export {Select} from './components/Select/Select';
 export type {
-  SelectProps,
-  OptionProps,
-  OptionGroupProps,
   OptionDescription,
   OptionGroupDescription,
+  OptionGroupProps,
+  OptionProps,
+  SelectProps,
 } from './components/Select/Select';
+export {TextField} from './components/TextField/TextField';
+export type {TextFieldProps} from './components/TextField/TextField';
+export {URLField} from './components/URLField/URLField';
+export type {URLFieldProps} from './components/URLField/URLField';
 export {VerticalStack} from './components/VerticalStack/VerticalStack';
 export type {VerticalStackProps} from './components/VerticalStack/VerticalStack';
