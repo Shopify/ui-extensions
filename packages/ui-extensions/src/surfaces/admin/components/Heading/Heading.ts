c1a4972bfeab94b750adac40bbf95b8998110768
Olavo Santos <olavo.a.santos@gmail.com>
2023-06-28 15:04:09 -0500

    refactor: remove accessibilityRole from admin Heading

--- packages/ui-extensions/src/surfaces/admin/components/Heading/Heading.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Heading/Heading.ts
@@ -1,5 +1,4 @@
 import {createRemoteComponent} from '@remote-ui/core';
-import type {AccessibilityRole} from '../shared';
 
 type Level = 1 | 2 | 3 | 4 | 5 | 6;
 
@@ -9,13 +8,6 @@ export interface HeadingProps {
 
   /** The visual level of the heading. */
   level?: Level;
-
-  /**
-   * Sets the semantic meaning of the component’s content. When set,
-   * the role will be used by assistive technologies to help buyers
-   * navigate the page.
-   */
-  accessibilityRole?: Extract<AccessibilityRole, 'presentation'>;
 }
 
 export const Heading = createRemoteComponent<'Heading', HeadingProps>(
