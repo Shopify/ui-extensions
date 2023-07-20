0ddabd86fefef1ad5ff5676cf3d63ccff6786a03
Olavo Santos <olavo.a.santos@gmail.com>
2023-07-20 10:38:26 -0500

    docs: Box example is using border prop which doesn’t exist

--- packages/ui-extensions/src/surfaces/admin/components/Box/examples/basic-box.example.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Box/examples/basic-box.example.ts
@@ -3,7 +3,7 @@ import {extend, Box} from '@shopify/ui-extensions/admin';
 extend('Playground', (root) => {
   const box = root.createComponent(
     Box,
-    {border: 'base', padding: 'base'},
+    {padding: 'base'},
     'Box',
   );
 
