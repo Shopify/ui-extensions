b529533a9b8ba6f72470751e385405bc9687828a
Olavo Santos <olavo.a.santos@gmail.com>
2023-07-20 10:46:01 -0500

    docs: error handling of the fetch request is trying to access response.error which doesn’t exist

--- packages/ui-extensions/src/surfaces/admin/components/Form/examples/basic-form.example.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Form/examples/basic-form.example.ts
@@ -12,10 +12,10 @@ extend('admin.product-details.block.render', (root) => {
     {
       label: 'name',
       value: name,
-      onChange = (value) => {
+      onChange: (value) => {
         textField.updateProps({value});
         name = value;
-      }
+      },
     }
   );
 
@@ -23,14 +23,15 @@ extend('admin.product-details.block.render', (root) => {
     // API call to save the values
     const res = await fetch('/save', {method:'POST', body: JSON.stringify({name})});
     if (!res.ok) {
+      const json = await res.json();
       // The Host can catch these errors and do something with them.
-      throw Error(`There were errors: ${res.errors.join(',')}`);
+      throw Error(`There were errors: ${json.errors.join(',')}`);
     }
-  }
+  };
 
   const onReset = async () => {
     name = '';
-  }, []);
+  };
 
   const form = root.createComponent(
     Form,
@@ -39,4 +40,4 @@ extend('admin.product-details.block.render', (root) => {
 
   form.appendChild(textField);
   root.appendChild(form);
-};
+});
