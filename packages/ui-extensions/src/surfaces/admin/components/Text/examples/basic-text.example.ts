d9e5a8288556ad56a55f3b63570faae797d5f00e
Olavo Santos <olavo.a.santos@gmail.com>
2023-07-20 11:00:46 -0500

    docs: Text examples are using the size prop that doesn’t exist

--- packages/ui-extensions/src/surfaces/admin/components/Text/examples/basic-text.example.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Text/examples/basic-text.example.ts
@@ -1,13 +1,9 @@
 import {extend, Text, BlockStack} from '@shopify/ui-extensions/admin';
 
 extend('Playground', (root) => {
-  const text = root.createComponent(BlockStack, undefined, [
-    root.createComponent(Text, {size: 'extraSmall'}, 'Extra Small'),
-    root.createComponent(Text, {size: 'small'}, 'Small'),
-    root.createComponent(Text, {size: 'base'}, 'Base'),
-    root.createComponent(Text, {size: 'medium'}, 'Medium'),
-    root.createComponent(Text, {size: 'large'}, 'Large'),
-    root.createComponent(Text, {size: 'extraLarge'}, 'Extra Large'),
+  const text = root.createComponent(BlockStack, {inlineAlignment: 'center', gap: true}, [
+    root.createComponent(Text, {fontWeight: 'bold'}, 'Name:'),
+    root.createComponent(Text, {}, 'Jane Doe'),
   ]);
 
   root.appendChild(text);
