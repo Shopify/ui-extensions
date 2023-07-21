ae6472676114ab26250466cacaf09de6c9e8ef98
Brian Shen <brian.shen@shopify.com>
2023-07-21 13:19:38 +0000

    remove customerApi from standardApi

--- packages/customer-account-ui-extensions-react/src/hooks/index.ts
+++ packages/customer-account-ui-extensions-react/src/hooks/index.ts
@@ -7,5 +7,4 @@ export {useExtensionCapabilities, useExtensionCapability} from './capabilities';
 export {useExtensionData} from './extension-data';
 export {useI18n} from './i18n';
 export {useRouter, useNavigate} from './router';
-export {useCustomerApi} from './customer-api';
 export {useExtensionPoint} from './extension-point';
