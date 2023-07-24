a2e3316e7b657b6f3effa8264618e9303e00b6f0
Chris Sauve <chris.sauve@shopify.com>
2023-07-24 14:33:53 -0400

    Make `unstable` checkout extension function use new API contract

--- packages/ui-extensions/src/surfaces/checkout/extension.ts
+++ packages/ui-extensions/src/surfaces/checkout/extension.ts
@@ -1,5 +1,7 @@
 import type {RemoteRoot, RemoteComponentType} from '@remote-ui/core';
 
+import {createExtensionRegistrationFunction} from '../../utilities/registration';
+
 import type {ExtensionPoints} from './extension-points';
 
 /**
@@ -43,25 +45,4 @@ export interface ExtensionRegistrationFunction<ExtensionPoints> {
     target: Target,
     implementation: ExtensionPoints[Target],
   ): ExtensionPoints[Target];
-}
-
-/**
- * This function takes an extension function that is expecting a `RemoteRoot` as its
- * first argument, and returns a new function that accepts a `RemoteChannel` instead.
- * This is a convenience that allows the raw UI extension API to only expose the simpler
- * `RemoteChannel` type, while allowing the extension to use the more powerful `RemoteRoot`,
- * provided by a version of `@remote-ui/core` that the extension controls.
- */
-export function createExtensionRegistrationFunction<
-  ExtensionPoints,
->(): ExtensionRegistrationFunction<ExtensionPoints> {
-  const extensionWrapper: ExtensionRegistrationFunction<ExtensionPoints> = (
-    target,
-    implementation,
-  ) => {
-    (globalThis as any).shopify?.extend(target, implementation);
-    return implementation;
-  };
-
-  return extensionWrapper;
 }
