696871b56bb8562bbb5816150bca9ef8533da75f
Trish Ta <trish.ta@shopify.com>
2023-07-25 11:09:42 -0400

    Remove unsupported props from Form component

---
+++ .changeset/sweet-otters-shout.md
@@ -0,0 +1,6 @@
+---
+'@shopify/ui-extensions': patch
+'@shopify/ui-extensions-react': patch
+---
+
+Remove unsupport props from Form component
--- packages/ui-extensions/src/surfaces/admin/components/Form/Form.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Form/Form.ts
@@ -2,21 +2,9 @@ import {createRemoteComponent} from '@remote-ui/core';
 
 export interface FormProps {
   /**
-   * Whether input elements within the form can be automatically completed by the browser.
-   */
-  autocomplete?: boolean;
-
-  /**
    * A unique identifier for the form.
    */
   id?: string;
-
-  /**
-   * Whether the form is able to be submitted.
-   *
-   * When set to `true`, this will also disable the implicit submit behavior of the form.
-   */
-  disabled?: boolean;
 
   /**
    * A callback that is run when the form is submitted.
