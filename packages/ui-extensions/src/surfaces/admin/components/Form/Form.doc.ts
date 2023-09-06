ea930c6251e126f267aedb046270236223c5d713
belalsj <belal.sejouk@shopify.com>
2023-09-06 15:45:13 +0200

    chore: specify Form integration with CSB

--- packages/ui-extensions/src/surfaces/admin/components/Form/Form.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Form/Form.doc.ts
@@ -4,7 +4,7 @@ const data: ReferenceEntityTemplateSchema = {
   name: 'Form',
   featureFlag: 'admin_extensibility',
   description:
-    'Use this component when you want to collect input from users. It provides a structure for various input fields and controls, such as text fields, checkboxes, and buttons. It also handles form submission and reset actions.',
+    'Use this component when you want to collect input from users. It provides a structure for various input fields and controls, such as text fields, checkboxes, and buttons. It also integrates with the native Contextual Save Bar to handle form submission and reset actions.',
   requires: '',
   thumbnail: 'form-thumbnail.png',
   isVisualComponent: true,
