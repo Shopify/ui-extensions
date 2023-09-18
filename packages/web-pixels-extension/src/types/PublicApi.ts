b2c5e82e0677016a2e897eb38f45a562c9f5ea6a
Henry Zhou <henry.zhou@shopify.com>
2023-09-18 23:11:44 +0000

    update ui extensions web pixels dev docs

--- packages/web-pixels-extension/src/types/PublicApi.ts
+++ packages/web-pixels-extension/src/types/PublicApi.ts
@@ -1,6 +1,8 @@
 import type {EventBus} from './EventBus';
+import {VisitorApi} from './VisitorApi';
 
 export interface PublicApi {
   publish: EventBus['publish'];
   publishCustomEvent: EventBus['publishCustomEvent'];
+  visitor: VisitorApi['visitor'];
 }
