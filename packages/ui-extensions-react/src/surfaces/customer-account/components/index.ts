2e589e6d5768e89097ef4ff007f22a0d71e594a2
Oleksandr Oliynyk <oleksandr.oliynyk@shopify.com>
2023-09-29 08:03:49 +0000

    Resource Item Component Definition

--- packages/ui-extensions-react/src/surfaces/customer-account/components/index.ts
+++ packages/ui-extensions-react/src/surfaces/customer-account/components/index.ts
@@ -2,4 +2,5 @@ export * from './Card';
 export * from './CustomerAccountAction';
 export * from './Page';
 export * from './PolicyModal';
+export * from './ResourceItem';
 export * from './shared-checkout-components';
