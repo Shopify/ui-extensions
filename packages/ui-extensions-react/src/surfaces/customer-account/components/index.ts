489ed2ba1e789d0bb518ac89204a392f069fc0e0
Rener Baffa <rener.baffa@shopify.com>
2023-10-02 15:07:25 +0000

    Hide PolicyModal from 3P Apps

--- packages/ui-extensions-react/src/surfaces/customer-account/components/index.ts
+++ packages/ui-extensions-react/src/surfaces/customer-account/components/index.ts
@@ -1,5 +1,6 @@
 export * from './Card';
 export * from './CustomerAccountAction';
 export * from './Page';
+/* @internal */
 export * from './PolicyModal';
 export * from './shared-checkout-components';
