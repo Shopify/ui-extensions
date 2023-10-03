d84eea970ce5e62e125b4224a09645c55a38fe87
Igor de Oliveira Martins <igor.deoliveiramartins@shopify.com>
2023-10-03 20:50:06 +0000

    undoing some file changes
    adding eslint files back
    
    remove lint files
    
    remove lint files

--- packages/ui-extensions/src/surfaces/checkout/components/shared.ts
+++ packages/ui-extensions/src/surfaces/checkout/components/shared.ts
@@ -20,7 +20,7 @@ export interface Autocomplete {
   field: AutocompleteField;
 }
 
-export type AutocompleteGroup = 'shipping' | 'billing';
+export type AutocompleteGroup = 'shipping' | 'billing' | 'location';
 
 export type AutocompleteField =
   | 'name'
