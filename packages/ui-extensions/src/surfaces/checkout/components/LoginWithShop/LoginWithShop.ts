e53621b6457f68331d7c6b175b078627ef3fa7ea
Mike Wilson <95659964+mikewilson01@users.noreply.github.com>
2023-10-24 13:51:40 -0700

    Update props for LWS component (#1466)

--- packages/ui-extensions/src/surfaces/checkout/components/LoginWithShop/LoginWithShop.ts
+++ packages/ui-extensions/src/surfaces/checkout/components/LoginWithShop/LoginWithShop.ts
@@ -1,11 +1,11 @@
 import {createRemoteComponent} from '@remote-ui/core';
 
 export interface LoginWithShopProps {
-  checked: boolean;
-  modalTitle?: string;
-  modalDescription?: string;
-  modalLogoSrc?: string;
-  apiKey?: string;
+  open: boolean;
+  modalTitle: string;
+  modalDescription: string;
+  modalLogoSrc: string;
+  modalCancelText: string;
   onComplete?(): void | Promise<void>;
   onClosed?(): void;
   onStatusChange?(status: 'shop_pay' | 'guest' | 'shop_pay_skipped'): void;
