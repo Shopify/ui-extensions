903a62c7db9f2bf980c44f358445991864f644e6
Mike Wilson <95659964+mikewilson01@users.noreply.github.com>
2023-10-26 13:28:14 -0700

    Add additional props to LWS component (#1473)

--- packages/ui-extensions/src/surfaces/checkout/components/LoginWithShop/LoginWithShop.ts
+++ packages/ui-extensions/src/surfaces/checkout/components/LoginWithShop/LoginWithShop.ts
@@ -2,13 +2,18 @@ import {createRemoteComponent} from '@remote-ui/core';
 
 export interface LoginWithShopProps {
   open: boolean;
+  openLogin?: boolean;
+  doRedirect?: boolean;
   modalTitle: string;
   modalDescription: string;
   modalLogoSrc: string;
+  modalContinueText?: string;
   modalCancelText: string;
   onComplete?(): void | Promise<void>;
   onClosed?(): void;
-  onStatusChange?(status: 'shop_pay' | 'guest' | 'shop_pay_skipped'): void;
+  onStatusChange?(
+    status: 'shop_pay' | 'guest' | 'shop_pay_skipped' | 'shop_pay_guest',
+  ): void;
 }
 
 export const LoginWithShop = createRemoteComponent<
