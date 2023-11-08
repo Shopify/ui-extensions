36ed572fb9572947b8756624626b4bc32064f30a
Rick Caplan <rick.caplan@shopify.com>
2023-11-08 22:07:08 +0000

    2023-11-08 changes

---
+++ packages/ui-extensions/docs/surfaces/checkout/reference/examples/purchase.thank-you.header.render-after/default.example.ts
@@ -0,0 +1,29 @@
+import {
+  BlockStack,
+  Text,
+  extension,
+} from '@shopify/ui-extensions/checkout';
+
+export default extension(
+  'purchase.thank-you.header.render-after',
+  (root, {shop, cost}) => {
+    root.appendChild(
+      root.createComponent(
+        BlockStack,
+        undefined,
+        [
+          root.createComponent(
+            Text,
+            undefined,
+            `Shop name: ${shop.name}`,
+          ),
+          root.createComponent(
+            Text,
+            undefined,
+            `Cost: ${cost.totalAmount}`,
+          ),
+        ],
+      ),
+    );
+  },
+);
