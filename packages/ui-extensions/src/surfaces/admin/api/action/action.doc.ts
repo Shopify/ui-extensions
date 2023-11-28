d4170a2233718afe2d75e3cf011065725fa727ab
Mitch Lillie <mitch.lillie@shopify.com>
2023-11-28 10:37:32 -0800

    chore: link back to tutorial for action and block APIs
    https://github.com/Shopify/shopify-dev-feedback/issues/3185

--- packages/ui-extensions/src/surfaces/admin/api/action/action.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/api/action/action.doc.ts
@@ -2,7 +2,8 @@ import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
 
 const data: ReferenceEntityTemplateSchema = {
   name: 'Action Extension API',
-  description: 'This API is available to all action extension types.',
+  description:
+    'This API is available to all action extension types. Refer to [the tutorial](/docs/apps/admin/admin-actions-and-blocks/build-an-admin-action) for more information.',
   isVisualComponent: false,
   type: 'API',
   definitions: [
--- packages/ui-extensions/src/surfaces/admin/api/block/block.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/api/block/block.doc.ts
@@ -2,7 +2,8 @@ import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
 
 const data: ReferenceEntityTemplateSchema = {
   name: 'Block Extension API',
-  description: 'This API is available to all block extension types.',
+  description:
+    'This API is available to all block extension types. Refer to [the tutorial](/docs/apps/admin/admin-actions-and-blocks/build-an-admin-block) for more information.',
   isVisualComponent: false,
   type: 'API',
   definitions: [
