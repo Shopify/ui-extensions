5f0ea68b9012b37564819a9eba678b99431d32ca
Thiago Temple <thiago.temple@shopify.com>
2023-12-04 16:22:27 -0500

    Adds customer account components

---
+++ packages/ui-extensions-react/src/surfaces/customer-account/components/index.ts
@@ -0,0 +1,8 @@
+export * from './Card';
+export * from './CustomerAccountAction';
+export * from './Page';
+/* @internal */
+export * from './PolicyModal';
+export * from './ResourceItem';
+export * from './ImageGroup';
+export * from './shared-checkout-components';
