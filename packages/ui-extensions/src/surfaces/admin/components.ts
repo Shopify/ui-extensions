8de632353b2939495bdcb0c38cd596a5671bd8fa
Chris Peddecord <chris.peddecord@shopify.com>
2024-01-25 18:53:46 +0000

    Function Settings top-level component

--- packages/ui-extensions/src/surfaces/admin/components.ts
+++ packages/ui-extensions/src/surfaces/admin/components.ts
@@ -32,6 +32,8 @@ export {EmailField} from './components/EmailField/EmailField';
 export type {EmailFieldProps} from './components/EmailField/EmailField';
 export {Form} from './components/Form/Form';
 export type {FormProps} from './components/Form/Form';
+export {FunctionSettings} from './components/FunctionSettings/FunctionSettings';
+export type {FunctionSettingsProps} from './components/FunctionSettings/FunctionSettings';
 export {Heading} from './components/Heading/Heading';
 export type {HeadingProps} from './components/Heading/Heading';
 export {HeadingGroup} from './components/HeadingGroup/HeadingGroup';
