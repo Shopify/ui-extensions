5d04eb5359566d5c230e10c1f9f8d822afb926f3
Igor Kozlov <igor.kozlov@shopify.com>
2024-01-29 12:51:25 -0500

    Update Heading level description

--- packages/ui-extensions/src/surfaces/admin/components/Heading/Heading.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Heading/Heading.ts
@@ -6,7 +6,12 @@ export interface HeadingProps {
   /** A unique identifier for the field. */
   id?: string;
 
-  /** The visual level of the heading. */
+  /**
+   * The visual size of the heading.
+   *
+   * There are no guarantee that the level set will render the same level in the HTML `<h*>` element.
+   * The heading level that gets rendered is determined by the parent `HeadingGroup` or `Section` component.
+   */
   size?: Level;
 }
 
