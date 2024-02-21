421f86cb392b496b75b5a9b786559dda00cb9665
Alex Rigney <alexander.rigney@shopify.com>
2024-02-21 17:25:53 +0000

    add fragmentProps to point-of-sale ui-extensions-react List

--- packages/ui-extensions-react/src/surfaces/point-of-sale/components/List/List.ts
+++ packages/ui-extensions-react/src/surfaces/point-of-sale/components/List/List.ts
@@ -1,4 +1,6 @@
 import {List as BaseList} from '@shopify/ui-extensions/point-of-sale';
 import {createRemoteReactComponent} from '@remote-ui/react';
 
-export const List = createRemoteReactComponent(BaseList);
+export const List = createRemoteReactComponent(BaseList, {
+  fragmentProps: ['listHeaderComponent'],
+});
