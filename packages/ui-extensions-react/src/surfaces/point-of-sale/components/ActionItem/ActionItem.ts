a83c2d1c046ead973d417efee5a27a5202020b1f
Anatolii Gorovyi <anatolii.gorovyi@shopify.com>
2024-02-29 17:50:44 -0500

    Add ActionItem to point-of-sale surface in react package

---
+++ packages/ui-extensions-react/src/surfaces/point-of-sale/components/ActionItem/ActionItem.ts
@@ -0,0 +1,4 @@
+import {ActionItem as BaseActionItem} from '@shopify/ui-extensions/point-of-sale';
+import {createRemoteReactComponent} from '@remote-ui/react';
+
+export const ActionItem = createRemoteReactComponent(BaseActionItem);
