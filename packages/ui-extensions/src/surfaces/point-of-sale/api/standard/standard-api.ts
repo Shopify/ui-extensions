f0f41c8577a22d69b30cea92e605716651215502
Jean-Sebastien Goupil <jeansebastien.goupil@shopify.com>
2024-03-11 10:53:31 -0400

    Removed cartAPI from standard API

--- packages/ui-extensions/src/surfaces/point-of-sale/api/standard/standard-api.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/api/standard/standard-api.ts
@@ -1,4 +1,3 @@
-import type {CartApi} from '../cart-api/cart-api';
 import type {ConnectivityApi} from '../connectivity-api/connectivity-api';
 import type {DeviceApi} from '../device-api/device-api';
 import type {LocaleApi} from '../locale-api/locale-api';
@@ -9,7 +8,6 @@ import type {ProductSearchApi} from '../product-search-api/product-search-api';
 export type StandardApi<T> = {[key: string]: any} & {
   extensionPoint: T;
 } & LocaleApi &
-  CartApi &
   ToastApi &
   SessionApi &
   ProductSearchApi &
