9017cd36412750419a4c5350de6d2ba59e18c357
Alessandro Dal Grande <alessandro.dalgrande@shopify.com>
2024-04-22 15:40:08 -0700

    Fix a typo in the validation settings docs

--- packages/ui-extensions/src/surfaces/admin/api/checkout-rules/validation-settings.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/api/checkout-rules/validation-settings.doc.ts
@@ -3,7 +3,7 @@ import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
 const data: ReferenceEntityTemplateSchema = {
   name: 'Validation Settings API',
   description:
-    'This API is available Validation Settings extensions. Refer to the [tutorial](/docs/apps/checkout/validation/create-complex-validation-rules) for more information. Note that the [`FunctionSettings`](/docs/api/admin-extensions/components/forms/functionsettings) component is required to build Validation Settings extensions.',
+    'This API is available to Validation Settings extensions. Refer to the [tutorial](/docs/apps/checkout/validation/create-complex-validation-rules) for more information. Note that the [`FunctionSettings`](/docs/api/admin-extensions/components/forms/functionsettings) component is required to build Validation Settings extensions.',
   isVisualComponent: false,
   type: 'API',
   definitions: [
