3d604fa5a07fc51e130e84e307855ac21a8ab49a
Elias Khan <elias.khan@shopify.com>
2024-05-16 20:02:45 +0000

    omit Sheet component changes

--- packages/ui-extensions/docs/surfaces/checkout/reference/examples/ui-components/sheet-description-preferences.example.tsx
+++ packages/ui-extensions/docs/surfaces/checkout/reference/examples/ui-components/sheet-description-preferences.example.tsx
@@ -6,7 +6,6 @@ import {
   Sheet,
   TextBlock,
   useApi,
-  useSubscription,
 } from '@shopify/ui-extensions-react/checkout';
 
 export default reactExtension(
@@ -17,17 +16,13 @@ export default reactExtension(
 function Extension() {
   const {customerPrivacy} = useApi();
 
-  const {shouldShowBanner} = useSubscription(
-    customerPrivacy,
-  );
-
   const sheetId = 'sheet-consent';
 
   return (
     <Sheet
       id={sheetId}
       accessibilityLabel="A sheet that collects privacy consent preferences"
-      defaultOpen={shouldShowBanner}
+      defaultOpen={customerPrivacy.current.shouldShowBanner}
       primaryAction={
         <>
           <Button
