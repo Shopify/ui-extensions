19bffac5962d510eb8763bf2cf4bf581e1de550c
Nathan Oliveira <nathan.oliveira@shopify.com>
2024-05-16 13:45:25 +0000

    Fix naming on order and scanner api

--- packages/ui-extensions/docs/surfaces/point-of-sale/reference/apis/order-api.doc.ts
+++ packages/ui-extensions/docs/surfaces/point-of-sale/reference/apis/order-api.doc.ts
@@ -10,7 +10,7 @@ const data: ReferenceEntityTemplateSchema = {
     {
       title: 'OrderApi',
       description: '',
-      type: 'OrderApiContent',
+      type: 'OrderAPIContent',
     },
   ],
   category: 'APIs',
