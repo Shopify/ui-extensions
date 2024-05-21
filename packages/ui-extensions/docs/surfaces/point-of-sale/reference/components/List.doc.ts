9182c941346123569f51820c57eb4b8be71d1df4
Marco Yip <marco.yip@shopify.com>
2024-05-21 13:39:43 +0000

    updated list component docs

--- packages/ui-extensions/docs/surfaces/point-of-sale/reference/components/List.doc.ts
+++ packages/ui-extensions/docs/surfaces/point-of-sale/reference/components/List.doc.ts
@@ -15,6 +15,36 @@ const data: ReferenceEntityTemplateSchema = {
   ],
   category: 'Components',
   related: [],
+  subSections: [
+    {
+      type: 'Generic',
+      anchorLink: 'guidelines',
+      title: 'Guidelines',
+      sectionContent: `
+List items have a wide variety of use cases:
+
+- To display and link to an object | Examples: an item in the cart, a customer in the customer list
+- To display information | Examples: the payment breakdown in an order, staff contact information
+- To display a menu item | Examples: an item on the first page of settings, an item in “More actions”
+- To display a setting
+- To display an action related to other items in the section
+- To show a selectable option | Example: one filter option
+- To display an external link
+    `,
+    },
+    {
+      type: 'Generic',
+      anchorLink: 'content-guidelines',
+      title: 'Content Guidelines',
+      sectionContent: `
+Subtitles:
+
+- Each subtitle should have a different piece of information. Don't use dashes to display more than one type of information on the same line.
+- Subtitles should be shown in order of relevance.
+- If you're showing the results of the form, the label should be the form field title and the subtitle should be the information the merchant entered.
+    `,
+    },
+  ],
 };
 
 export default data;
