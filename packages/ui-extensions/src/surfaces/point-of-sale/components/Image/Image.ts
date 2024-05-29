e1550a7021870061e239f6cec48ab11caf740377
Britta Evans-Fenton <britta.evansfenton@shopify.com>
2024-05-29 16:07:18 +0100

    Add optional alt prop to POS image

--- packages/ui-extensions/src/surfaces/point-of-sale/components/Image/Image.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/components/Image/Image.ts
@@ -2,6 +2,7 @@ import {createRemoteComponent} from '@remote-ui/core';
 
 export interface ImageProps {
   src?: string;
+  alt?: string;
 }
 
 export const Image = createRemoteComponent<'Image', ImageProps>('Image');
