6a8de2802bfcb659f24a25adef3013824497e758
Igor Martins <m.oliverunb@gmail.com>
2024-07-02 10:26:40 -0400

    [UI components]: 2023-04 Docs patch (#2113)
    * scrollTo and image source changes
    
    * changeset
    
    * style helper docs

---
+++ .changeset/odd-avocados-sip.md
@@ -0,0 +1,6 @@
+---
+'@shopify/checkout-ui-extensions': patch
+'@shopify/checkout-ui-extensions-react': patch
+---
+
+ScrollView scrollTo docs changes, image source docs changes
--- packages/checkout-ui-extensions/src/components/Image/examples/basic-image.example.ts
+++ packages/checkout-ui-extensions/src/components/Image/examples/basic-image.example.ts
@@ -2,8 +2,7 @@ import {extend, Image} from '@shopify/checkout-ui-extensions';
 
 extend('Checkout::Dynamic::Render', (root) => {
   const image = root.createComponent(Image, {
-    source:
-      'https://shopify.dev/assets/api/checkout-extensions/checkout/components/image-example-code.png',
+    source: 'https://cdn.shopify.com/YOUR_IMAGE_HERE',
   });
 
   root.appendChild(image);
