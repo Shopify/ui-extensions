0cd9d599f1b2e59aad98308d39b2dcbbe1deb3b2
Mitch Lillie <mitch.lillie@shopify.com>
2024-07-12 08:43:15 -0700

    Update AdminPrintAction.ts

--- packages/ui-extensions/src/surfaces/admin/components/AdminPrintAction/AdminPrintAction.ts
+++ packages/ui-extensions/src/surfaces/admin/components/AdminPrintAction/AdminPrintAction.ts
@@ -4,7 +4,7 @@ export interface AdminPrintActionProps {
   /**
    * Sets the src URL of the preview and the document to print.
    * If not provided, the preview will show an empty state and the print button will be disabled.
-   * HTML, PDF and images are supported.
+   * HTML, PDFs and images are supported.
    */
   src?: string;
 }
