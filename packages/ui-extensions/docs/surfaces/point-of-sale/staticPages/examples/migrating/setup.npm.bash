67989a2da8cdd4693ea90aeff2731ba6cb01be35
JS Goupil <46533923+js-goupil@users.noreply.github.com>
2024-07-15 11:34:08 -0400

    Merge pull request #2187 from Shopify/js-migration-docs
    Added CLI step to pos migration docs

--- packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/migrating/setup.npm.bash
+++ packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/migrating/setup.npm.bash
@@ -1,10 +1,13 @@
-# 1. Remove the old packages
+# 1. Update Shopify CLI
+npm i -g @shopify/cli@latest
+
+# 2. Remove the old packages
 npm uninstall @shopify/retail-ui-extensions*
 
-# 2. Upgrade React
+# 3. Upgrade React
 npm update react@^18.2.0
 npm update @types/react@^18.2.0
 
-# 3. Install the new packages
+# 4. Install the new packages
 npm install @shopify/ui-extensions@2024.4
 npm install @shopify/ui-extensions-react@2024.4
--- packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/migrating/setup.yarn.bash
+++ packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/migrating/setup.yarn.bash
@@ -1,10 +1,13 @@
-# 1. Remove the old packages
+# 1. Update Shopify CLI
+yarn global add @shopify/cli@latest
+
+# 2. Remove the old packages
 yarn remove @shopify/retail-ui-extensions*
 
-# 2. Upgrade React
+# 3. Upgrade React
 yarn upgrade react@^18.2.0
 yarn upgrade @types/react@^18.2.0
 
-# 3. Install the new packages
+# 4. Install the new packages
 yarn add @shopify/ui-extensions@2024.4
 yarn add @shopify/ui-extensions-react@2024.4
