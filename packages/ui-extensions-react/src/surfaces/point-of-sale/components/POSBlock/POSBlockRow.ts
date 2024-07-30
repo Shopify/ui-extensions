7c068ad7d65ba6e5ae7bd6d6d7396dd7124eaf14
Nathan Oliveira <nathan.oliveira@shopify.com>
2024-07-30 09:33:49 -0400

    Fix the POSBlockRow import statement

--- packages/ui-extensions-react/src/surfaces/point-of-sale/components/POSBlock/POSBlockRow.ts
+++ packages/ui-extensions-react/src/surfaces/point-of-sale/components/POSBlock/POSBlockRow.ts
@@ -1,4 +1,4 @@
-import {POSBlockRow as BasePOSBlockRow} from '@shopify/ui-extensions/src/surfaces/point-of-sale';
+import {POSBlockRow as BasePOSBlockRow} from '@shopify/ui-extensions/point-of-sale';
 import {createRemoteReactComponent} from '@remote-ui/react';
 
 export const POSBlockRow = createRemoteReactComponent(BasePOSBlockRow);
