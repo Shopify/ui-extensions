3d48dcec50c25d80853fd08b7a8e102088d52212
Sylvain Hamann <sylvain.hamann@shopify.com>
2024-08-06 18:07:22 +0000

    expose more Menu props

--- packages/ui-extensions/src/surfaces/customer-account/components/Menu/Menu.ts
+++ packages/ui-extensions/src/surfaces/customer-account/components/Menu/Menu.ts
@@ -6,6 +6,14 @@ export interface MenuProps extends IdProps {
    * A label to describe the purpose of the menu that is announced by screen readers.
    */
   accessibilityLabel?: string;
+  /**
+   * Callback to run when the Menu is opened
+   */
+  onOpen?: () => void;
+  /**
+   * Callback to run when the Menu is closed
+   */
+  onClose?: () => void;
 }
 
 export const Menu = createRemoteComponent<'Menu', MenuProps>('Menu');
