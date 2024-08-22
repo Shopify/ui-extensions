d5776abe0481a209d1b1c5ab0c887cbfa9a13460
Josh Larson <josh.larson@shopify.com>
2024-08-22 13:39:37 -0500

    Fix doc generation on localdev

--- packages/ui-extensions/docs/surfaces/point-of-sale/build-docs.sh
+++ packages/ui-extensions/docs/surfaces/point-of-sale/build-docs.sh
@@ -8,6 +8,15 @@ fail_and_exit() {
   exit $1
 }
 
+run_sed() {
+  if [ -n "$SPIN" ]
+  then
+    sed -i "$1" $2
+  else
+    sed -i '' "$1" $2
+  fi
+}
+
 if [ -z $API_VERSION ]
 then
   API_VERSION="unstable"
@@ -36,33 +45,30 @@ fi
 
 # Make sure https://shopify.dev URLs are relative so they work in Spin.
 # See https://github.com/Shopify/generate-docs/issues/181
-sed -i 's/https:\/\/shopify.dev//gi' ./$DOCS_PATH/generated/generated_docs_data.json
+run_sed 's/https:\/\/shopify.dev//gi' ./$DOCS_PATH/generated/generated_docs_data.json
 sed_exit=$?
 if [ $sed_exit -ne 0 ]; then
   fail_and_exit $sed_exit
 fi
 
-if [ -n "$SPIN" ]; then
+if [ -d ~/src/github.com/Shopify/shopify-dev ]; then
+  mkdir -p ~/src/github.com/Shopify/shopify-dev/db/data/docs/templated_apis/pos_ui_extensions/$API_VERSION
+  cp ./$DOCS_PATH/generated/* ~/src/github.com/Shopify/shopify-dev/db/data/docs/templated_apis/pos_ui_extensions/$API_VERSION
+  # Replace 'unstable' with the exact API version in relative doc links
+  run_sed \
+    "s/\/docs\/api\/pos-ui-extensions\/unstable/\/docs\/api\/pos-ui-extensions\/$API_VERSION/gi" \
+    ~/src/github.com/Shopify/shopify-dev/db/data/docs/templated_apis/pos_ui_extensions/$API_VERSION/generated_docs_data.json
+  sed_exit=$?
+  if [ $sed_exit -ne 0 ]; then
+    fail_and_exit $sed_exit
+  fi
+  rsync -a --delete ./$DOCS_PATH/screenshots/ ~/src/github.com/Shopify/shopify-dev/app/assets/images/templated-apis-screenshots/pos-ui-extensions/$API_VERSION
+
   if [ -n "$SPIN_SHOPIFY_DEV_SERVICE_FQDN" ]; then
-
-    mkdir -p ~/src/github.com/Shopify/shopify-dev/db/data/docs/templated_apis/pos_ui_extensions/$API_VERSION
-    cp ./$DOCS_PATH/generated/* ~/src/github.com/Shopify/shopify-dev/db/data/docs/templated_apis/pos_ui_extensions/$API_VERSION
-    # Replace 'unstable' with the exact API version in relative doc links
-    sed -i \
-      "s/\/docs\/api\/pos-ui-extensions\/unstable/\/docs\/api\/pos-ui-extensions\/$API_VERSION/gi" \
-      ~/src/github.com/Shopify/shopify-dev/db/data/docs/templated_apis/pos_ui_extensions/$API_VERSION/generated_docs_data.json
-    sed_exit=$?
-    if [ $sed_exit -ne 0 ]; then
-      fail_and_exit $sed_exit
-    fi
-    rsync -a --delete ./$DOCS_PATH/screenshots/ ~/src/github.com/Shopify/shopify-dev/app/assets/images/templated-apis-screenshots/pos-ui-extensions/$API_VERSION
-
-    cd ~/src/github.com/Shopify/shopify-dev
-
     echo "Docs: https://$SPIN_SHOPIFY_DEV_SERVICE_FQDN/docs/api/pos-ui-extensions"
   else
-    echo "If you include shopify-dev in your Spin constellation, this will automatically copy ./$DOCS_PATH/generated to shopify-dev"
+    echo "Docs: https://shopify-dev.myshopify.io/docs/api/pos-ui-extensions"
   fi
 else
-  echo "Not copying docs to shopify-dev because we're not in Spin"
+  echo "Not copying docs to shopify-dev because it was not found at ~/src/github.com/Shopify/shopify-dev."
 fi
