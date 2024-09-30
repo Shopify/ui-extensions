e8eb8ba058621ab815808e63c3b9b93e24366ae8
Julien Henrotte <hoshi.graphi@gmail.com>
2024-10-01 01:21:28 +0900

    Type

--- packages/ui-extensions/src/surfaces/admin/api/purchase-options-card-action/purchase-options-card-action.ts
+++ packages/ui-extensions/src/surfaces/admin/api/purchase-options-card-action/purchase-options-card-action.ts
@@ -1,9 +1,9 @@
-import type {StandardApi} from '../standard/standard';
+import {ActionExtensionApi} from '../action/action';
 import type {ExtensionTarget as AnyExtensionTarget} from '../../extension-targets';
 
 export interface PurchaseOptionsCardConfigurationApi<
   ExtensionTarget extends AnyExtensionTarget,
-> extends StandardApi<ExtensionTarget> {
+> extends ActionExtensionApi<ExtensionTarget> {
   data: {
     selected: {id: string; sellingPlanId?: string}[];
   };
