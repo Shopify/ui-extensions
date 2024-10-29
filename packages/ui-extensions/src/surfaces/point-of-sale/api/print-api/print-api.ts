9ac908787a77f7914386ed9918777ef3dbacf9fa
Victor Chu <victorchu1996@gmail.com>
2024-10-29 13:10:02 -0700

    Make print api print action async

--- packages/ui-extensions/src/surfaces/point-of-sale/api/print-api/print-api.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/api/print-api/print-api.ts
@@ -5,7 +5,7 @@ export interface PrintApiContent {
   /** Trigger a print dialog
    * @param src the source URL of the content to print. This URL must be a path of the app backend. Valid document types are text, HTML, image, and PDF.
    */
-  print(src: string): void;
+  print(src: string): Promise<void>;
 }
 
 /**
