db113051475e265f3b510cbe69c7ac591d57713c
Melissa Luu <melissa.luu@shopify.com>
2024-11-18 17:59:05 -0500

    Add resourcePicker to ActionExtensionApi interface

--- packages/ui-extensions/src/surfaces/admin/api/action/action.ts
+++ packages/ui-extensions/src/surfaces/admin/api/action/action.ts
@@ -1,6 +1,7 @@
 import type {StandardApi} from '../standard/standard';
 import type {ExtensionTarget as AnyExtensionTarget} from '../../extension-targets';
 import type {Data} from '../shared';
+import type {ResourcePickerApi} from '../resource-picker/resource-picker';
 
 export interface ActionExtensionApi<ExtensionTarget extends AnyExtensionTarget>
   extends StandardApi<ExtensionTarget> {
@@ -13,4 +14,9 @@ export interface ActionExtensionApi<ExtensionTarget extends AnyExtensionTarget>
    * Information about the currently viewed or selected items.
    */
   data: Data;
+
+  /**
+   * Renders the [Resource Picker](/docs/api/app-bridge-library/apis/resource-picker), allowing users to select a resource for the extension to use as part of its flow.
+   */
+  resourcePicker: ResourcePickerApi;
 }
