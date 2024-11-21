48eb7c31c115d2ff0e3d82cd9cdbe888059d1e7e
Alejandro Perea <alejandro.perea@shopify.com>
2024-11-21 16:18:18 -0600

    Defaulting to string type for enabled features passed to the templates

--- packages/ui-extensions/src/surfaces/admin/api/customer-segment-template/customer-segment-template.ts
+++ packages/ui-extensions/src/surfaces/admin/api/customer-segment-template/customer-segment-template.ts
@@ -2,16 +2,11 @@ import type {StandardApi} from '../standard/standard';
 import type {I18n} from '../../../../api';
 import type {ExtensionTarget as AnyExtensionTarget} from '../../extension-targets';
 
-/* List of enabled query language features during a progressive rollout */
-type CustomerSegmentationFeature =
-  /* Enables templates using filters only available when B2B is enabled. For example: companies IS NOT NULL */
-  'b2bEnabled';
-
 export interface CustomerSegmentTemplateApi<
   ExtensionTarget extends AnyExtensionTarget,
 > extends StandardApi<ExtensionTarget> {
   /* Utilities for translating content according to the current `localization` of the admin. */
   i18n: I18n;
   /** @private */
-  __enabledFeatures: CustomerSegmentationFeature[];
+  __enabledFeatures: string[];
 }
