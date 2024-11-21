b7d0232144f0145f3220f1490bddd8e5fa3d9d54
Elana Kopelevich <elana.kopelevich@shopify.com>
2024-11-21 14:34:29 -0700

    Remove shouldRender method and add should-render targets to extension

--- packages/ui-extensions/src/surfaces/admin/shouldRender.ts
+++
@@ -1,5 +0,0 @@
-import {createDataRegistrationFunction} from '../../utilities/dataRegistration';
-
-import type {ExtensionTargets} from './should-render-targets';
-
-export const shouldRender = createDataRegistrationFunction<ExtensionTargets>();
