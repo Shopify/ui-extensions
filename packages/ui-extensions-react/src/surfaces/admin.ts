82cc750e6adf6b97e092cc98a1f3a2f6afa4bb19
Trish Ta <trish.ta@shopify.com>
2024-11-26 14:16:27 -0500

    Manually add components.d.ts content into admin.d.ts

--- packages/ui-extensions-react/src/surfaces/admin.ts
+++ packages/ui-extensions-react/src/surfaces/admin.ts
@@ -1,5 +1,5 @@
 export {extend, extension} from '@shopify/ui-extensions/admin';
 
 export * from './admin/hooks';
-export * from './admin/components';
+// export type * from './admin/components';
 export {render, reactExtension} from './admin/render';
--- packages/ui-extensions/src/surfaces/admin.ts
+++ packages/ui-extensions/src/surfaces/admin.ts
@@ -1,5 +1,4 @@
 export * from './admin/api';
-export * from './admin/components';
 export * from './admin/extension-targets';
 export * from './admin/extension';
 export * from './admin/globals';
