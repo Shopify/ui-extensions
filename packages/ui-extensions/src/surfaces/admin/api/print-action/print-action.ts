4dd1f914259e6fb04f72db4101afdccbe237d932
Melissa Luu <melissa.luu@shopify.com>
2024-11-28 15:56:46 -0500

    Update PrintActionExtensionApii to include ResourcePicker

--- packages/ui-extensions/src/surfaces/admin/api/print-action/print-action.ts
+++ packages/ui-extensions/src/surfaces/admin/api/print-action/print-action.ts
@@ -1,6 +1,7 @@
 import type {StandardApi} from '../standard/standard';
 import type {ExtensionTarget as AnyExtensionTarget} from '../../extension-targets';
 import type {Data} from '../shared';
+import type {ResourcePickerApi} from '../resource-picker/resource-picker';
 
 export interface PrintActionExtensionApi<
   ExtensionTarget extends AnyExtensionTarget,
@@ -9,4 +10,9 @@ export interface PrintActionExtensionApi<
    * Information about the currently viewed or selected items.
    */
   data: Data;
+
+  /**
+   * Renders the [Resource Picker](/docs/api/app-bridge-library/apis/resource-picker), allowing users to select a resource for the extension to use as part of its flow.
+   */
+  resourcePicker: ResourcePickerApi;
 }
