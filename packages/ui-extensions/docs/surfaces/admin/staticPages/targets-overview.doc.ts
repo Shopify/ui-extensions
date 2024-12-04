a4173bd6cae143baa13cc3e47b407fe2dbf1ec8d
Aaron Springut <aaron.springut@shopify.com>
2024-12-04 19:15:38 +0000

    Removing unimplemented target

--- packages/ui-extensions/docs/surfaces/admin/staticPages/targets-overview.doc.ts
+++ packages/ui-extensions/docs/surfaces/admin/staticPages/targets-overview.doc.ts
@@ -282,118 +282,113 @@ You register targets in your \`shopify.extension.toml\` and inside the Javascrip
         {
           title: 'Abandoned checkout details',
           description:
-            'This page shows information about a single abandoned checkout. The `admin.abandoned-checkout-details.action.render` target is available in the "More actions" on the page.',
-        },
-        {
-          title: 'Catalog details',
-          description:
-            'This page shows information about a single catalog. The `admin.catalog-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single abandoned checkout. The `admin.abandoned-checkout-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Collection details',
           description:
-            'This page shows information about a single collection. The `admin.collection-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single collection. The `admin.collection-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Collection index',
           description:
-            'This page shows a table of multiple collections. The `admin.collection-index.action.render` target is available in the "More actions" on the page.',
+            'This page shows a table of multiple collections. The `admin.collection-index.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Company details',
           description:
-            'This page shows information about a single company. The `admin.company-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single company. The `admin.company-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Customer details',
           description:
-            'This page shows information about a single customer. The `admin.customer-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single customer. The `admin.customer-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Customer index',
           description:
-            'This page shows a table of multiple customers. The `admin.customer-index.action.render` target is available in the "More actions" on the page.',
+            'This page shows a table of multiple customers. The `admin.customer-index.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Customer index selection',
           description:
-            'This page shows a table of multiple customers. The `admin.customer-index.selection-action.render` target is available on this page when multiple customers are selected.',
+            'This page shows a table of multiple customers. The `admin.customer-index.selection-action.link` target is available on this page when multiple customers are selected.',
         },
         {
           title: 'Customer segment details',
           description:
-            'This page shows information about a single customer segment. The `admin.customer-segment-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single customer segment. The `admin.customer-segment-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Discount details',
           description:
-            'This page shows information about a single discount. The `admin.discount-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single discount. The `admin.discount-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Discount index',
           description:
-            'This page shows a table of multiple discounts. The `admin.discount-index.action.render` target is available in the "More actions" on the page.',
+            'This page shows a table of multiple discounts. The `admin.discount-index.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Draft order details',
           description:
-            'This page shows information about a single draft order. The `admin.draft-order-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single draft order. The `admin.draft-order-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Draft order index',
           description:
-            'This page shows a table of multiple draft orders. The `admin.draft-order-index.action.render` target is available in the "More actions" on the page.',
+            'This page shows a table of multiple draft orders. The `admin.draft-order-index.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Draft order index selection',
           description:
-            'This page shows a table of multiple draft orders. The `admin.draft-order-index.selection-action.render` target is available on this page when multiple draft orders are selected.',
+            'This page shows a table of multiple draft orders. The `admin.draft-order-index.selection-action.link` target is available on this page when multiple draft orders are selected.',
         },
         {
           title: 'Gift card details',
           description:
-            'This page shows information about a single gift card. The `admin.gift-card-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single gift card. The `admin.gift-card-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Order details',
           description:
-            'This page shows information about a single order. The `admin.order-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single order. The `admin.order-details.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Order details fulfilled card',
           description:
-            'This page shows information about a single order, including a card showing the fulfillment details. The `admin.order-fulfilled-card.action.render` target is available on this page, but only if your app is selected as the fulfillment app for that order.',
+            'This page shows information about a single order, including a card showing the fulfillment details. The `admin.order-fulfilled-card.action.link` target is available on this page, but only if your app is selected as the fulfillment app for that order.',
         },
         {
           title: 'Order index',
           description:
-            'This page shows a table of multiple orders. The `admin.order-index.action.render` target is available in the "More actions" on the page.',
+            'This page shows a table of multiple orders. The `admin.order-index.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Order index selection',
           description:
-            'This page shows a table of multiple orders. The `admin.order-index.selection-action.render` target is available on this page when multiple orders are selected.',
+            'This page shows a table of multiple orders. The `admin.order-index.selection-action.link` target is available on this page when multiple orders are selected.',
         },
         {
           title: 'Product details',
           description:
-            'This page shows information about a single product. The `admin.product-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single product. The `admin.product-details.action.link` target is available in the "More actions" on the page.',
           image: 'admin.product-details.action.render.png',
         },
         {
           title: 'Product index',
           description:
-            'This page shows a table of multiple products. The `admin.product-index.action.render` target is available in the "More actions" on the page.',
+            'This page shows a table of multiple products. The `admin.product-index.action.link` target is available in the "More actions" on the page.',
         },
         {
           title: 'Product index selection',
           description:
-            'This page shows a table of multiple products. The `admin.product-index.selection-action.render` target is available on this page when multiple products are selected.',
+            'This page shows a table of multiple products. The `admin.product-index.selection-action.link` target is available on this page when multiple products are selected.',
         },
         {
           title: 'Product variant details',
           description:
-            'This page shows information about a single product variant. The `admin.product-variant-details.action.render` target is available in the "More actions" on the page.',
+            'This page shows information about a single product variant. The `admin.product-variant-details.action.link` target is available in the "More actions" on the page.',
         },
       ],
     },
