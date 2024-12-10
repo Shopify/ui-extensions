b2d3ee72d995cfd233b7c24226476e5350e9c1e2
Melissa Luu <melissa.luu@shopify.com>
2024-12-10 16:13:50 -0500

    Add picker references and update picker docs

--- packages/ui-extensions/src/surfaces/admin/api/action/action.ts
+++ packages/ui-extensions/src/surfaces/admin/api/action/action.ts
@@ -2,6 +2,7 @@ import type {StandardApi} from '../standard/standard';
 import type {ExtensionTarget as AnyExtensionTarget} from '../../extension-targets';
 import type {Data} from '../shared';
 import type {ResourcePickerApi} from '../resource-picker/resource-picker';
+import type {PickerApi} from '../picker/picker';
 
 export interface ActionExtensionApi<ExtensionTarget extends AnyExtensionTarget>
   extends StandardApi<ExtensionTarget> {
@@ -16,7 +17,12 @@ export interface ActionExtensionApi<ExtensionTarget extends AnyExtensionTarget>
   data: Data;
 
   /**
-   * Renders the [Resource Picker](/docs/api/app-bridge-library/apis/resource-picker), allowing users to select a resource for the extension to use as part of its flow.
+   * Renders the [Resource Picker](resource-picker), allowing users to select a resource for the extension to use as part of its flow.
    */
   resourcePicker: ResourcePickerApi;
+
+  /**
+   * Renders a custom [Picker](picker) dialog allowing users to select values from a list.
+   */
+  picker: PickerApi;
 }
