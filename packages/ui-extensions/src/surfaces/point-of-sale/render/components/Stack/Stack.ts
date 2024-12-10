3747c951eebdc46a1c0b2421d7b919dee1ad29c5
merkoyep <yipmarcokh@gmail.com>
2024-12-10 10:36:03 -0800

    add box component and image sizing

--- packages/ui-extensions/src/surfaces/point-of-sale/render/components/Stack/Stack.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/render/components/Stack/Stack.ts
@@ -1,61 +1,103 @@
 import {createRemoteComponent} from '@remote-ui/core';
-import type {HorizontalSpacing, VerticalSpacing} from '../Spacing/Spacing';
+import type {PaddingProps, SizingProps} from '../shared/box';
+import {
+  ContentDistribution,
+  ContentPosition,
+  SpacingKeyword,
+  GapProps,
+} from '../shared/inner-layouts';
+import {
+  HorizontalSpacing,
+  Spacing,
+  VerticalSpacing,
+} from '../shared/deprecated-types';
 
-export type Spacing =
-  | 0.5
-  | 1
-  | 2
-  | 3
-  | 4
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 13
-  | 16;
+/**
+ *
+ * @deprecated Use the `block` or `inline` as a value instead.
+ */
+type DeprecatedStackDirection = 'vertical' | 'horizontal';
 
-export interface StackProps {
+export interface StackProps extends PaddingProps, SizingProps, GapProps {
   /**
-   * The direction of the stack.
+   * Sets how the Stack's children are placed within the Stack.
+   *
+   * @default 'inline'
    */
-  direction: 'vertical' | 'horizontal';
+  direction?: 'inline' | 'block' | DeprecatedStackDirection;
+
   /**
-   * The alignment of the children along the cross axis.
-   * @defaultValue 'flex-end'
+   * The alignment of the children along the main axis.
+   *
+   * @defaultValue 'flex-start'
+   * @deprecated Use the `justifyContent` prop instead.
    */
-  alignment?:
-    | 'flex-end'
-    | 'center'
-    | 'flex-start'
-    | 'space-around'
-    | 'space-between'
-    | 'space-evenly';
+  alignment?: ContentPosition | ContentDistribution;
+
+  /**
+   * Aligns the Stack along the main axis.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/justify-content
+   * @default 'start'
+   */
+  justifyContent?: ContentPosition | ContentDistribution;
+
+  /**
+   * Aligns the Stack's children along the cross axis.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/align-items
+   * @default 'start'
+   */
+  alignItems?: 'stretch' | ContentPosition;
+
+  /**
+   * Aligns the Stack along the cross axis.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/CSS/align-content
+   * @default 'start'
+   */
+  alignContent?: 'stretch' | ContentPosition | ContentDistribution;
+
   /**
    * The vertical padding around the stack.
+   * @deprecated Use the `paddingBlock` prop instead.
    */
   paddingVertical?: VerticalSpacing;
+
   /**
    * The horizontal padding around the stack.
+   * @deprecated Use the `paddingInline` prop instead.
    */
   paddingHorizontal?: HorizontalSpacing;
+
   /**
    * The spacing between each child in the stack.
    * @defaultValue 1
+   * @deprecated Use the `gap` prop instead.
    */
   spacing?: Spacing;
+
+  /**
+   * The size of the gap between each child in the stack.
+   * @defaultValue '0'
+   */
+  gap?: SpacingKeyword;
+
   /**
    * Whether the children should be stretched to fill the cross axis.
+   * @deprecated Has no effect.
    */
   flexChildren?: boolean;
+
   /**
    * The flex value for the stack.
+   * @deprecated All stacks use flex 1. Use `blockSize` and `inlineSize` to customize the size of your stack.
    */
   flex?: number;
+
   /**
    * Wrap behavior for the children of the stack.
+   * @deprecated Has no effect, content will always wrap.
    */
   flexWrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
 }
