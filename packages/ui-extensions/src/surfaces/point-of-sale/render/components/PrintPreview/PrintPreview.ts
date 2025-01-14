c5dfb93f4624c2b5692965dd08cdccc5763c21bb
Victor Chu <victorchu1996@gmail.com>
2025-01-14 13:35:56 -0800

    Add clarification to print api on receipt printing

--- packages/ui-extensions/src/surfaces/point-of-sale/render/components/PrintPreview/PrintPreview.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/render/components/PrintPreview/PrintPreview.ts
@@ -3,6 +3,16 @@ import {createRemoteComponent} from '@remote-ui/core';
 export interface PrintPreviewProps {
   /**
    * The source to print.
+   *
+   * The src must be either:
+   * - A relative path that will be appended to your app's [application_url](/docs/apps/build/cli-for-apps/app-configuration#application_url)
+   * - A full URL to your app's backend that will be used to return the document
+   *
+   * Supported document types:
+   * - HTML documents (recommended for best printing experience)
+   * - Text files
+   * - Image files (PNG, JPEG, etc.)
+   * - PDF files
    */
   src: string;
 }
