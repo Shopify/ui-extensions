a113703b19b4a2e570998a389e08015c321358ac
oleksandroliynyk <101111253+oleksandroliynyk@users.noreply.github.com>
2025-01-27 15:32:29 +0100

    Clean up tests for billing / shipping addresses

--- packages/ui-extensions-react/src/surfaces/customer-account/hooks/tests/billing-address.test.ts
+++ packages/ui-extensions-react/src/surfaces/customer-account/hooks/tests/billing-address.test.ts
@@ -3,6 +3,7 @@ import {MailingAddress} from '@shopify/ui-extensions/customer-account';
 import {useBillingAddress} from '..';
 
 import {mount, createMockStatefulRemoteSubscribable} from './mount';
+import {ScopeNotGrantedError} from '../../errors';
 
 describe('useBillingAddress', () => {
   it('returns latest billing address', async () => {
@@ -17,5 +18,18 @@ describe('useBillingAddress', () => {
     const {value} = mount.hook(() => useBillingAddress(), {extensionApi});
 
     expect(value).toMatchObject(address);
+  });
+
+  it('raises an exception without approval scopes granted', () => {
+    expect(() =>
+      mount.hook(() => useBillingAddress(), {
+        extensionApi: {
+          extension: {
+            target: 'customer-account.order-status.block.render' as const,
+          },
+          billingAddress: undefined,
+        },
+      }),
+    ).toThrow(ScopeNotGrantedError);
   });
 });
--- packages/ui-extensions-react/src/surfaces/customer-account/hooks/tests/shipping-address.test.ts
+++ packages/ui-extensions-react/src/surfaces/customer-account/hooks/tests/shipping-address.test.ts
@@ -3,6 +3,7 @@ import {MailingAddress} from '@shopify/ui-extensions/customer-account';
 import {useShippingAddress} from '..';
 
 import {mount, createMockStatefulRemoteSubscribable} from './mount';
+import {ScopeNotGrantedError} from '../../errors';
 
 describe('useShippingAddress', () => {
   it('returns latest shipping address', async () => {
@@ -17,5 +18,18 @@ describe('useShippingAddress', () => {
     const {value} = mount.hook(() => useShippingAddress(), {extensionApi});
 
     expect(value).toMatchObject(address);
+  });
+
+  it('raises an exception without approval scopes granted', () => {
+    expect(() =>
+      mount.hook(() => useShippingAddress(), {
+        extensionApi: {
+          extension: {
+            target: 'customer-account.order-status.block.render' as const,
+          },
+          shippingAddress: undefined,
+        },
+      }),
+    ).toThrow(ScopeNotGrantedError);
   });
 });
