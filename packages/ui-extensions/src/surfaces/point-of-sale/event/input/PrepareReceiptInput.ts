99321bd72d78656bd1ebfd675d86694fb464cf99
Alex-Palad <alex.palad@shopify.com>
2025-02-03 21:44:37 -0500

    Minor fix: PrepareReceiptInput extends BaseInput

--- packages/ui-extensions/src/surfaces/point-of-sale/event/input/PrepareReceiptInput.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/event/input/PrepareReceiptInput.ts
@@ -1,5 +1,6 @@
 import {Checkout} from '../../types/checkout';
+import {BaseInput} from './BaseInput';
 
-export interface PrepareReceiptInput {
+export interface PrepareReceiptInput extends BaseInput {
   checkout: Checkout;
 }
