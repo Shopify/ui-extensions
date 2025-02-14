58a7d9b93da8b589aeacb86b1650c6332d37f667
Trish Ta <trish.ta@gmail.com>
2025-02-13 23:34:22 -0500

    Test targeted global

--- packages/ui-extensions/src/surfaces/admin/globals.ts
+++ packages/ui-extensions/src/surfaces/admin/globals.ts
@@ -1,16 +1,4 @@
 import type {ExtensionTargets} from './extension-targets';
 
-export interface ShopifyGlobal {
-  extend<ExtensionTarget extends keyof ExtensionTargets>(
-    target: ExtensionTarget,
-    extend: ExtensionTargets[ExtensionTarget],
-    version?: string,
-  ): void;
-}
-
-declare global {
-  // conflicts with build/ts/globals.d.ts
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const shopify: ShopifyGlobal;
-}
+export type TargetApi<ExtensionTarget extends keyof ExtensionTargets> =
+  Parameters<ExtensionTargets[ExtensionTarget]>[1];
