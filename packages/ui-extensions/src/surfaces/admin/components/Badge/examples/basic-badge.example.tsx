6fcda783f5ac389b71432c795b6faf3972107774
Alex Page <hi@alexpage.dev>
2025-03-24 09:44:32 +1100

    Fix badge example (#2705)

--- packages/ui-extensions/src/surfaces/admin/components/Badge/examples/basic-badge.example.tsx
+++ packages/ui-extensions/src/surfaces/admin/components/Badge/examples/basic-badge.example.tsx
@@ -2,5 +2,5 @@
   <s-badge tone="info">Info</s-badge>
   <s-badge tone="success">Fulfilled</s-badge>
   <s-badge tone="warning">Warning</s-badge>
-  <s-badge tone="error">Error</s-badge>
-</s-stack>;
+  <s-badge tone="critical">Critical</s-badge>
+</s-stack>
