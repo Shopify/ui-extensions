2158e15db07ed17caad8a09da2a7989ce97f2d3e
Luke Sterrett <luke.sterrett@shopify.com>
2025-03-31 15:32:15 -0400

    reintroduce payment method export until next rc release

--- packages/ui-extensions-react/src/surfaces/checkout.ts
+++ packages/ui-extensions-react/src/surfaces/checkout.ts
@@ -57,6 +57,7 @@ export type {
   MaybeShorthandProperty,
   NonPresentationalAccessibilityRole,
   OverlayActivatorProps,
+  PaymentMethod,
   PopoverPosition,
   RenderExtension,
   RenderExtensions,
