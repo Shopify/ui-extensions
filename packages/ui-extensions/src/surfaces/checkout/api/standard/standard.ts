9a9cedf4d78b31fcff78ed2478a523938932ede8
Luke Sterrett <luke.sterrett@shopify.com>
2025-04-02 11:45:56 -0400

    fix path to respect selected version of api

--- packages/ui-extensions/src/surfaces/checkout/api/standard/standard.ts
+++ packages/ui-extensions/src/surfaces/checkout/api/standard/standard.ts
@@ -515,7 +515,7 @@ export interface StandardApi<Target extends ExtensionTarget = ExtensionTarget> {
    * check `discounts.canUpdateDiscountCodes` to ensure it's supported in this checkout.
    *
    * > Caution: As of version `2024-07`, UI extension code must check for instructions before calling select APIs in case those APIs are not available.
-   *  See the [update guide](/docs/api/checkout-ui-extensions/unstable/apis/cart-instructions#examples) for more information.
+   *  See the [update guide](/docs/api/checkout-ui-extensions/apis/cart-instructions#examples) for more information.
    *
    */
   instructions: StatefulRemoteSubscribable<CartInstructions>;
