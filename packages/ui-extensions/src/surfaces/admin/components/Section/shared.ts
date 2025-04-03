38276b2a078f4b5fc84f28213f345027919bbbe2
Aaron Springut <aaron.springut@shopify.com>
2025-04-03 12:46:53 -0700

    Adding more context to component overviews to make llms (and humans) better at using them

--- packages/ui-extensions/src/surfaces/admin/components/Section/shared.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Section/shared.ts
@@ -1,7 +1,7 @@
 const shared = {
   name: 'Section',
   description:
-    '`s-section` is a structural component that allows thematic grouping of content. Its visual style is contextual and controlled by Shopify, so a `Section` may look different depending on the component it is nested inside.\n\n`Section` also automatically increases the heading level for its content to ensure a semantically correct heading structure in the document. To further increase the heading level inside the `Section`, consider nesting new `Section`s.',
+    "`s-section` is a structural component that allows thematic and logical grouping of content. You should use sections as the primary container for all of your app's content. Nest sections to progressively group information. \n\n Section's visual style is contextual and controlled by Shopify. In the admin, at the top level of nesting, sections render as cards. Nested further, they render as the appropriate container for related information within a card. The sections header property will automatically use the correct heading level for the section's content based on the level of nesting. \n\n Section provides default vertical white space to children, so using Stack and Grid are not necessary unless building a complex layout.",
   requires: '',
   thumbnail: 'section-thumbnail.png',
   isVisualComponent: true,
--- packages/ui-extensions/src/surfaces/admin/components/Stack/shared.ts
+++ packages/ui-extensions/src/surfaces/admin/components/Stack/shared.ts
@@ -1,7 +1,7 @@
 const shared = {
   name: 'Stack',
   description:
-    "`s-stack` structures layout elements along the vertical axis of the page. It's useful for vertical alignment.",
+    "`s-stack` structures layout elements along the [block or inline axes](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_display/Block_and_inline_layout_in_normal_flow) of the page. It is useful for organizing buttons, creating layouts that adjust to screen size, and controlling spacing between elements.\n\nIt's important to note that Stack does not include any padding by default. If you need padding around your stacked elements, use `base` to apply the default padding. When spacing becomes limited, Stack will always wrap children to a new line.\n\nWhen using `s-stack`, use smaller gaps between small elements and larger gaps between big ones. Maintain a consistent spacing in stacks across all pages of your app.",
   requires: '',
   thumbnail: 'stack-thumbnail.png',
   isVisualComponent: true,
