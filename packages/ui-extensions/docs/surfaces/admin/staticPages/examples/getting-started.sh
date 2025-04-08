29fcd8a39dc301404f4e35f99764c8d786b55d3e
Bill Fienberg <bill.fienberg@shopify.com>
2025-04-08 15:34:09 -0500

    use shopify global instead of npm

--- packages/ui-extensions/docs/surfaces/admin/staticPages/examples/getting-started.sh
+++ packages/ui-extensions/docs/surfaces/admin/staticPages/examples/getting-started.sh
@@ -1,10 +1,10 @@
 # create an app if you don't already have one:
-npm init @shopify/app@latest --name my-app
+shopify app init --name my-app
 
 # navigate to your app's root directory:
 cd my-app
 
 # generate a new extension:
-npm run generate extension
+shopify generate extension
 # follow the steps to create a new
 # extension in ./extensions.
