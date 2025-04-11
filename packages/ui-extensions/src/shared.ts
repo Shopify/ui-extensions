edbb381d47b96372bba9b316e354bb4955e30551
James Vidler <james.vidler@shopify.com>
2025-04-11 16:53:08 -0400

    cleanup

--- packages/ui-extensions/src/shared.ts
+++ packages/ui-extensions/src/shared.ts
@@ -1,17 +1,3 @@
-import type {RemoteComponentType} from '@remote-ui/core';
-
-export type ComponentsBuilder<ComponentTypes> = {
-  [K in keyof ComponentTypes]: ComponentTypes[K] extends RemoteComponentType<
-    any,
-    any
-  >
-    ? ComponentTypes[K]
-    : never;
-};
-
-export type AnyComponentBuilder<ComponentTypes> =
-  ComponentsBuilder<ComponentTypes>[keyof ComponentsBuilder<ComponentTypes>];
-
 /**
  * Union of supported API versions
  */
