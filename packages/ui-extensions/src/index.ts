e3c186bc8179fe1f704933e1e01c0484564e7f9f
James Vidler <james.vidler@shopify.com>
2025-04-11 12:49:55 -0400

    lint fixes

--- packages/ui-extensions/src/index.ts
+++ packages/ui-extensions/src/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-restricted-syntax
 export * from './api';
-// eslint-disable-next-line no-restricted-syntax
+
 export * from './extension';
--- packages/ui-extensions/src/surfaces/checkout.ts
+++ packages/ui-extensions/src/surfaces/checkout.ts
@@ -185,9 +185,8 @@ export type {
   PaymentOptionItemApi,
 } from './checkout/api/payment/payment-option-item';
 
-// eslint-disable-next-line no-restricted-syntax
 export * from './checkout/extension-targets';
-// eslint-disable-next-line no-restricted-syntax
+
 export * from './checkout/shared';
 export {
   Style,
@@ -206,7 +205,6 @@ export type {
   ViewportSizeCondition,
 } from './checkout/style/types';
 
-// eslint-disable-next-line no-restricted-syntax
 export * from './checkout/globals';
-// eslint-disable-next-line no-restricted-syntax
+
 export * from '../shared';
