870d6debeb69ce643899535adfe1e4d3e17a5dc0
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2025-04-25 16:49:08 -0400

    Add SearchField component docs

---
+++ packages/ui-extensions/src/surfaces/admin/components/SearchField/shared.ts
@@ -0,0 +1,28 @@
+const shared = {
+  name: 'SearchField',
+  description: 'SearchField is used to ...',
+  thumbnail: 'search-field-thumbnail.png',
+  isVisualComponent: true,
+  definitions: [
+    {
+      title: 'SearchField',
+      description: '',
+      type: 'SearchField',
+    },
+  ],
+  subCategory: '',
+  related: [],
+  defaultExample: {
+    codeblock: {
+      title: '',
+      tabs: [
+        {
+          code: './examples/default.html',
+          language: 'preview',
+        },
+      ],
+    },
+  },
+};
+
+export default shared;
