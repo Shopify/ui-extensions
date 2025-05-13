3ba022838ff1f0c1d70414ab8fab30e5f1a74837
Igor Martins <m.oliverunb@gmail.com>
2025-05-13 16:17:07 -0400

    Update packages/ui-extensions/docs/surfaces/checkout/categories/components.doc.ts
    Co-authored-by: Jean-Frederic Fortier <jeanfrederic.fortier@shopify.com>

--- packages/ui-extensions/docs/surfaces/checkout/categories/components.doc.ts
+++ packages/ui-extensions/docs/surfaces/checkout/categories/components.doc.ts
@@ -13,7 +13,7 @@ const data: CategoryTemplateSchema = {
           type: 'info',
           title: 'Early access',
           sectionContent:
-            'This is an early access preview of Polaris built with Web components. If you’re looking for a stable React implementation, see the [Checkout React documentation](/docs/api/checkout-ui-extensions/2025-04/components).',
+            'This is an early access preview of Polaris built with web components. If you’re looking for a stable React implementation, see the [Checkout React documentation](/docs/api/checkout-ui-extensions/2025-04/components).',
         },
       ],
     },
