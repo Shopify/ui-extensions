cdecc5186fc6b627a8c61be253db8a9718d1c7f2
Jean-Frederic Fortier <jeanfrederic.fortier@shopify.com>
2025-05-16 10:39:35 -0400

    Heading: subsections

--- packages/ui-extensions/src/surfaces/checkout/components/Heading/Heading.doc.ts
+++ packages/ui-extensions/src/surfaces/checkout/components/Heading/Heading.doc.ts
@@ -28,15 +28,30 @@ const data: ReferenceEntityTemplateSchema = {
   },
   subSections: [
     {
-      type: 'Generic',
-      anchorLink: 'best-practices',
-      title: 'Best Practices',
-      sectionContent: `
-        - Add a heading at the top of each section that clearly describe what’s below.
-        - Use the heading to highlight the most important concepts or pieces of information that customers need to know.
-      `,
+      title: 'Useful for',
+      type: 'Generic' as const,
+      anchorLink: 'useful-for',
+      sectionContent: `- Creating titles and subtitles for your content that are consistent across your app.
+- Helping users with visual impairments navigate through content effectively using assistive technologies like screen readers.`,
     },
-  ],
+    {
+      title: 'Considerations',
+      type: 'Generic' as const,
+      anchorLink: 'considerations',
+      sectionContent: `- The level of the heading is automatically determined by how deeply it's nested inside other components, starting from h2.
+- Default to using the \`heading\` property in \`s-section\`. The \`s-heading\` component should only be used if you need to implement a custom layout for your heading in the UI.`,
+    },
+    {
+      title: 'Best practices',
+      type: 'Generic' as const,
+      anchorLink: 'best-practices',
+      sectionContent: `
+- Use short headings to make your content scannable.
+- Use plain and clear terms.
+- Don't use jargon or technical language.
+- Don't use different terms to describe the same thing.
+- Don't duplicate content.`,
+    },
 };
 
 export default data;
