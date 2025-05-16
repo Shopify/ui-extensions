a045763c69e4da2b82c0f5e32c33fff2c32d992d
Jean-Frederic Fortier <jeanfrederic.fortier@shopify.com>
2025-05-16 10:54:35 -0400

    Copy edits for: Icon, Image, Link, ListItem, Image

--- packages/ui-extensions/src/docs/shared/components/Image.ts
+++ packages/ui-extensions/src/docs/shared/components/Image.ts
@@ -3,7 +3,7 @@ import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
 const data: SharedReferenceEntityTemplateSchema = {
   name: 'Image',
   description:
-    'Use `s-image` when you want to display an image. \n\n When using images or illustrations in your app, ensure that they add clarity and clearly direct users to the next step. Make sure to use high-resolution images to ensure a professional, high-quality experience.',
+    'Embeds an image within the interface and controls its presentation. Use to visually illustrate concepts, showcase products, or support user tasks and interactions.',
   category: 'Polaris web components',
   subCategory: 'Media',
   related: [],
--- packages/ui-extensions/src/surfaces/checkout/components/Image/Image.doc.ts
+++ packages/ui-extensions/src/surfaces/checkout/components/Image/Image.doc.ts
@@ -26,6 +26,17 @@ const data: ReferenceEntityTemplateSchema = {
       ],
     },
   },
+  subSections: [
+    {
+      title: 'Best practices',
+      type: 'Generic' as const,
+      anchorLink: 'best-practices',
+      sectionContent: `
+- Use high-resolution images to ensure a professional and high-quality experience.
+- Use optimized images so your app loads as fast as possible.
+- Use images intentionally, these should add clarity and lead users to the next step.`,
+    },
+  ],
 };
 
 export default data;
