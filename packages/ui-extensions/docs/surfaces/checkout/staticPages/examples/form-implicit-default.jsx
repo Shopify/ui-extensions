0799e5f071c4e523e1bf2d17d09e97a328836ed9
Jean-Frederic Fortier <jeanfrederic.fortier@shopify.com>
2025-05-20 13:16:03 -0400

    Checkout: use polaris web components section

---
+++ packages/ui-extensions/docs/surfaces/checkout/staticPages/examples/form-implicit-default.jsx
@@ -0,0 +1,37 @@
+import { render } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default function extension() {
+  render(<Extension />, document.body);
+}
+
+async function Extension() {
+  const data = await fetch('/data.json');
+  const {text, number} = await data.json();
+  return <App text={text} number={number} />;
+}
+
+function App({text, number}) {
+  // The initial values set in the form fields will be the default values
+  const [textValue, setTextValue] = useState(text);
+  const [numberValue, setNumberValue] = useState(number);
+
+  return (
+      <s-form onSubmit={() => console.log('submit', {textValue, numberValue})}>
+        <s-stack gap="base">
+          <s-text-field
+            label="Default Value"
+            name="my-text"
+            value={textValue}
+            onChange={(e) => setTextValue(e.target.value)}
+          />
+          <s-number-field
+            label="Percentage field"
+            name="my-number"
+            value={numberValue}
+            onChange={(e) => setNumberValue(e.target.value)}
+          />
+        </s-stack>
+      </s-form>
+  );
+}
