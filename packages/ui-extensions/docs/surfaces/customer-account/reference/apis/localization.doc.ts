14e9d7892b01f64bdaf13b90c4f42aa54d1c2bcb
Zhenjie Zhuo <zhenjie.zhuo@shopify.com>
2025-05-21 09:47:37 -0400

    move the new example to top

--- packages/ui-extensions/docs/surfaces/customer-account/reference/apis/localization.doc.ts
+++ packages/ui-extensions/docs/surfaces/customer-account/reference/apis/localization.doc.ts
@@ -72,30 +72,6 @@ const data: ReferenceEntityTemplateSchema = {
     examples: [
       {
         description:
-          'You can use the count option in the translate method to get a pluralized string based on the current locale.',
-        codeblock: {
-          title: 'Translating strings with pluralization',
-          tabs: [
-            {
-              code: '../examples/apis/translate-pluralization.example.tsx',
-              language: 'jsx',
-              title: 'React',
-            },
-            {
-              code: '../examples/apis/translate-pluralization.example.ts',
-              language: 'js',
-              title: 'JavaScript',
-            },
-            {
-              code: '../examples/apis/translate-pluralization.locale.example.json',
-              language: 'json',
-              title: 'locales/en.default.json',
-            },
-          ],
-        },
-      },
-      {
-        description:
           'You can access the current country of a customer to implement country specific logic.',
         codeblock: {
           title: 'Getting the country of the customer',
@@ -112,6 +88,30 @@ const data: ReferenceEntityTemplateSchema = {
             },
             {
               code: '../examples/apis/localization-country.locale.example.json',
+              language: 'json',
+              title: 'locales/en.default.json',
+            },
+          ],
+        },
+      },
+      {
+        description:
+          'You can use the count option in the translate method to get a pluralized string based on the current locale.',
+        codeblock: {
+          title: 'Translating strings with pluralization',
+          tabs: [
+            {
+              code: '../examples/apis/translate-pluralization.example.tsx',
+              language: 'jsx',
+              title: 'React',
+            },
+            {
+              code: '../examples/apis/translate-pluralization.example.ts',
+              language: 'js',
+              title: 'JavaScript',
+            },
+            {
+              code: '../examples/apis/translate-pluralization.locale.example.json',
               language: 'json',
               title: 'locales/en.default.json',
             },
