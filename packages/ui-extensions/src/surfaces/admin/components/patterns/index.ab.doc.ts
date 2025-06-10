a7386cd1e711525c8bf7fae737a5cec462b7b4e4
Charles Lee <charles.lee@shopify.com>
2025-06-10 14:05:29 +1000

    Add pattern docs from extensibility repo

---
+++ packages/ui-extensions/src/surfaces/admin/components/patterns/index.ab.doc.ts
@@ -0,0 +1,82 @@
+import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
+
+const data: ReferenceEntityTemplateSchema = {
+  name: 'Index',
+  isOneColumnLayout: true,
+  overviewPreviewDescription:
+    'Manage objects efficiently with dynamic table actions.',
+  description: `The index layout lets merchants view and manage all their objects at once in a table format. They can filter, sort and do quick actions on their objects. To prevent tables from becoming visually cluttered, reveal actions only when the row is hovered over or selected
+
+  | Used to    | Examples |
+| -------- | ------- |
+| View all objects at once  | Products, orders, customers, discounts    |
+| Perform bulk actions | Delete products, pause/activate campaigns      |
+
+  ![Preview of the index pattern](/assets/templated-apis-screenshots/admin/patterns/index-example.png)
+
+  This pattern uses \`Section\`, \`Stack\`, \`Heading\`, \`Button\`, \`Table\`, \`Paragraph\`, \`Box\` and \`Grid\` components.
+
+  ---
+
+  ## Design guidelines
+  Design your index page so users can organize and take action on resource objects.
+
+  ### Navigation
+
+  * Users must be able to return to the previous page without using the browser button. To achieve this, your app can provide breadcrumbs or a Back button on the page.
+  * Offer users clear and predictable action labels.
+
+  ---
+
+  ### Layout
+
+  * Design your app to be responsive and adapt to different screen sizes and devices. This ensures a seamless user experience across various platforms.
+  * For resource index pages, use a full-width page. This is helpful when users are dealing with lists of data that have many columns.
+
+  ---
+
+  <style>
+    div[class*="CodeBlock-module-CodeBlock_"] {
+      max-height: calc(100vh - 80px) !important;
+    }
+    div[class*="Tabs-module-TabsContent_"] {
+      overflow: auto !important;
+    }
+    div[class*="Screenshot-module-Screenshot_"] {
+      display: none !important;
+    }
+  </style>`,
+  isVisualComponent: true,
+  category: 'Patterns',
+  thumbnail: '/assets/templated-apis-screenshots/admin/patterns/index.png',
+  defaultExample: {
+    image:
+      '/assets/templated-apis-screenshots/admin/patterns/index-example.png',
+    codeblock: {
+      title: 'Index',
+      tabs: [
+        {
+          title: 'JSX',
+          code: './examples/index.jsx',
+          language: 'jsx',
+        },
+        {
+          title: 'HTML',
+          code: './examples/index.html',
+          language: 'html',
+        },
+      ],
+    },
+  },
+
+  related: [
+    {
+      name: 'Built for Shopify',
+      subtitle: 'Requirements',
+      url: '/docs/apps/launch/built-for-shopify/requirements',
+      type: 'component',
+    },
+  ],
+};
+
+export default data;
