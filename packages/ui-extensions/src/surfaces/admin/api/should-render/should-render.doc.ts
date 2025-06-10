33883ce903344a9cc89ffdfe38345697364dd93c
Elana Kopelevich <elana.kopelevich@shopify.com>
2025-06-10 17:09:19 -0600

    Add context for should-render api

--- packages/ui-extensions/src/surfaces/admin/api/should-render/should-render.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/api/should-render/should-render.doc.ts
@@ -2,7 +2,8 @@ import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
 
 const data: ReferenceEntityTemplateSchema = {
   name: 'ShouldRender API',
-  description: 'This API is available to all shouldRender extension types.',
+  description:
+    'This API controls the render state of an admin action extension. Learn more in the <a href="https://shopify.dev/docs/apps/build/admin/actions-blocks/hide-extensions?extension=react#hide-an-admin-action">admin extensions tutorial</a>.',
   isVisualComponent: false,
   type: 'API',
   definitions: [
