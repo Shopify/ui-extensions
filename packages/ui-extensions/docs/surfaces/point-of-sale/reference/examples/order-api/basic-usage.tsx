037af9f301df817f238f5eb51fba959d10d8f923
Nathan Oliveira <nathan.oliveira@shopify.com>
2025-06-19 11:37:26 -0400

    Add order-api examples

---
+++ packages/ui-extensions/docs/surfaces/point-of-sale/reference/examples/order-api/basic-usage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  reactExtension,
+  Screen,
+  Navigator,
+  ScrollView,
+  Text,
+  Section,
+  useApi,
+} from '@shopify/ui-extensions-react/point-of-sale';
+
+const PostPurchaseAction = () => {
+  const api = useApi<'pos.purchase.post.action.render'>();
+  const order = api.order;
+
+  return (
+    <Navigator>
+      <Screen name="PostPurchaseAction" title="Post Purchase Action">
+        <ScrollView>
+          <Section title="Order Information">
+            <Text>Order ID: {order.id}</Text>
+            <Text>Order Name: {order.name}</Text>
+            {order.customerId && <Text>Order Customer ID: {order.customerId}</Text>}
+          </Section>
+        </ScrollView>
+      </Screen>
+    </Navigator>
+  );
+};
+
+export default reactExtension('pos.purchase.post.action.render', () => (
+  <PostPurchaseAction />
+));
