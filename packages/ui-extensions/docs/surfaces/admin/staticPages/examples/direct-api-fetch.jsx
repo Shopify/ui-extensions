14f326b23ccf04f58a4d067c999a3bb02ae54c71
Bill Fienberg <billfienberg@users.noreply.github.com>
2025-06-23 11:14:51 -0500

    Update Direct API and Preact docs (#2955)
    * update direct-api-fetch example
    
    * update direct-api-query example
    
    * update Scaffolded with Preact section

--- packages/ui-extensions/docs/surfaces/admin/staticPages/examples/direct-api-fetch.jsx
+++ packages/ui-extensions/docs/surfaces/admin/staticPages/examples/direct-api-fetch.jsx
@@ -1,9 +1,10 @@
 import {render} from 'preact';
-import {useEffect, useState} from 'preact/hooks';
 
-export default function extension() {
-  render(<Extension />, document.body);
-}
+export default async () => {
+  const productId = shopify.data.selected?.[0]?.id;
+  const product = await getProduct(productId);
+  render(<Extension product={product} />, document.body);
+};
 
 async function getProduct(id) {
   const res = await fetch('shopify:admin/api/graphql.json', {
@@ -19,22 +20,14 @@ async function getProduct(id) {
       variables: {id},
     }),
   });
-  return res.json();
+  const {data} = await res.json();
+  return data.product;
 }
 
-function Extension() {
-  // Contextual "input" data passed to this extension:
-  const {data} = shopify;
-
-  const [product, setProduct] = useState();
-  useEffect(() => {
-    const productId = data.selected?.[0]?.id;
-    getProduct(productId).then(({data}) => setProduct(data.product));
-  }, [data]);
-
+function Extension({product}) {
   return (
-    <s-admin-block title="Product Info">
-      <s-text>The selected product title is {product?.title}</s-text>
+    <s-admin-block heading="Product Info">
+      <s-text>The selected product title is {product.title}</s-text>
     </s-admin-block>
   );
 }
