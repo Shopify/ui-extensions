3f536b18a2d41e1fffae9e284fd52a05c52110e1
Igor Martins <igor.deoliveiramartins@shopify.com>
2025-06-25 11:35:20 +0200

    routing

--- packages/ui-extensions/docs/surfaces/checkout/staticPages/changelog.doc.ts
+++ packages/ui-extensions/docs/surfaces/checkout/staticPages/changelog.doc.ts
@@ -31,14 +31,14 @@ These web components are an early access preview of the [Polaris](/beta/next-gen
 Released: June 25, 2025
 
 - Added new components:
-  - [Checkbox](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/checkbox)
-  - [EmailField](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/emailfield)
-  - [Map](/docs/api/checkout-ui-extensions/2025-10-rc/components/interactive/map)
-  - [MapMarker](/docs/api/checkout-ui-extensions/2025-10-rc/components/interactive/map#mapmarker)
-  - [Modal](/docs/api/checkout-ui-extensions/2025-10-rc/components/overlay/modal)
-  - [PhoneField](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/phonefield)
-  - [ProductThumbnail](/docs/api/checkout-ui-extensions/2025-10-rc/components/media/productthumbnail)
-  - [TextArea](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/textarea)
+  - [Checkbox](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/checkbox)
+  - [EmailField](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/emailfield)
+  - [Map](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/interactive/map)
+  - [MapMarker](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/interactive/map#mapmarker)
+  - [Modal](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/overlay/modal)
+  - [PhoneField](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/phonefield)
+  - [ProductThumbnail](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/media/productthumbnail)
+  - [TextArea](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/textarea)
 `,
     },
     {
@@ -49,28 +49,28 @@ Released: June 25, 2025
 Released: May 21, 2025
 
 - Added new components:
-  - [Abbreviation](/docs/api/checkout-ui-extensions/2025-10-rc/components/titles-and-text/abbreviation)
-  - [Banner](/docs/api/checkout-ui-extensions/2025-10-rc/components/feedback/banner)
-  - [Box](/docs/api/checkout-ui-extensions/2025-10-rc/components/structure/box)
-  - [Button](/docs/api/checkout-ui-extensions/2025-10-rc/components/actions/button)
-  - [ClipboardItem](/docs/api/checkout-ui-extensions/2025-10-rc/components/utilities/clipboarditem)
-  - [DropZone](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/dropzone)
-  - [Form](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/form)
-  - [Heading](/docs/api/checkout-ui-extensions/2025-10-rc/components/titles-and-text/heading)
-  - [Icon](/docs/api/checkout-ui-extensions/2025-10-rc/components/media/icon)
-  - [Image](/docs/api/checkout-ui-extensions/2025-10-rc/components/media/image)
-  - [Link](/docs/api/checkout-ui-extensions/2025-10-rc/components/actions/link)
-  - [ListItem](/docs/api/checkout-ui-extensions/2025-10-rc/components/structure/listitem)
-  - [OrderedList](/docs/api/checkout-ui-extensions/2025-10-rc/components/structure/orderedlist)
-  - [Paragraph](/docs/api/checkout-ui-extensions/2025-10-rc/components/titles-and-text/paragraph)
-  - [PaymentIcon](/docs/api/checkout-ui-extensions/2025-10-rc/components/media/paymenticon)
-  - [Progress](/docs/api/checkout-ui-extensions/2025-10-rc/components/feedback/progress)
-  - [QRCode](/docs/api/checkout-ui-extensions/2025-10-rc/components/other/qrcode)
-  - [Spinner](/docs/api/checkout-ui-extensions/2025-10-rc/components/feedback/spinner)
-  - [Stack](/docs/api/checkout-ui-extensions/2025-10-rc/components/structure/stack)
-  - [TextField](/docs/api/checkout-ui-extensions/2025-10-rc/components/forms/textfield)
-  - [Time](/docs/api/checkout-ui-extensions/2025-10-rc/components/titles-and-text/time)
-  - [UnorderedList](/docs/api/checkout-ui-extensions/2025-10-rc/components/structure/unorderedlist)
+  - [Abbreviation](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/titles-and-text/abbreviation)
+  - [Banner](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/feedback/banner)
+  - [Box](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/structure/box)
+  - [Button](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/actions/button)
+  - [ClipboardItem](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/utilities/clipboarditem)
+  - [DropZone](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/dropzone)
+  - [Form](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/form)
+  - [Heading](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/titles-and-text/heading)
+  - [Icon](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/media/icon)
+  - [Image](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/media/image)
+  - [Link](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/actions/link)
+  - [ListItem](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/structure/listitem)
+  - [OrderedList](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/structure/orderedlist)
+  - [Paragraph](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/titles-and-text/paragraph)
+  - [PaymentIcon](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/media/paymenticon)
+  - [Progress](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/feedback/progress)
+  - [QRCode](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/other/qrcode)
+  - [Spinner](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/feedback/spinner)
+  - [Stack](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/structure/stack)
+  - [TextField](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/forms/textfield)
+  - [Time](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/titles-and-text/time)
+  - [UnorderedList](/docs/api/checkout-ui-extensions/2025-10-rc/polaris-web-components/structure/unorderedlist)
 `,
     },
   ],
