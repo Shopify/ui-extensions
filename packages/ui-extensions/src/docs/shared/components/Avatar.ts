c10bf90260b89f894d807199ffcc0aaf7c83d02f
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2025-07-07 20:18:26 +0200

    Add Avatar and Thumbnail components with documentation and examples for Admin UI Extensions

---
+++ packages/ui-extensions/src/docs/shared/components/Avatar.ts
@@ -0,0 +1,12 @@
+import {SharedReferenceEntityTemplateSchema} from '../docs-type';
+
+const data: SharedReferenceEntityTemplateSchema = {
+  name: 'Avatar',
+  description:
+    'Show a user’s profile image or initials in a compact, visual element.',
+  category: 'Polaris web components',
+  subCategory: 'Media',
+  related: [],
+};
+
+export default data;
