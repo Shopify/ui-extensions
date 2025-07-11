8175e13f7c490b6ec52cc397becd958e6da1ebd0
thunderfern <linxin.liu@shopify.com>
2025-07-11 10:58:24 -0400

    added open function declaration to the Navigation API

--- packages/ui-extensions/src/surfaces/point-of-sale/api/navigation-api/navigation-api.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/api/navigation-api/navigation-api.ts
@@ -11,6 +11,39 @@ export interface NavigationApiContent {
 
   /** Dismisses the extension. */
   dismiss(): void;
+
+  /**
+   * Open POS screens. Resource screens open in a modal. Tabs close all modals before navigating to the tab.
+   * Available screens:
+   * - `gid://shopify/Product/<productId>` to open product details.
+   * - `gid://shopify/ProductVariant/<variantId>` to open product details for variant.
+   * - `gid://shopify/Customer/<customerId>` to open customer details.
+   * - `gid://shopify/Order/<orderId>` to open order details.
+   * - `gid://shopify/DraftOrder/<draftOrderId>` to open draft order details.
+   * - `gid://shopify/StaffMember/<staffMemberId>` to open staff details.
+   * - `pos://home-tab` to open the home tab.
+   * - `pos://cart-tab` to open the cart tab. Opens home tab on tablets.
+   * - `pos://products-tab` to open the products tab.
+   * - `pos://orders-tab` to open the orders tab.
+   * - `pos://customers-tab` to open the customers tab. Available only on tablets, and throws error on phones.
+   * - `pos://more-tab` to open the more tab.
+   *
+   * @example
+   * // Open product details screen for product id 123
+   * openUrl('gid://shopify/Product/123');
+   *
+   * @example
+   * // Open home tab
+   * openUrl('pos://home-tab');
+   *
+   * @example
+   * // Open home tab
+   * openUrl(new URL('pos://home-tab'));
+   *
+   * @param url - The POS screen to open
+   * @returns A promise that resolves when the POS screen is opened, rejects when an error occurs like an unsupported GID or POS screen.
+   */
+  open(url: string | URL): Promise<void>;
 }
 
 /**
