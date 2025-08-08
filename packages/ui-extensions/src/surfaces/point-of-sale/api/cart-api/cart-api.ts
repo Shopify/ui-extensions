488f0050d93a72550404d12ed5267f65b9ba96ed
Nathan Oliveira <nathan.oliveira@shopify.com>
2025-08-08 10:57:22 -0400

    Add note for throwing, remove | undefined

--- packages/ui-extensions/src/surfaces/point-of-sale/api/cart-api/cart-api.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/api/cart-api/cart-api.ts
@@ -93,13 +93,16 @@ export interface CartApiContent {
   addCustomSale(customSale: CustomSale): Promise<string>;
 
   /**
-   * Add a line item by variant ID to the cart. Returns the uuid of the line item added, or undefined if the user dismissed an oversell guard modal without adding anything. Throws if POS fails to add the line item.
+   * Add a line item by variant ID to the cart.
+   * Returns the uuid of the line item added, or the empty string if the user dismissed an oversell guard modal without adding anything.
+   * Throws if POS fails to add the line item. Throws if POS fails to add the line item.
    *
    * @param variantId the product variant's numeric ID to add to the cart
    * @param quantity the number of this variant to add to the cart
-   * @returns {string | undefined} the uuid of the line item added, or undefined if the user dismissed an oversell guard modal
+   * @returns {string} the uuid of the line item added, or the empty string if the user dismissed an oversell guard modal
+   * @throws {Error} if POS fails to add the line item
    */
-  addLineItem(variantId: number, quantity: number): Promise<string | undefined>;
+  addLineItem(variantId: number, quantity: number): Promise<string>;
 
   /**
    * Remove the line item at this uuid from the cart
