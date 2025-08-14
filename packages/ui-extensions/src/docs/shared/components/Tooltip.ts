ed372f904139ae847bfb0e0675025a9c1ca9695b
Sophie Schneider <sophie.schneider@shopify.com>
2025-08-14 17:26:47 -0400

    [Tooltip] Add docs

---
+++ packages/ui-extensions/src/docs/shared/components/Tooltip.ts
@@ -0,0 +1,12 @@
+import {SharedReferenceEntityTemplateSchema} from '../docs-type';
+
+const data: SharedReferenceEntityTemplateSchema = {
+  name: 'Tooltip',
+  description:
+    'Floating labels that briefly explain the function of a user interface element. They can be triggered when merchants hover or focus.',
+  category: 'Polaris web components',
+  subCategory: 'Overlays',
+  related: [],
+};
+
+export default data;
