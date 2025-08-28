d690a49c13f7556a16d84398b8738cd8c00bafb9
Austin Woodall <austin.woodall@shopify.com>
2025-08-28 16:18:16 -0400

    Update upgrading page

--- packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/upgrading-to-2025-10/hooks-new.tsx
+++ packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/upgrading-to-2025-10/hooks-new.tsx
@@ -1,30 +1,18 @@
 import '@shopify/ui-extensions/preact';
 import {render} from 'preact';
-import {useAttributeValues} from '@shopify/ui-extensions/point-of-sale/preact';
+import {useConnectivitySubscription} from '@shopify/ui-extensions/point-of-sale/preact';
 
 export default function extension() {
-  render(<Extension />, document.body);
+  render(<ConnectivityStatus />, document.body);
 }
 
-function Extension() {
-  const [includeGift] = useAttributeValues(['includeGift']);
+function ConnectivityStatus() {
+  const connectivity = useConnectivitySubscription();
+  const isConnected = connectivity.internetConnected === 'Connected';
+
   return (
-    <s-checkbox
-      checked={includeGift === 'yes'}
-      onChange={onCheckboxChange}
-      label="Include a complimentary gift"
-    />
+    <s-text tone={isConnected ? 'success' : 'warning'}>
+      Status: {isConnected ? 'Online' : 'Offline'}
+    </s-text>
   );
-}
-
-async function onCheckboxChange(event) {
-  const isChecked = event.target.checked;
-
-  const result = await shopify.applyAttributeChange({
-    type: 'updateAttribute',
-    key: 'includeGift',
-    value: isChecked ? 'yes' : 'no',
-  });
-
-  console.log('applyAttributeChange result', result);
 }
