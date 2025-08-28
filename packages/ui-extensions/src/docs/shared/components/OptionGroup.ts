bb18112735ecd577d71f77e8ae3e685658dcee8e
Maya Murad <maya.murad@shopify.com>
2025-08-28 10:00:04 -0400

    [Select] Update docs to include Option and OptionGroup

---
+++ packages/ui-extensions/src/docs/shared/components/OptionGroup.ts
@@ -0,0 +1,12 @@
+import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
+
+const data: SharedReferenceEntityTemplateSchema = {
+  name: 'OptionGroup',
+  description:
+    'Represents a group of options within a select component. Use only as a child of `s-select` components.',
+  category: 'Polaris web components',
+  subCategory: 'Forms',
+  related: [],
+};
+
+export default data;
