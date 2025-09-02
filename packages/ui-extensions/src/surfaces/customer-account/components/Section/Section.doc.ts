38b3a024d00f825491973453855afe4ce7fc0441
Lianne Sit <lianne.sit@shopify.com>
2025-09-02 14:05:43 -0400

    Update Customer account component images

--- packages/ui-extensions/src/surfaces/customer-account/components/Section/Section.doc.ts
+++ packages/ui-extensions/src/surfaces/customer-account/components/Section/Section.doc.ts
@@ -36,7 +36,7 @@ const data: ReferenceEntityTemplateSchema = {
   defaultExample: {
     image: 'section-preview.png',
     altText:
-      'An example of the Section component with a header, some text, and two actions.',
+      'An example of the Section component shows a header, some text, a primary action, and a secondary action.',
     codeblock: {
       title: 'Basic Section',
       tabs: [
