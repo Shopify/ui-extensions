e279cb50b19f2635d32b447841473e622aecfdf1
Austin Woodall <austin.woodall@shopify.com>
2025-09-04 21:20:38 -0400

    add heading doc

---
+++ packages/ui-extensions/src/surfaces/point-of-sale/components/Heading/Heading.doc.ts
@@ -0,0 +1,28 @@
+import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
+
+const data: ReferenceEntityTemplateSchema = {
+  name: 'Heading',
+  description:
+    'Renders hierarchical titles to communicate the structure and organization of page content. Heading levels adjust automatically based on nesting within parent Section components, ensuring a meaningful and accessible page outline.',
+  thumbnail: 'heading-thumbnail.png',
+  isVisualComponent: true,
+  type: '',
+  definitions: [],
+  category: 'Polaris web components',
+  subCategory: 'Titles and text',
+  defaultExample: {
+    image: 'heading-default.png',
+    codeblock: {
+      title: 'Code',
+      tabs: [
+        {
+          code: './examples/default.html',
+          language: 'html',
+        },
+      ],
+    },
+  },
+  related: [],
+};
+
+export default data;
