2324878dcb75ba49d19f5e1702dbd5d3e782b223
Austin Woodall <austin.woodall@shopify.com>
2025-09-04 22:41:20 -0400

    Update textarea doc

--- packages/ui-extensions/src/surfaces/point-of-sale/components/TextArea/TextArea.doc.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/components/TextArea/TextArea.doc.ts
@@ -33,7 +33,7 @@ const data: ReferenceEntityTemplateSchema = {
       tabs: [
         {
           code: './examples/default.html',
-          language: 'HTML',
+          language: 'html',
         },
       ],
     },
