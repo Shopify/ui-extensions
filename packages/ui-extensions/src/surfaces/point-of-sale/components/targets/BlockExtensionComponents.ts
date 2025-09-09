84e5b7654c07f2d0ee95dc7c9c15e42774eb6830
Daniel Friyia <daniel.friyia@shopify.com>
2025-09-09 12:51:28 -0400

    Add new components to BlockExtensionComponents and StandardComponents

--- packages/ui-extensions/src/surfaces/point-of-sale/components/targets/BlockExtensionComponents.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/components/targets/BlockExtensionComponents.ts
@@ -2,11 +2,17 @@ export type BlockExtensionComponents =
   | 'Badge'
   | 'Button'
   | 'DatePicker'
+  | 'DateSpinner'
   | 'Dialog'
+  | 'Heading'
   | 'Icon'
   | 'Image'
+  | 'Modal'
   | 'POSBlock'
+  | 'PosBlock' // Case is important in 2025-10
   | 'POSBlockRow'
+  | 'PrintPreview'
+  | 'Section'
   | 'Stack'
   | 'Text'
   | 'TimePicker';
