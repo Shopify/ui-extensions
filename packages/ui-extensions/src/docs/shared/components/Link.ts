ccd9cdf0d7699a65e9ac0569440244cda878fa14
Jason Addleman <jason.addleman@shopify.com>
2025-09-12 15:45:18 -0400

    Add a link to the useNavigate docs about 'shopify:navigate'

--- packages/ui-extensions/src/docs/shared/components/Link.ts
+++ packages/ui-extensions/src/docs/shared/components/Link.ts
@@ -3,7 +3,7 @@ import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
 const data: SharedReferenceEntityTemplateSchema = {
   name: 'Link',
   description:
-    'Makes text interactive, allowing users to navigate to other pages or perform specific actions. Supports standard URLs, custom protocols, and navigation within Shopify or app pages.',
+    "Makes text interactive, allowing users to navigate to other pages or perform specific actions. Supports standard URLs, custom protocols, and navigation within Shopify or app pages.\n\nWhen using the Link component inside embedded apps, you'll need to handle the `'shopify:navigate'` event to intercept navigation requests. Refer to [handling navigation events](/docs/api/app-home?accordionItem=getting-started-existing-remix-application) for implementation details and framework-specific examples.",
   category: 'Polaris web components',
   subCategory: 'Actions',
   related: [],
