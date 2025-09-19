e030357aab8342dfd6228d8c5f8b64e26d0d5ce2
Nicolas Cardelino <nicolas.cardelino@shopify.com>
2025-09-19 15:43:36 -0300

    Update Customer Accounts announcement examples

---
+++ packages/ui-extensions/docs/surfaces/customer-account/reference/targets/customer-account.order-index.announcement.render.doc.ts
@@ -0,0 +1,32 @@
+import type {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
+
+import {ORDER_STATUS_API} from '../helper.docs';
+
+const data: ReferenceEntityTemplateSchema = {
+  name: 'customer-account.order-index.announcement.render',
+  description: `A static extension target that is rendered on top of the **Order Index page** as a dismissable announcement.`,
+  defaultExample: {
+    description: '',
+    codeblock: {
+      title: 'Customer account order index announcement extension example',
+      tabs: [
+        {
+          code: '../examples/targets/customer-account.order-index.announcement.render/default.example.tsx',
+          language: 'tsx',
+          title: 'Preact',
+        },
+      ],
+    },
+  },
+  subCategory: 'Order index',
+  related: [
+    {
+      name: 'Placement references',
+      subtitle: 'Navigate to',
+      url: '/docs/apps/customer-accounts/best-practices/testing-ui-extensions#order-index',
+    },
+  ],
+  ...ORDER_STATUS_API,
+};
+
+export default data;
