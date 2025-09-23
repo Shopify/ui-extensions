e7d9d23f89724f664c56307dc364e74d009d1a75
Nicolas Cardelino <ncardeli@gmail.com>
2025-09-23 17:12:16 -0300

    Update Customer Accounts announcement examples (#3336)

---
+++ packages/ui-extensions/docs/surfaces/customer-account/reference/targets/customer-account.profile.announcement.render.doc.ts
@@ -0,0 +1,34 @@
+import type {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
+
+import {CUSTOMER_ACCOUNT_STANDARD_API_DEFINITION} from '../helper.docs';
+
+const data: ReferenceEntityTemplateSchema = {
+  name: 'customer-account.profile.announcement.render',
+  description: `A static extension target that is rendered on top of the **Profile page** as a dismissable announcement.`,
+  defaultExample: {
+    description: '',
+    codeblock: {
+      title: 'Customer account profile announcement extension example',
+      tabs: [
+        {
+          code: '../examples/targets/customer-account.profile.announcement.render/default.example.tsx',
+          language: 'tsx',
+          title: 'Preact',
+        },
+      ],
+    },
+  },
+  subCategory: 'Profile (Default)',
+  related: [
+    {
+      name: 'Placement references',
+      subtitle: 'Navigate to',
+      url: '/docs/apps/customer-accounts/best-practices/testing-ui-extensions#profile',
+    },
+  ],
+  definitions: [CUSTOMER_ACCOUNT_STANDARD_API_DEFINITION],
+  category: 'Targets',
+  isVisualComponent: false,
+};
+
+export default data;
