133125921e53df337094affb6ee50878084bd4dc
Charles Lee <charles.lee@shopify.com>
2025-09-26 09:30:05 +1000

    Fix color-field description to be more specific and useful

--- packages/ui-extensions/src/docs/shared/components/ColorField.ts
+++ packages/ui-extensions/src/docs/shared/components/ColorField.ts
@@ -2,7 +2,8 @@ import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
 
 const data: SharedReferenceEntityTemplateSchema = {
   name: 'ColorField',
-  description: 'Allow users to select a color from a color palette.',
+  description:
+    'Allow users to select a color with a color picker or as a text input.',
   category: 'Polaris web components',
   subCategory: 'Forms',
   related: [],
