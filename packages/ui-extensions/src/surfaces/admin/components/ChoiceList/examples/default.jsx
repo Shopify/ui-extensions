071282d4ba553116e9cd2ea97dc0e4ec8d57b4a4
David Cortés <477473+davebcn87@users.noreply.github.com>
2025-10-03 13:24:12 +0200

    fixed type issues in default examples

--- packages/ui-extensions/src/surfaces/admin/components/ChoiceList/examples/default.jsx
+++ packages/ui-extensions/src/surfaces/admin/components/ChoiceList/examples/default.jsx
@@ -1,8 +1,8 @@
-<>
-  <script>
-    const handleChange = (event) => console.log('Values: ',
-    event.currentTarget.values)
-  </script>
+const handleChange = (event) => {
+  console.log('Values: ', event.currentTarget.values)
+}
+
+return (
   <s-choice-list
     label="Company name"
     name="Company name"
@@ -13,4 +13,4 @@
     <s-choice value="optional">Optional</s-choice>
     <s-choice value="required">Required</s-choice>
   </s-choice-list>
-</>
+)
