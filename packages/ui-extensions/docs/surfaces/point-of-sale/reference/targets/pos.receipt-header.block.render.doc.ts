cec95305e276539aff7ebf227825af8a45f20583
Chad Cromwell <chad.cromwell@shopify.com>
2025-10-14 10:01:06 -0300

    update note

--- packages/ui-extensions/docs/surfaces/point-of-sale/reference/targets/pos.receipt-header.block.render.doc.ts
+++ packages/ui-extensions/docs/surfaces/point-of-sale/reference/targets/pos.receipt-header.block.render.doc.ts
@@ -7,7 +7,7 @@ const data: ReferenceEntityTemplateSchema = {
   name: ExtensionTargetType.PosReceiptHeaderBlockRender,
   description: `Renders a custom section within the POS receipt header
   > Note:
-  > This is part of a [POS UI Extensions developer preview](/docs/api/developer-previews#pos-ui-extensions-developer-preview). More information to come. This target is available in POS 10.8.`,
+  > This is part of a [POS UI Extensions developer preview](/docs/api/developer-previews#pos-ui-extensions-developer-preview). More information to come. This target and \`transaction.lineItems.discounts.discountAllocations\` are available in POS 10.8 and later.`,
   defaultExample: {
     codeblock: generateCodeBlock(
       'Block',
