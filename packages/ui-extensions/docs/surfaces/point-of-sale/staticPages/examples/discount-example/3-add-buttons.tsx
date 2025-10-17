e5b0bda47b6e9135b1fd967d62916302ba46b41d
Nathan Oliveira <nathan.oliveira@shopify.com>
2025-10-17 12:31:03 -0400

    Remove pos static pages that are migrating

--- packages/ui-extensions/docs/surfaces/point-of-sale/staticPages/examples/discount-example/3-add-buttons.tsx
+++
@@ -1,4 +0,0 @@
-<ScrollView>
-  <Button title='25%' onPress={() => onButtonPress('Percentage', '25% off', '25')} />
-  <Button title='$10' onPress={() => onButtonPress('FixedAmount', '$10 off', '10')} />
-</ScrollView>
