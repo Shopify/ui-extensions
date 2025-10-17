e67e9321ea4f560f2fee316dc58520472006713f
David Cortés <477473+davebcn87@users.noreply.github.com>
2025-10-17 16:33:59 +0200

    added numeric to the default example as well

--- packages/ui-extensions/src/surfaces/admin/components/Table/examples/default.jsx
+++ packages/ui-extensions/src/surfaces/admin/components/Table/examples/default.jsx
@@ -3,23 +3,27 @@
     <s-table-header-row>
       <s-table-header>Name</s-table-header>
       <s-table-header>Email</s-table-header>
+      <s-table-header format="numeric">Orders placed</s-table-header>
       <s-table-header>Phone</s-table-header>
     </s-table-header-row>
     <s-table-body>
       <s-table-row>
-        <s-table-cell>John Doe</s-table-cell>
-        <s-table-cell>john.doe@example.com</s-table-cell>
+        <s-table-cell>John Smith</s-table-cell>
+        <s-table-cell>john@example.com</s-table-cell>
+        <s-table-cell>23</s-table-cell>
         <s-table-cell>123-456-7890</s-table-cell>
       </s-table-row>
       <s-table-row>
-        <s-table-cell>Jane Doe</s-table-cell>
-        <s-table-cell>jane.doe@example.com</s-table-cell>
-        <s-table-cell>123-456-7890</s-table-cell>
+        <s-table-cell>Jane Johnson</s-table-cell>
+        <s-table-cell>jane@example.com</s-table-cell>
+        <s-table-cell>15</s-table-cell>
+        <s-table-cell>234-567-8901</s-table-cell>
       </s-table-row>
       <s-table-row>
-        <s-table-cell>Brandon Doe</s-table-cell>
-        <s-table-cell>brandon.doe@example.com</s-table-cell>
-        <s-table-cell>123-456-7890</s-table-cell>
+        <s-table-cell>Brandon Williams</s-table-cell>
+        <s-table-cell>brandon@example.com</s-table-cell>
+        <s-table-cell>42</s-table-cell>
+        <s-table-cell>345-678-9012</s-table-cell>
       </s-table-row>
     </s-table-body>
   </s-table>
