d385f8b44acb97e3112f12e87f45b25c415b6d43
David Cortés <477473+davebcn87@users.noreply.github.com>
2025-10-21 14:02:41 +0200

    fixed tooltip with accessory after removing the div

--- packages/ui-extensions/src/surfaces/admin/components/TextField/examples/with-accessory.jsx
+++ packages/ui-extensions/src/surfaces/admin/components/TextField/examples/with-accessory.jsx
@@ -1,4 +1,6 @@
-<s-text-field label="Discount code">
-  <s-icon slot="accessory" interestFor="info-tooltip" type="info" />
+<>
   <s-tooltip id="info-tooltip">This is info tooltip</s-tooltip>
-</s-text-field>
+  <s-text-field label="Discount code">
+    <s-icon slot="accessory" interestFor="info-tooltip" type="info" />
+  </s-text-field>
+</>
