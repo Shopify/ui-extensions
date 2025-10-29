d450901331e34ca297b2faf2760d3fb8736d476e
Marco <marco.yip@shopify.com>
2025-10-29 10:16:10 -0400

    update examples

--- packages/ui-extensions/docs/surfaces/point-of-sale/reference/apis/cash-drawer-api.doc.ts
+++ packages/ui-extensions/docs/surfaces/point-of-sale/reference/apis/cash-drawer-api.doc.ts
@@ -1,10 +1,30 @@
 import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
-import {generateCodeBlock} from '../helpers/generateCodeBlock';
+import {generateJsxCodeBlock} from '../helpers/generateCodeBlock';
+import {TargetLink} from '../types/ExtensionTargetType';
+
+const generateJsxCodeBlockForCashDrawerApi = (
+  title: string,
+  fileName: string,
+) => generateJsxCodeBlock(title, 'cash-drawer-api', fileName);
 
 const data: ReferenceEntityTemplateSchema = {
   name: 'Cash Drawer API',
   description: `
-The Cash Drawer API is an API exposed to extensions for cash drawer functionality, specifically allowing UI extensions to control cash drawer operations.`,
+The Cash Drawer API is an API exposed to extensions for cash drawer functionality, specifically allowing UI extensions to control cash drawer operations.
+
+#### Supporting targets
+- ${TargetLink.PosHomeTileRender}
+- ${TargetLink.PosHomeModalRender}
+- ${TargetLink.PosPurchasePostActionMenuItemRender}
+- ${TargetLink.PosPurchasePostActionRender}
+- ${TargetLink.PosPurchasePostBlockRender}
+- ${TargetLink.PosReturnPostActionMenuItemRender}
+- ${TargetLink.PosReturnPostActionRender}
+- ${TargetLink.PosReturnPostBlockRender}
+- ${TargetLink.PosExchangePostActionMenuItemRender}
+- ${TargetLink.PosExchangePostActionRender}
+- ${TargetLink.PosExchangePostBlockRender}
+`,
   isVisualComponent: false,
   type: 'APIs',
   definitions: [
@@ -20,9 +40,8 @@ The Cash Drawer API is an API exposed to extensions for cash drawer functionalit
     description: 'Examples of using the Cash Drawer API',
     examples: [
       {
-        codeblock: generateCodeBlock(
-          'Cash Drawer API',
-          'cash-drawer-api',
+        codeblock: generateJsxCodeBlockForCashDrawerApi(
+          'Open the cash drawer',
           'default.example',
         ),
       },
