5149d130c6997dd450e4b36087bad880e6d03c04
andy-chhuon <andy.chhuon@shopify.com>
2025-11-11 15:08:54 -0500

    20644: Implement Camera API

--- packages/ui-extensions/src/surfaces/point-of-sale/api.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/api.ts
@@ -15,6 +15,13 @@ export type {StandardApi} from './api/standard/standard-api';
 export type {ActionTargetApi} from './api/action-target-api/action-target-api';
 
 export type {
+  CameraApi,
+  CameraApiContent,
+  CameraMediaOptions,
+  CameraMediaResponse,
+} from './api/camera-api/camera-api';
+
+export type {
   ConnectivityStateSeverity,
   ConnectivityState,
   ConnectivityApiContent,
--- packages/ui-extensions/src/surfaces/point-of-sale/api/action-target-api/action-target-api.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/api/action-target-api/action-target-api.ts
@@ -1,7 +1,9 @@
 import {ScannerApi} from '../scanner-api/scanner-api';
 import {StandardApi} from '../standard/standard-api';
+import {CameraApi} from '../camera-api/camera-api';
 
 export type ActionTargetApi<T> = {[key: string]: any} & {
   extensionPoint: T;
 } & StandardApi<T> &
-  ScannerApi;
+  ScannerApi &
+  CameraApi;
