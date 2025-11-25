e24201935bbbb7627ac25cc0befb9d06e11b827b
David Cortés <477473+davebcn87@users.noreply.github.com>
2025-11-25 13:12:48 +0100

    recovered link to css grid layout

--- packages/ui-extensions/src/docs/shared/components/Grid.ts
+++ packages/ui-extensions/src/docs/shared/components/Grid.ts
@@ -3,7 +3,7 @@ import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
 const data: SharedReferenceEntityTemplateSchema = {
   name: 'Grid',
   description:
-    'Organizes content into a grid-like layout with columns and rows, consistent alignment, and spacing. Creates responsive layouts following the CSS grid layout pattern.',
+    'Organizes content into a grid-like layout with columns and rows, consistent alignment, and spacing. Creates responsive layouts following the [CSS grid layout](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_grid_layout) pattern.',
   category: 'Polaris web components',
   subCategory: 'Structure',
   related: [],
