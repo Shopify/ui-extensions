0df2c893f1d11bb4429b89389699fad1c6a83966
David Cortés <477473+davebcn87@users.noreply.github.com>
2025-11-25 17:54:09 +0100

    fix lint issues

--- packages/ui-extensions/src/docs/shared/components/Paragraph.ts
+++ packages/ui-extensions/src/docs/shared/components/Paragraph.ts
@@ -3,7 +3,7 @@ import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
 const data: SharedReferenceEntityTemplateSchema = {
   name: 'Paragraph',
   description:
-    'Displays text content in a block format while grouping elements with the same style. Icons and other inline elements automatically adopt the paragraph's tone.',
+    "Displays text content in a block format while grouping elements with the same style. Icons and other inline elements automatically adopt the paragraph's tone.",
   category: 'Polaris web components',
   subCategory: 'Titles and text',
   related: [],
