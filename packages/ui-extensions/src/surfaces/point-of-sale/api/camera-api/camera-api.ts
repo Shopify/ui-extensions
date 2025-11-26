4f2098e0cb079bd4dab3ea5777fc5349b5fd5ba8
andy-chhuon <andy.chhuon@shopify.com>
2025-11-26 09:17:55 -0500

    t

---
+++ packages/ui-extensions/src/surfaces/point-of-sale/api/camera-api/camera-api.ts
@@ -0,0 +1,51 @@
+export interface CameraMediaOptions {
+  /**
+   * The camera that will be active when the camera interface first opens.
+   * - `'user'`: The front-facing camera
+   * - `'environment'`: The rear-facing camera
+   * @defaultValue 'environment'
+   */
+  facingMode?: 'user' | 'environment';
+  /**
+   * The maximum width (0 to 1080) of the image in pixels. Resizes the image to this width if it is larger.
+   * @defaultValue 1080
+   */
+  maxWidth?: number;
+  /**
+   * The maximum height (0 to 1080) of the image in pixels. Resizes the image to this height if it is larger.
+   * @defaultValue 1080
+   */
+  maxHeight?: number;
+  /**
+   * The quality of the image returned. Percentile value between 0 and 1.
+   * @defaultValue 0.9
+   */
+  quality?: number;
+}
+
+export interface CameraMediaResponse {
+  /** The image data as base64 string. */
+  base64: string;
+  /** The width of the image in pixels. */
+  width: number;
+  /** The height of the image in pixels. */
+  height: number;
+  /** The file size of the image in bytes. */
+  fileSize: number;
+  /** The MIME type of the image. */
+  type: string;
+}
+
+export interface CameraApiContent {
+  /**
+   * Launch the device's camera to take a photo.
+   *
+   * @param options the options for the camera media.
+   * @returns Promise<CameraMediaResponse> that resolves when the POS has necessary permissions to access the camera and the media is captured.
+   */
+  takePhoto: (options?: CameraMediaOptions) => Promise<CameraMediaResponse>;
+}
+
+export interface CameraApi {
+  camera: CameraApiContent;
+}
