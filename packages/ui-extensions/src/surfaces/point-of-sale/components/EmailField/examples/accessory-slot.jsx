6bf34fad1abff9eec2d302b63d077518a1bfd065
Prakash Raman <prakash.raman@shopify.com>
2025-12-10 16:10:36 -0500

    Removed slot references and examples from 2026-01 docs

--- packages/ui-extensions/src/surfaces/point-of-sale/components/EmailField/examples/accessory-slot.jsx
+++
@@ -1,9 +0,0 @@
-<s-email-field
-  label="Newsletter signup"
-  placeholder="Enter email address"
-  value="customer@example.com"
->
-  <s-button slot="accessory" onClick={() => console.log('Subscribe')}>
-    Subscribe
-  </s-button>
-</s-email-field>;
