c236f3d4c86c2292db755eb35b6d8f48e8dd3549
Olavo Santos <olavo.a.santos@gmail.com>
2025-12-12 15:07:45 -0600

    docs: fix metafield and metaobject definition intent docs

--- packages/ui-extensions/src/surfaces/admin/api/intents/examples/edit-metafield-definition.js
+++ packages/ui-extensions/src/surfaces/admin/api/intents/examples/edit-metafield-definition.js
@@ -2,6 +2,7 @@ const {intents} = useApi(TARGET);
 
 const activity = await intents.invoke('edit:shopify/MetafieldDefinition', {
   value: 'gid://shopify/MetafieldDefinition/123456789',
+  data: {ownerType: 'product'},
 });
 
 const response = await activity.complete;
