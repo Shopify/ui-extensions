950301702db8a832120acbfbb533ca45756c1e44
Wendy Liu <wendy.liu@shopify.com>
2026-01-22 17:57:44 -0500

    [2025-04] Update Buyer Journey API docs

--- packages/ui-extensions/docs/surfaces/checkout/reference/examples/buyer-journey-intercept/target-native-field.example.ts
+++ packages/ui-extensions/docs/surfaces/checkout/reference/examples/buyer-journey-intercept/target-native-field.example.ts
@@ -1,12 +1,52 @@
-import {extension} from '@shopify/ui-extensions/checkout';
+import {
+  extension,
+  Banner,
+} from '@shopify/ui-extensions/checkout';
 
 export default extension(
   'purchase.checkout.delivery-address.render-before',
-  (root, {buyerJourney, shippingAddress}) => {
+  (
+    root,
+    {buyerJourney, extension, shippingAddress},
+  ) => {
+    const banner = root.createComponent(
+      Banner,
+      {
+        status: 'warning',
+        title: 'This app may be misconfigured',
+      },
+      `To allow this app to block checkout, enable this behavior in "Checkout behavior" settings.`,
+    );
+    const editorType = extension.editor.type;
+
     let address = shippingAddress?.current;
     shippingAddress?.subscribe((newAddress) => {
       address = newAddress;
     });
+
+    let blockProgressGranted =
+      extension.capabilities.current.find(
+        (capability) =>
+          capability === 'block_progress',
+      );
+    extension.capabilities.subscribe(
+      (newCapabilities) => {
+        blockProgressGranted =
+          newCapabilities.find(
+            (capability) =>
+              capability === 'block_progress',
+          );
+
+        if (
+          editorType === 'checkout' &&
+          !blockProgressGranted
+        ) {
+          root.appendChild(banner);
+        } else {
+          root.removeChild(banner);
+        }
+      },
+    );
 
     buyerJourney.intercept(
       ({canBlockProgress}) => {
