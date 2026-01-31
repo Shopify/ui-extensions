c04d3376cf172536e7bb772c39ee3f60b08a17ce
Han T. <han.tan@shopify.com>
2026-01-30 16:04:20 -0800

    update `Screen.onReceiveParams` with clarifying description.

--- packages/ui-extensions/src/surfaces/point-of-sale/render/components/Screen/Screen.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale/render/components/Screen/Screen.ts
@@ -56,7 +56,7 @@ export interface ScreenProps {
    */
   overrideNavigateBack?: () => void;
   /**
-   * A callback function triggered when the navigation event completes and the screen receives parameters passed during navigation.
+   * A callback function triggered when the navigation event completes with any passed parameters. Runs when screen is mounted (with `undefined`).
    */
   onReceiveParams?: (params: any) => void;
 }
