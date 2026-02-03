0f601e3daadc7e6813bb8b3546de3cff197676fe
Michelle Vinci <michelle.vinci@shopify.com>
2026-02-02 18:19:11 -0800

    Integrate feedback from Steve

--- packages/ui-extensions/src/shared.ts
+++ packages/ui-extensions/src/shared.ts
@@ -19,6 +19,6 @@ export type AnyComponentBuilder<ComponentTypes> =
   ComponentsBuilder<ComponentTypes>[keyof ComponentsBuilder<ComponentTypes>];
 
 /**
- * The supported GraphQL Admin API versions. Use this to specify which API version your GraphQL queries should execute against. Each version includes specific features, bug fixes, and breaking changes. The `unstable` version provides access to the latest features but may change without notice.
+ * The supported GraphQL Admin API versions. Use this to specify which API version your GraphQL queries should execute against. Each version includes specific features, bug fixes, and breaking changes. The `unstable` version provides access to the latest features, and can change without notice because it's not subject to versioning guarantees.
  */
 export type ApiVersion = '2023-04' | '2023-07' | '2023-10' | 'unstable';
