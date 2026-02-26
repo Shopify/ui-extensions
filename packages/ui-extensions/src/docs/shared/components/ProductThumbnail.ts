86a5469ce8dbf3bf8fbeccf5f32ed956fd5f7075
Wendy Liu <wendy.liu@shopify.com>
2026-02-25 23:10:47 -0500

    [2026-04] Generate checkout docs with sentence casing for components

--- packages/ui-extensions/src/docs/shared/components/ProductThumbnail.ts
+++ packages/ui-extensions/src/docs/shared/components/ProductThumbnail.ts
@@ -1,7 +1,7 @@
 import type {SharedReferenceEntityTemplateSchema} from '../docs-type';
 
 const data: SharedReferenceEntityTemplateSchema = {
-  name: 'ProductThumbnail',
+  name: 'Product thumbnail',
   description: 'Use ProductThumbnail to display a product thumbnail',
   category: 'Polaris web components',
   subCategory: 'Media',
