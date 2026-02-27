af0c4af60e3fbeaa9b4d5bf24f45c81081b3c590
Chad Carlson <chad.carlson@shopify.com>
2026-02-27 12:13:16 -0500

    Fix linting errors.

--- packages/ui-extensions/src/surfaces/admin/components/patterns/details.ab.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/components/patterns/details.ab.doc.ts
@@ -47,7 +47,7 @@ const data: AdminReferenceEntityTemplateSchema = {
   recommendedCompositions: ['Empty state', 'Footer help', 'Media card'],
   defaultExample: {
     description:
-      "This example presents a product details view for a Product Quality Auditor app with editable quality score fields in the main column and image and score in the sidebar.",
+      'This example presents a product details view for a Product Quality Auditor app with editable quality score fields in the main column and image and score in the sidebar.',
     codeblock: {
       title: 'Details',
       tabs: [
