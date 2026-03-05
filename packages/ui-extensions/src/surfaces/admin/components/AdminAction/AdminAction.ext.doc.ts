32418eb7b1177bb322a429e904e4a5e532f9d0b8
Michelle Vinci <michelle.vinci@shopify.com>
2026-03-04 19:30:10 -0800

    Fix add form fields example

--- packages/ui-extensions/src/surfaces/admin/components/AdminAction/AdminAction.ext.doc.ts
+++ packages/ui-extensions/src/surfaces/admin/components/AdminAction/AdminAction.ext.doc.ts
@@ -76,7 +76,7 @@ const data: ReferenceEntityTemplateSchema = {
           },
           {
             description:
-              'Embed a [form](/docs/api/admin-extensions/{API_VERSION}/polaris-web-components/forms/form) inside the action modal to collect merchant input before saving. This example includes a [text field](/docs/api/admin-extensions/{API_VERSION}/polaris-web-components/forms/text-field) and [number field](/docs/api/admin-extensions/{API_VERSION}/polaris-web-components/forms/number-field) wrapped in a form component.',
+              'Collect merchant input inside the action modal using form fields. This example includes a [text field](/docs/api/admin-extensions/{API_VERSION}/polaris-web-components/forms/text-field) and [number field](/docs/api/admin-extensions/{API_VERSION}/polaris-web-components/forms/number-field) arranged in a [stack](/docs/api/admin-extensions/{API_VERSION}/polaris-web-components/layout-and-structure/stack).',
             codeblock: {
               title: 'Add form fields to the modal',
               tabs: [
