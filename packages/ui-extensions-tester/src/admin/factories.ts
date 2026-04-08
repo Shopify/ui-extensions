e6646bb88f4bc0f8f213df052f7ba449fc3f567f
owen huang <owen.huang@shopify.com>
2026-04-08 13:31:27 -0400

    Added in subscription fields

--- packages/ui-extensions-tester/src/admin/factories.ts
+++ packages/ui-extensions-tester/src/admin/factories.ts
@@ -212,6 +212,11 @@ function createDiscountFunctionSettingsMock<T extends ExtensionTarget>(
       discountClasses: createReadonlySignalLike([]),
       updateDiscountClasses: () => createResult('updateDiscountClasses'),
       discountMethod: createReadonlySignalLike('automatic' as const),
+      purchaseType: createReadonlySignalLike('one_time_purchase' as const),
+      updatePurchaseType: () => createResult('updatePurchaseType'),
+      recurringCycleLimit: createReadonlySignalLike(null),
+      updateRecurringCycleLimit: () =>
+        createResult('updateRecurringCycleLimit'),
     },
   };
 }
