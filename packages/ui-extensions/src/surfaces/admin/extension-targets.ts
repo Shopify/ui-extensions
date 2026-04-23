1944e24762fe22e4acc73e2004fc284d7089a5b9
Henry Tao <henry.tao@shopify.com>
2026-04-23 08:59:41 -0400

    Add Page component

--- packages/ui-extensions/src/surfaces/admin/extension-targets.ts
+++ packages/ui-extensions/src/surfaces/admin/extension-targets.ts
@@ -681,7 +681,7 @@ export interface ExtensionTargets {
    */
   'admin.app.home.render': RenderExtension<
     StandardApi<'admin.app.home.render'>,
-    FormExtensionComponents | 'Modal'
+    FormExtensionComponents | 'Modal' | 'Page'
   >;
 }
 
