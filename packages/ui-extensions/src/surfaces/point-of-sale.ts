6a2190449f3dace896417eb2bd98e8a7ee45043f
Henry Stelle <henry.stelle@shopify.com>
2026-07-15 11:37:20 -0700

    Remove unnecessary top-level ReadonlySignalLike re-export from POS surface

--- packages/ui-extensions/src/surfaces/point-of-sale.ts
+++ packages/ui-extensions/src/surfaces/point-of-sale.ts
@@ -3,4 +3,3 @@ export * from './point-of-sale/events';
 export * from './point-of-sale/extension-targets';
 export * from './point-of-sale/event/data';
 export * from './point-of-sale/event/output';
-export type {ReadonlySignalLike} from '../shared';
